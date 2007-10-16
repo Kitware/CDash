6a8616cb3e48524d415d93907820c03ff4d70799
Julien Jomier <julien.jomier@kitware.com>
2007-10-16 16:38:00 +0200

    ENH: First checkin

---
+++ sql/cdash.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 2.6.1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Oct 14, 2007 at 03:41 PM
+-- Server version: 4.1.9
+-- PHP Version: 4.3.10
+-- 
+-- Database: `cdash`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `build`
+-- 
+
+CREATE TABLE `build` (
+  `id` int(11) NOT NULL auto_increment,
+  `siteid` int(11) NOT NULL default '0',
+  `stamp` varchar(255) NOT NULL default '',
+  `name` varchar(255) NOT NULL default '',
+  `type` varchar(255) NOT NULL default '',
+  `generator` varchar(255) NOT NULL default '',
+  `starttime` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `endtime` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `command` text NOT NULL,
+  `log` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `siteid` (`siteid`,`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `builderror`
+-- 
+
+CREATE TABLE `builderror` (
+  `buildid` int(11) NOT NULL default '0',
+  `type` tinyint(4) NOT NULL default '0',
+  `logline` int(11) NOT NULL default '0',
+  `text` text NOT NULL,
+  `sourcefile` varchar(255) NOT NULL default '',
+  `sourceline` int(11) NOT NULL default '0',
+  `precontext` text NOT NULL,
+  `postcontext` text NOT NULL,
+  `repeatcount` int(11) NOT NULL default '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `buildupdate`
+-- 
+
+CREATE TABLE `buildupdate` (
+  `buildid` int(11) NOT NULL default '0',
+  `starttime` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `endtime` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `command` text NOT NULL,
+  `type` varchar(4) NOT NULL default ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `configure`
+-- 
+
+CREATE TABLE `configure` (
+  `buildid` int(11) NOT NULL default '0',
+  `starttime` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `endtime` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `command` text NOT NULL,
+  `log` text NOT NULL,
+  `status` tinyint(4) NOT NULL default '0',
+  UNIQUE KEY `buildid` (`buildid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `note`
+-- 
+
+CREATE TABLE `note` (
+  `buildid` int(11) NOT NULL default '0',
+  `text` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `project`
+-- 
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `description` text NOT NULL,
+  `homeurl` varchar(255) NOT NULL default '',
+  `cvsurl` varchar(255) NOT NULL default '',
+  `bugtrackerurl` varchar(255) NOT NULL default '',
+  `logo` mediumblob,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `site`
+-- 
+
+CREATE TABLE `site` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `description` text NOT NULL,
+  `processor` varchar(255) NOT NULL default '',
+  `numprocessors` tinyint(4) NOT NULL default '0',
+  `ip` varchar(255) NOT NULL default '',
+  `latitude` varchar(10) NOT NULL default '',
+  `longitude` varchar(10) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `site2user`
+-- 
+
+CREATE TABLE `site2user` (
+  `siteid` int(11) NOT NULL default '0',
+  `userid` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`siteid`),
+  KEY `userid` (`userid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `test`
+-- 
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL default '0',
+  `buildid` int(11) NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  `status` varchar(10) NOT NULL default '0',
+  `path` varchar(255) NOT NULL default '',
+  `fullname` varchar(255) NOT NULL default '',
+  `command` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `updatefile`
+-- 
+
+CREATE TABLE `updatefile` (
+  `buildid` int(11) NOT NULL default '0',
+  `filename` varchar(255) NOT NULL default '',
+  `checkindate` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `author` varchar(255) NOT NULL default '',
+  `email` varchar(255) NOT NULL default '',
+  `log` text NOT NULL,
+  `revision` float NOT NULL default '0',
+  `priorrevision` float NOT NULL default '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `user`
+-- 
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `email` varchar(255) NOT NULL default '',
+  `password` varchar(40) NOT NULL default '',
+  `firstname` varchar(40) NOT NULL default '',
+  `lastname` varchar(40) NOT NULL default '',
+  `admin` tinyint(4) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `user2project`
+-- 
+
+CREATE TABLE `user2project` (
+  `userid` int(11) NOT NULL default '0',
+  `projectid` int(11) NOT NULL default '0',
+  `role` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`userid`,`projectid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
