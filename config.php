edbcf96bfd8bdc43b7e3f0cca144de1f0996cccf
Julien Jomier <julien.jomier@kitware.com>
2007-10-16 16:30:17 +0200

    ENH: First checkin

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $RCSfile: config.php,v $
+  Language:  PHP
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+// Hostname of the MySQL database	
+$CDASH_DB_HOST = 'localhost';
+// Login for MySQL database access
+$CDASH_DB_LOGIN = 'root';
+// Password for MySQL database access
+$CDASH_DB_PASS = '';
+// Name of the MySQL database
+$CDASH_DB_NAME = 'cdash';
+// Dashboard time frame
+$CDASH_DASHBOARD_TIMEFRAME = 24; //24 hours
+?>
