2ccd3b6948e3eb1f2510418307c4443e8ccdf9c8
Julien Jomier <julien.jomier@kitware.com>
2007-10-18 01:24:25 +0200

    ENH: First checkin

---
+++ viewBuildError.php
@@ -0,0 +1,119 @@
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $RCSfile: common.php,v $
+  Language:  PHP
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+include("config.php");
+include("common.php");
+
+@$buildid = $_GET["buildid"];
+@$date = $_GET["date"];
+
+include("config.php");
+$db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+mysql_select_db("$CDASH_DB_NAME",$db);
+		
+$build_array = mysql_fetch_array(mysql_query("SELECT projectid FROM build WHERE id='$buildid'"));		
+$projectid = $build_array["projectid"];
+
+if(!isset($date) || strlen($date)==0)
+	  	{ 
+				$currenttime = time();
+		  }
+		else
+		  {
+				$currenttime = mktime("23","59","0",substr($date,4,2),substr($date,6,2),substr($date,0,4));
+		  }
+				
+$project = mysql_query("SELECT * FROM project WHERE id='$projectid'");
+if(mysql_num_rows($project)>0)
+		{
+		$project_array = mysql_fetch_array($project);
+		$svnurl = $project_array["cvsurl"];
+		$homeurl = $project_array["homeurl"];
+		$bugurl = $project_array["bugtrackerurl"];			
+		$projectname	= $project_array["name"];		
+		}
+
+		$previousdate = date("Ymd",$currenttime-24*3600);	
+		$nextdate = date("Ymd",$currenttime+24*3600);
+
+$xml = '<?xml version="1.0"?><cdash>';
+$xml .= "<title>CDash : ".$projectname."</title>";
+$xml .= "<cssfile>".$CDASH_CSS_FILE."</cssfile>";
+$xml .="<dashboard>
+  <datetime>".date("D, d M Y H:i:s",$currenttime)."</datetime>
+  <date>".date("l, F d Y",$currenttime)."</date>
+		<svn>".$svnurl."</svn>
+		<bugtracker>".$bugurl."</bugtracker>	
+		<home>".$homeurl."</home>
+		<projectid>".$projectid."</projectid>	
+  <projectname>".$projectname."</projectname>	
+		<previousdate>".$previousdate."</previousdate>	
+		<nextdate>".$nextdate."</nextdate>	
+		</dashboard>
+  ";
+		
+		// Build
+		$xml .= "<build>";
+		$build = mysql_query("SELECT * FROM build WHERE id='$buildid'");
+		$build_array = mysql_fetch_array($build); 
+		$siteid = $build_array["siteid"];
+		$site_array = mysql_fetch_array(mysql_query("SELECT name FROM site WHERE id='$siteid'"));
+		$xml .= add_XML_value("site",$site_array["name"]);
+		$xml .= add_XML_value("buildname",$build_array["name"]);
+		$xml .= add_XML_value("buildid",$build_array["id"]);
+  $xml .= "</build>";
+		
+		@$type = $_GET["type"];
+		if(!isset($type))
+		  {
+				$type = 0;
+		  }
+		// Set the error
+		if($type == 0)
+		  {
+				$xml .= add_XML_value("errortypename","Error");
+				$xml .= add_XML_value("nonerrortypename","Warning");
+				$xml .= add_XML_value("nonerrortype","1");	
+		  }
+		else
+				{
+				$xml .= add_XML_value("errortypename","Warning");
+				$xml .= add_XML_value("nonerrortypename","Error");
+				$xml .= add_XML_value("nonerrortype","0");	
+				} 
+		
+		$xml .= "<errors>";
+		
+	 $errors = mysql_query("SELECT * FROM builderror WHERE buildid='$buildid' and type='$type'");
+		while($error_array = mysql_fetch_array($errors))
+		  {
+				$xml .= "<error>";
+				$xml .= add_XML_value("logline",$error_array["logline"]);
+				$xml .= add_XML_value("text",$error_array["text"]);
+				$xml .= add_XML_value("sourcefile",$error_array["sourcefile"]);
+				$xml .= add_XML_value("sourceline",$error_array["sourceline"]);
+				$xml .= add_XML_value("precontext",$error_array["precontext"]);
+				$xml .= add_XML_value("postcontext",$error_array["postcontext"]);
+				$xml .= "</error>";
+		  }
+
+		$xml .= "</errors>";
+  $xml .= "</cdash>";
+
+// Now doing the xslt transition
+generate_XSLT($xml,"viewBuildError");
+?>
---
+++ viewBuildError.xsl
@@ -0,0 +1,47 @@
+<xsl:stylesheet
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version='1.0'>
+    
+				
+			<xsl:include href="header.xsl"/>
+			<xsl:include href="footer.xsl"/>
+				
+    <xsl:output method="html"/>
+    <xsl:template match="/">
+      <html>
+	      <head>
+							<title><xsl:value-of select="cdash/title"/></title>
+		      <meta name="robots" content="noindex,nofollow" />
+         <link rel="StyleSheet" type="text/css">
+									<xsl:attribute name="href"><xsl:value-of select="cdash/cssfile"/></xsl:attribute>
+									</link>
+	      </head>
+       <body bgcolor="#ffffff">
+			
+							<xsl:call-template name="header"/>
+<br/>
+
+<p><b>Site:</b><xsl:value-of select="cdash/build/site"/></p>
+<p><b>Build Name:</b><xsl:value-of select="cdash/build/buildname"/></p>       
+Found <xsl:value-of select="count(cdash/errors/error)"/><xsl:text>&#x20;</xsl:text><xsl:value-of select="cdash/errortypename"/>s<br/>
+<p><a>
+<xsl:attribute name="href">viewBuildError.php?type=<xsl:value-of select="cdash/nonerrortype"/>&#38;buildid=<xsl:value-of select="cdash/build/buildid"/></xsl:attribute>
+<xsl:value-of select="cdash/nonerrortypename"/>s</a> are here.</p>
+<xsl:for-each select="cdash/errors/error">
+<hr/>
+<h3><A Name="650">Build Log line <xsl:value-of select="logline"/></A></h3>
+<br/>
+File: <b><xsl:value-of select="sourcefile"/></b>
+Line: <b><xsl:value-of select="sourceline"/></b>
+<a href="http://www.itk.org/cgi-bin/viewcvs.cgi/Code/BasicFilters/itkGradientRecursiveGaussianImageFilter.txx?cvsroot=Insight&amp;annotate=HEAD#84">CVS</a>
+<pre><xsl:value-of select="precontext"/></pre>
+<pre><xsl:value-of select="text"/></pre>
+<pre><xsl:value-of select="postcontext"/></pre>
+</xsl:for-each>
+<hr/>
+<!-- FOOTER -->
+<br/>
+<xsl:call-template name="footer"/>
+					   </body>
+      </html>
+    </xsl:template>
+</xsl:stylesheet>
