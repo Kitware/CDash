093d14fe716abfd45991ba75ddf53cd83af5f0f8
Julien Jomier <julien.jomier@kitware.com>
2007-10-24 20:23:47 +0200

    ENH: Added table of projects

--- index.php
+++ index.php
@@ -17,168 +17,197 @@
 =========================================================================*/
 include("config.php");
 include("common.php");
+  
+/** Generate the index table */
+function generate_index_table()
+{
+  include("config.php");
+  
+  $xml = '<?xml version="1.0"?><cdash>';
+  $xml .= "<title>"."</title>";
+  $xml .= "<cssfile>".$CDASH_CSS_FILE."</cssfile>";
+  $xml .= "<hostname>".$_SERVER['SERVER_NAME']."</hostname>";
+  $xml .= "<date>".date("r")."</date>";
+  
+  $projects = get_projects();
+  $row=0;
+  foreach($projects as $project)
+    {
+    $xml .= "<project>";
+    $xml .= "<name>".$project['name']."</name>";
+    $xml .= "<lastbuild>".$project['last_build']."</lastbuild>";
+    $xml .= "<nbuilds>".$project['nbuilds']."</nbuilds>";
+    $xml .= "<ntests>".$project['ntests']."</ntests>";
+    $xml .= "<row>".$row."</row>";
+    $xml .= "</project>";
+    $row = !$row;
+    }
+
+  $xml .= "</cdash>";  
+  return $xml;
+}
 
 /** Generate the main dashboard XML */
 function generate_main_dasboard_XML($projectid,$date)
 {
   include("config.php");
- 	$db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+  $db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
   mysql_select_db("$CDASH_DB_NAME",$db);
-		
-	 $project = mysql_query("SELECT * FROM project WHERE id='$projectid'");
-		if(mysql_num_rows($project)>0)
-		  {
-				$project_array = mysql_fetch_array($project);
-				$svnurl = $project_array["cvsurl"];
-				$homeurl = $project_array["homeurl"];
-				$bugurl = $project_array["bugtrackerurl"];			
-				$projectname	= $project_array["name"];		
-		  }
-		else
-		  {
-		  $projectname = "NA";
-		  }
-				
-	 $xml = '<?xml version="1.0"?><cdash>';
-		$xml .= "<title>CDash - ".$projectname."</title>";
-		$xml .= "<cssfile>".$CDASH_CSS_FILE."</cssfile>";
-		
-		if(!isset($date) || strlen($date)==0)
-	  	{ 
-				$currenttime = time();
-		  }
-		else
-		  {
-				$currenttime = mktime("23","59","0",substr($date,4,2),substr($date,6,2),substr($date,0,4));
-		  }
-		
-		$previousdate = date("Ymd",$currenttime-24*3600);	
-		$nextdate = date("Ymd",$currenttime+24*3600);
-		
-		// Main dashboard section 
-		$xml .=
+  
+  $project = mysql_query("SELECT * FROM project WHERE id='$projectid'");
+  if(mysql_num_rows($project)>0)
+    {
+    $project_array = mysql_fetch_array($project);
+    $svnurl = $project_array["cvsurl"];
+    $homeurl = $project_array["homeurl"];
+    $bugurl = $project_array["bugtrackerurl"];   
+    $projectname = $project_array["name"];  
+    }
+  else
+    {
+    $projectname = "NA";
+    }
+    
+  $xml = '<?xml version="1.0"?><cdash>';
+  $xml .= "<title>CDash - ".$projectname."</title>";
+  $xml .= "<cssfile>".$CDASH_CSS_FILE."</cssfile>";
+  
+  if(!isset($date) || strlen($date)==0)
+    { 
+    $currenttime = time();
+    }
+  else
+    {
+    $currenttime = mktime("23","59","0",substr($date,4,2),substr($date,6,2),substr($date,0,4));
+    }
+  
+  $previousdate = date("Ymd",$currenttime-24*3600); 
+  $nextdate = date("Ymd",$currenttime+24*3600);
+  
+  // Main dashboard section 
+  $xml .=
   "<dashboard>
   <datetime>".date("D, d M Y H:i:s",$currenttime)."</datetime>
   <date>".date("l, F d Y",$currenttime)."</date>
-		<svn>".$svnurl."</svn>
-		<bugtracker>".$bugurl."</bugtracker>	
-		<home>".$homeurl."</home>
-		<projectid>".$projectid."</projectid>	
-  <projectname>".$projectname."</projectname>	
-		<previousdate>".$previousdate."</previousdate>	
-		<nextdate>".$nextdate."</nextdate>	
-		
-		</dashboard>
+  <svn>".$svnurl."</svn>
+  <bugtracker>".$bugurl."</bugtracker> 
+  <home>".$homeurl."</home>
+  <projectid>".$projectid."</projectid> 
+  <projectname>".$projectname."</projectname> 
+  <previousdate>".$previousdate."</previousdate> 
+  <nextdate>".$nextdate."</nextdate> 
+  
+  </dashboard>
   ";
-		
-		// builds
+  
+  // builds
   $xml .= "<builds>";
-	
+ 
 
-		$totalerrors = 0;
-		$totalwarnings = 0;
-		$totalconfigure = 0;
-		$totalnotrun = 0;
-		$totalfail= 0;
-		$totalpass = 0;	
-		$totalna = 0;	
-				
+  $totalerrors = 0;
+  $totalwarnings = 0;
+  $totalconfigure = 0;
+  $totalnotrun = 0;
+  $totalfail= 0;
+  $totalpass = 0; 
+  $totalna = 0; 
+    
   // Check the builds
-		$beginning_timestamp = $currenttime-($CDASH_DASHBOARD_TIMEFRAME*3600);
-		$end_timestamp = $currenttime;
+  $beginning_timestamp = $currenttime-($CDASH_DASHBOARD_TIMEFRAME*3600);
+  $end_timestamp = $currenttime;
   $builds = mysql_query("SELECT * FROM build 
-		                       WHERE UNIX_TIMESTAMP(starttime)<$end_timestamp AND UNIX_TIMESTAMP(starttime)>$beginning_timestamp
-																									AND projectid='$projectid'
-																									");
-		echo mysql_error();
-		while($build_array = mysql_fetch_array($builds))
-		  {  
-				// Get the site name
-				$xml .= "<".strtolower($build_array["type"]).">";
-				$siteid = $build_array["siteid"];
-				$buildid = $build_array["id"];
-				$site_array = mysql_fetch_array(mysql_query("SELECT name FROM site WHERE id='$siteid'"));
-				
-				$xml .= add_XML_value("site",$site_array["name"]);
-				$xml .= add_XML_value("siteid",$siteid);
-				$xml .= add_XML_value("buildname",$build_array["name"]);
-				$xml .= add_XML_value("buildid",$build_array["id"]);
+                         WHERE UNIX_TIMESTAMP(starttime)<$end_timestamp AND UNIX_TIMESTAMP(starttime)>$beginning_timestamp
+                         AND projectid='$projectid'
+                         ");
+  echo mysql_error();
+  while($build_array = mysql_fetch_array($builds))
+    {  
+    // Get the site name
+    $xml .= "<".strtolower($build_array["type"]).">";
+    $siteid = $build_array["siteid"];
+    $buildid = $build_array["id"];
+    $site_array = mysql_fetch_array(mysql_query("SELECT name FROM site WHERE id='$siteid'"));
+    
+    $xml .= add_XML_value("site",$site_array["name"]);
+    $xml .= add_XML_value("siteid",$siteid);
+    $xml .= add_XML_value("buildname",$build_array["name"]);
+    $xml .= add_XML_value("buildid",$build_array["id"]);
     $xml .= add_XML_value("generator",$build_array["generator"]);
     //<notes>note</notes>
-				
-				$update = mysql_query("SELECT buildid FROM updatefile WHERE buildid='$buildid'");
-			 $xml .= add_XML_value("update",mysql_num_rows($update));
+    
+    $update = mysql_query("SELECT buildid FROM updatefile WHERE buildid='$buildid'");
+    $xml .= add_XML_value("update",mysql_num_rows($update));
     
     $xml .= "<build>";
-				
-				// Find the number of errors and warnings
-				$builderror = mysql_query("SELECT buildid FROM builderror WHERE buildid='$buildid' AND type='0'");
-				$nerrors = mysql_num_rows($builderror);
-				$totalerrors += $nerrors;
-				$xml .= add_XML_value("error",$nerrors);
-				$buildwarning = mysql_query("SELECT buildid FROM builderror WHERE buildid='$buildid' AND type='1'");
-				$nwarnings = mysql_num_rows($buildwarning);
-				$totalwarnings += $nwarnings;
-				$xml .= add_XML_value("warning",$nwarnings);
-				$diff = (strtotime($build_array["endtime"])-strtotime($build_array["starttime"]))/60;
+    
+    // Find the number of errors and warnings
+    $builderror = mysql_query("SELECT buildid FROM builderror WHERE buildid='$buildid' AND type='0'");
+    $nerrors = mysql_num_rows($builderror);
+    $totalerrors += $nerrors;
+    $xml .= add_XML_value("error",$nerrors);
+    $buildwarning = mysql_query("SELECT buildid FROM builderror WHERE buildid='$buildid' AND type='1'");
+    $nwarnings = mysql_num_rows($buildwarning);
+    $totalwarnings += $nwarnings;
+    $xml .= add_XML_value("warning",$nwarnings);
+    $diff = (strtotime($build_array["endtime"])-strtotime($build_array["starttime"]))/60;
     $xml .= "<time>".$diff."</time>";
     $xml .= "</build>";
-				
-				// Get the Configure options
-				$configure = mysql_query("SELECT status FROM configure WHERE buildid='$buildid'");
-				if(mysql_num_rows($configure)>0)
-				  {
-						$configure_array = mysql_fetch_array($configure);
-						$xml .= add_XML_value("configure",$configure_array["status"]);
-						$totalconfigure += $configure_array["status"];
-				  }
-		
-				// Get the tests
-				$test = mysql_query("SELECT * FROM test WHERE buildid='$buildid'");
-				if(mysql_num_rows($test)>0)
-				  {
-				  $test_array = mysql_fetch_array($test);
-						$xml .= "<test>";
-						$nnotrun = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='notrun'"));
-						$nfail = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='failed'"));
-						$npass = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='passed'"));
-						$nna = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='na'"));
-						
-						$totalnotrun += $nnotrun;
-						$totalfail += $nfail;
-						$totalpass += $npass;
-						$totalna += $nna;
-						
-			   $xml .= add_XML_value("notrun",$nnotrun);
+    
+    // Get the Configure options
+    $configure = mysql_query("SELECT status FROM configure WHERE buildid='$buildid'");
+    if(mysql_num_rows($configure)>0)
+      {
+      $configure_array = mysql_fetch_array($configure);
+      $xml .= add_XML_value("configure",$configure_array["status"]);
+      $totalconfigure += $configure_array["status"];
+      }
+  
+    // Get the tests
+    $test = mysql_query("SELECT * FROM test WHERE buildid='$buildid'");
+    if(mysql_num_rows($test)>0)
+      {
+      $test_array = mysql_fetch_array($test);
+      $xml .= "<test>";
+      $nnotrun = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='notrun'"));
+      $nfail = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='failed'"));
+      $npass = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='passed'"));
+      $nna = mysql_num_rows(mysql_query("SELECT * FROM test WHERE buildid='$buildid' AND status='na'"));
+      
+      $totalnotrun += $nnotrun;
+      $totalfail += $nfail;
+      $totalpass += $npass;
+      $totalna += $nna;
+      
+      $xml .= add_XML_value("notrun",$nnotrun);
       $xml .= add_XML_value("fail",$nfail);
       $xml .= add_XML_value("pass",$npass);
-						$xml .= add_XML_value("na",$nna);
-						$xml .= add_XML_value("time","NA");
-				  $xml .= "</test>";
-					 }
-				$xml .= add_XML_value("builddate",$build_array["starttime"]);
-				$xml .= add_XML_value("submitdate",$build_array["submittime"]);
+      $xml .= add_XML_value("na",$nna);
+      $xml .= add_XML_value("time","NA");
+      $xml .= "</test>";
+      }
+    $xml .= add_XML_value("builddate",$build_array["starttime"]);
+    $xml .= add_XML_value("submitdate",$build_array["submittime"]);
   $xml .= "</".strtolower($build_array["type"]).">";
   } // end looping through builds
  
-	 $xml .= add_XML_value("totalConfigure",$totalconfigure);
+  $xml .= add_XML_value("totalConfigure",$totalconfigure);
   $xml .= add_XML_value("totalError",$totalerrors);
   $xml .= add_XML_value("totalWarning",$totalwarnings);
-	
-		$xml .= add_XML_value("totalNotRun",$totalnotrun);
-		$xml .= add_XML_value("totalFail",$totalfail);
-		$xml .= add_XML_value("totalPass",$totalpass);	
-		$xml .= add_XML_value("totalNA",$totalna);
-			
+ 
+  $xml .= add_XML_value("totalNotRun",$totalnotrun);
+  $xml .= add_XML_value("totalFail",$totalfail);
+  $xml .= add_XML_value("totalPass",$totalpass); 
+  $xml .= add_XML_value("totalNA",$totalna);
+   
   $xml .= "</builds>";
-		
-		/*
+  
+  /*
 <coverage>
     <site>gaia.kitware</site>
-				<siteid>1</siteid>
+    <siteid>1</siteid>
     <buildname>Win32-vs8-Debug-Coverage</buildname>
-				<percentage>57.62</percentage>
-				<fail>2</fail>
+    <percentage>57.62</percentage>
+    <fail>2</fail>
     <pass>3</pass>
     <date>Oct 09 23:09 Eastern Daylight Time</date>
     <submitdate>Oct 09 23:09 Eastern Daylight Time</submitdate>
@@ -187,7 +216,7 @@ function generate_main_dasboard_XML($projectid,$date)
 
   $xml .= "</cdash>";
 
-	 return $xml;
+  return $xml;
 } 
 
 
@@ -195,35 +224,29 @@ function generate_main_dasboard_XML($projectid,$date)
 $db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
 if(!$db)
   {
-		// redirect to the install.php script
-		echo "<script language=\"javascript\">window.location='install.php'</script>";
-		return;
+  // redirect to the install.php script
+  echo "<script language=\"javascript\">window.location='install.php'</script>";
+  return;
   }
 if(mysql_select_db("$CDASH_DB_NAME",$db) === FALSE)
   {
-		echo "<script language=\"javascript\">window.location='install.php'</script>";
-		return;
+  echo "<script language=\"javascript\">window.location='install.php'</script>";
+  return;
   }
-		
+  
 @$projectname = $_GET["project"];
-if(!isset($projectname )) // if the project name is not set we grab the first project
+if(!isset($projectname )) // if the project name is not set we display the table of projects
   {
-  $project = mysql_query("SELECT id FROM project ORDER BY id");
-		if(mysql_num_rows($project)>0)
-		  {
-				$project_array = mysql_fetch_array($project);
-				$projectid = $project_array["id"];
-    }
+  $xml = generate_index_table();
+  // Now doing the xslt transition
+  generate_XSLT($xml,"indextable");
   }
 else
   {
-		$projectid = get_project_id($projectname);
+  $projectid = get_project_id($projectname);
+  @$date = $_GET["date"];
+  $xml = generate_main_dasboard_XML($projectid,$date);
+  // Now doing the xslt transition
+  generate_XSLT($xml,"index");
   }
-		
-@$date = $_GET["date"];
-
-$xml = generate_main_dasboard_XML($projectid,$date);
-
-// Now doing the xslt transition
-generate_XSLT($xml,"index");
 ?>
---
+++ indextable.xsl
@@ -0,0 +1,78 @@
+<xsl:stylesheet
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version='1.0'>
+
+   <xsl:include href="footer.xsl"/>
+    
+    <xsl:output method="html"/>
+    <xsl:template match="/">
+      <html>
+       <head>
+       <title><xsl:value-of select="cdash/title"/></title>
+        <meta name="robots" content="noindex,nofollow" />
+         <link rel="StyleSheet" type="text/css">
+         <xsl:attribute name="href"><xsl:value-of select="cdash/cssfile"/></xsl:attribute>
+         </link>
+       </head>
+       <body bgcolor="#ffffff">
+   
+<table border="0" cellpadding="0" cellspacing="2" width="100%">
+<tr>
+<td align="center"><a href="index.php"><img alt="Logo/Homepage link" height="100" src="images/cdash.gif" border="0"/></a>
+</td>
+<td bgcolor="#6699cc" valign="top" width="100%">
+<font color="#ffffff"><h2>CDash on <xsl:value-of select="cdash/hostname"/></h2>
+<h3><xsl:value-of select="cdash/date"/></h3></font>
+</td></tr><tr><td></td><td>
+<div id="navigator">
+</div>
+</td>
+</tr>
+</table>
+
+<br/>
+
+<table class="dart">
+<tbody>
+<tr class="table-heading">
+  <th colspan="4" align="left">Available Dashboards</th>
+</tr>
+
+  <tr class="table-heading">
+     <th align="center">Project</th>
+     <th align="center">Submissions</th>
+     <th align="center">Tests</th>
+     <th align="center">Last activity</th>
+  </tr>
+
+   <xsl:for-each select="cdash/project">
+   <tr>
+     <xsl:choose>
+          <xsl:when test="row=0">
+            <xsl:attribute name="class">tr-odd</xsl:attribute>
+           </xsl:when>
+          <xsl:otherwise>
+           <xsl:attribute name="class">tr-even</xsl:attribute>
+           </xsl:otherwise>
+        </xsl:choose>
+   <td>
+     <a>
+     <xsl:attribute name="href">index.php?project=<xsl:value-of select="name"/></xsl:attribute>
+     <xsl:value-of select="name"/>
+     </a></td>
+    <td align="right"><xsl:value-of select="nbuilds"/></td>
+    <td align="right"><xsl:value-of select="ntests"/></td>
+    <td align="right"><xsl:value-of select="lastbuild"/></td>
+    </tr>
+   </xsl:for-each>
+  
+</tbody>
+</table>  
+
+<br/>
+<!-- FOOTER -->
+<br/>
+<xsl:call-template name="footer"/>
+        </body>
+      </html>
+    </xsl:template>
+</xsl:stylesheet>
