f992f5347a119256c488d07ed6a7a438cbbfe1b5
Julien Jomier <julien.jomier@kitware.com>
2007-10-29 20:39:41 +0100

    ENH: Missing field

--- sql/cdashdata.sql
+++ sql/cdashdata.sql
@@ -14,4 +14,4 @@
 -- Dumping data for table `user`
 -- 
 
-INSERT INTO `user` VALUES (1, 'admin@cdash.org', '200ceb26807d6bf99fd6f4f0d1ca54d4', 'administrator', '', 1);
+INSERT INTO `user` VALUES (1, 'admin@cdash.org', '200ceb26807d6bf99fd6f4f0d1ca54d4', 'administrator', '','Kitware Inc.', 1);
