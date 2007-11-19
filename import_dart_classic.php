d11c7323e16060b77279835dca1eb8b432092624
Zack Galbreath <zack.galbreath@kitware.com>
2007-11-19 15:06:09 -0500

    ENH: slightly more robust import script

--- import_dart_classic.php
+++ import_dart_classic.php
@@ -19,6 +19,8 @@ include_once("config.php");
 include_once("common.php");
 include_once("ctestparser.php");
 
+set_time_limit(60);
+
 $db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
 mysql_select_db("$CDASH_DB_NAME",$db);
 
@@ -47,10 +49,12 @@ $xml = "<cdash>";
 $xml .= "<cssfile>".$CDASH_CSS_FILE."</cssfile>";
 
 $project = mysql_query("SELECT name,id FROM project ORDER BY id");
+$projName = "";
 while($project_array = mysql_fetch_array($project))
   {
+  $projName = $project_array["name"];
   $xml .= "<project>";
-  $xml .= "<name>".$project_array["name"]."</name>";
+  $xml .= "<name>".$projName."</name>";
   $xml .= "<id>".$project_array["id"]."</id>";
   $xml .= "</project>";
   }
@@ -69,31 +73,49 @@ if($Submit)
   {
   $directory = $_POST["directory"];
   $projectid = $_POST["project"];
-  if(strlen($directory)>0)
+  if(strlen($directory)>0 )
     {
     $directory = str_replace('\\\\','/',$directory);
+    if(!file_exists($directory) || strpos($directory, "Sites") === FALSE)
+      {
+      echo("Error: $directory is not a valid path to Dart XML data on the server.<br>\n");
+      generate_XSLT($xml,"import_dart_classic");
+      exit(0);
+      }
     $startDate = mktime(0,0,0,$monthFrom,$dayFrom,$yearFrom);
     $endDate = mktime(0,0,0,$monthTo,$dayTo,$yearTo);
     $numDays = ($endDate - $startDate) / (24 * 3600) + 1;
     for($i=0;$i<$numDays;$i++)
       {
       $currentDay = date("Ymd", mktime(0,0,0,$monthFrom,$dayFrom+$i,$yearFrom));
+      echo("Gathering XML files for $currentDay...<br>\n");
       $files = glob($directory."/*/*/$currentDay-*/XML/*.xml");
       $numFiles = count($files);
-      echo("$numFiles imported for $currentDay<br>\n");
+      echo("$numFiles found<br>\n");
+      $numDots = 0;
       foreach($files as $file)
         {
         if(strlen($file)==0)
           {
-   continue;
-   }
+          continue;
+          }
         $handle = fopen($file,"r");
         $contents = fread($handle,filesize($file));
+        echo ".";
+        $numDots++;
+        if($numDots > 79)
+          {
+          echo "<br>\n";
+          $numDots = 0;
+          }
         ctest_parse($contents,$projectid);
         fclose($handle);
         }
+      echo "<br>\n";
       }
     } // end strlen(directory)>0
+    echo("<a href=index.php?project=$projName>Back to $projName dashboard</a>\n");
+    exit(0);
   }
 
 // Now doing the xslt transition
