03c46979ef72b663888cdb2151bc3bc42499fc20
Julien Jomier <julien.jomier@kitware.com>
2007-11-23 22:33:31 +0100

    ENH: Fixed problem with fade in

--- javascript/cdashBuildGroup.js
+++ javascript/cdashBuildGroup.js
@@ -1,13 +1,20 @@
 // Main function
 function buildgroup_click(buildid)
-{
+{			
 		var group = "#buildgroup_"+buildid;
-	 if($(group).html() != "" && $(group).html() != "added to group!")
+		
+		$(group).ajaxSuccess(function(evt, request, settings){
+    $(group).fadeIn('slow');
+		 });
+		
+	 //if($(group).html() != "" && $(group).html() != "added to group!")
+		if($(group).html() != "" && $(group).is(":visible"))
 		  {
-				$(group).html("");
-				$(group).fadeOut('slow');
+				$(group).fadeOut('medium');
 				return;
 		  }
-		$(group).fadeIn('slow');
+		
+		$(group).hide();
 		$(group).load("ajax/addbuildgroup.php?buildid="+buildid);
+
 }
