e8fc04ecead775ba93d659dc719deb9ec146fec4
Julien Jomier <julien.jomier@kitware.com>
2007-11-26 04:31:12 +0100

    ENH: Some fixes

--- javascript/cdashBuildGroup.js
+++ javascript/cdashBuildGroup.js
@@ -1,37 +1,29 @@
 // Main function
 function buildgroup_click(buildid)
-{			
-		var group = "#buildgroup_"+buildid;
-		
-		$(group).ajaxSuccess(function(evt, request, settings){
-    $(group).fadeIn('slow');
-		 });
-		
-	 //if($(group).html() != "" && $(group).html() != "added to group!")
-		if($(group).html() != "" && $(group).is(":visible"))
-		  {
-				$(group).fadeOut('medium');
-				return;
-		  }
-		
-		$(group).hide();
-		$(group).load("ajax/addbuildgroup.php?buildid="+buildid);
+{   
+  var group = "#buildgroup_"+buildid;
+  
+  //if($(group).html() != "" && $(group).html() != "added to group!")
+  if($(group).html() != "" && $(group).is(":visible"))
+    {
+    $(group).fadeOut('medium');
+    return;
+    }
+  
+  $(group).hide();
+  $(group).load("ajax/addbuildgroup.php?buildid="+buildid,{},function(){$(this).fadeIn('slow');});
 }
 
 function buildinfo_click(buildid)
-{			
-		var group = "#buildgroup_"+buildid;
-		
-		$(group).ajaxSuccess(function(evt, request, settings){
-    $(group).fadeIn('slow');
-		 });
-		
-		if($(group).html() != "" && $(group).is(":visible"))
-		  {
-				$(group).fadeOut('medium');
-				return;
-		  }
-		
-		$(group).hide();
-		$(group).load("ajax/buildinfogroup.php?buildid="+buildid);
+{   
+  var group = "#buildgroup_"+buildid;
+  if($(group).html() != "" && $(group).is(":visible"))
+    {
+    $(group).fadeOut('medium');
+    return;
+    }
+  
+  $(group).hide();
+  $(group).load("ajax/buildinfogroup.php?buildid="+buildid,{},function(){$(this).fadeIn('slow');});
 }
+
