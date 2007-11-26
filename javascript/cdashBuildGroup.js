f19dc89f21fad271ccb18bd90cafeee7e4cef03a
Julien Jomier <julien.jomier@kitware.com>
2007-11-26 18:31:07 +0100

    ENH: Added expectedinfo_click

--- javascript/cdashBuildGroup.js
+++ javascript/cdashBuildGroup.js
@@ -27,3 +27,14 @@ function buildinfo_click(buildid)
   $(group).load("ajax/buildinfogroup.php?buildid="+buildid,{},function(){$(this).fadeIn('slow');});
 }
 
+function expectedinfo_click(siteid,buildname,projectid,buildtype,currentime)
+{   
+  var group = "#infoexpected_"+siteid+"_"+buildname;
+  if($(group).html() != "" && $(group).is(":visible"))
+    {
+    $(group).fadeOut('medium');
+    return;
+    }
+  $(group).hide();
+  $(group).load("ajax/expectedinfo.php?siteid="+siteid+"&buildname="+buildname+"&projectid="+projectid+"&buildtype="+buildtype+"&currenttime="+currentime,{},function(){$(this).fadeIn('slow');});
+}
