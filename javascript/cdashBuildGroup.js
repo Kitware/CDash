fa1c9ff57acd931633908f8d437ab6348fc42cd2
Julien Jomier <julien.jomier@kitware.com>
2007-11-26 21:13:18 +0100

    ENH: Some fixes

--- javascript/cdashBuildGroup.js
+++ javascript/cdashBuildGroup.js
@@ -27,9 +27,20 @@ function buildinfo_click(buildid)
   $(group).load("ajax/buildinfogroup.php?buildid="+buildid,{},function(){$(this).fadeIn('slow');});
 }
 
-function expectedinfo_click(siteid,buildname,projectid,buildtype,currentime)
+
+function URLencode(sStr) 
+{
+    return escape(sStr)
+       .replace(/\+/g, '%2B')
+          .replace(/\"/g,'%22')
+             .replace(/\'/g, '%27');
+}
+
+function expectedinfo_click(siteid,buildname,divname,projectid,buildtype,currentime)
 {   
-  var group = "#infoexpected_"+siteid+"_"+buildname;
+  buildname = URLencode(buildname);
+
+  var group = "#infoexpected_"+divname;
   if($(group).html() != "" && $(group).is(":visible"))
     {
     $(group).fadeOut('medium');
