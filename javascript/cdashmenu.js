44124bfaa646b6533dbfa01b09cd612d6e6e2e4d
Julien Jomier <julien.jomier@kitware.com>
2007-12-07 00:38:40 +0100

    BUG: Fixed calendar

--- javascript/cdashmenu.js
+++ javascript/cdashmenu.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
      $('#calendar').datepicker({onSelect: calendarSelected}); 
-     
+    
   // Display the date range from a multi-month inline date picker 
   function calendarSelected(dateStr) {
-    window.location = "index.php?project=Insight&date="+dateStr.substr(6,4)+dateStr.substr(0,2)+dateStr.substr(3,2);
+			var project = document.getElementById("projectname");
+    window.location = "index.php?project="+project.value+"&date="+dateStr.substr(6,4)+dateStr.substr(0,2)+dateStr.substr(3,2);
    }
   
    $('#calendar').hide();
