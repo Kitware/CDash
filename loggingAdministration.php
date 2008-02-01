36f6e4a187f83044cd2ec5487c3f18293212ea73
Julien Jomier <julien.jomier@kitware.com>
2008-02-01 03:54:07 +0100

    ENH: Added view of the unparsed files

---
+++ loggingAdministration.php
@@ -0,0 +1,42 @@
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $RCSfile: common.php,v $
+  Language:  PHP
+  Date:      $Date: 2008-01-25 14:40:27 -0500 (Fri, 25 Jan 2008) $
+  Version:   $Revision: 373 $
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+include("config.php");
+include("common.php"); 
+
+@$db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+mysql_select_db("$CDASH_DB_NAME",$db);
+$xml = "<cdash>";
+$xml .= "<cssfile>".$CDASH_CSS_FILE."</cssfile>";
+$xml .= "<backurl>user.php</backurl>";
+$xml .= "<title>CDash - Logging Administration</title>";
+$xml .= "<menutitle>CDash</menutitle>";
+$xml .= "<menusubtitle>Log Viewer</menusubtitle>";
+
+// List of the file in the directory that have other*.xml
+foreach (glob($CDASH_BACKUP_DIRECTORY."/*_Other*.xml") as $filename) {
+    $xml .= "<file>";
+		$xml .= "<name>".substr($filename,strrpos($filename,"/")+1)."</name>";
+		$xml .= "<fullpath>".$filename."</fullpath>";
+		$xml .= "</file>";
+}
+
+$xml .= "</cdash>";
+
+// Now doing the xslt transition
+generate_XSLT($xml,"loggingAdministration");
+?>
---
+++ loggingAdministration.xsl
@@ -0,0 +1,47 @@
+<xsl:stylesheet
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version='1.0'>
+
+   <xsl:include href="footer.xsl"/>
+    <xsl:include href="headerback.xsl"/> 
+   
+    <xsl:output method="html"/>
+    <xsl:template match="/">
+      <html>
+       <head>
+       <title><xsl:value-of select="cdash/title"/></title>
+        <meta name="robots" content="noindex,nofollow" />
+         <link rel="StyleSheet" type="text/css">
+         <xsl:attribute name="href"><xsl:value-of select="cdash/cssfile"/></xsl:attribute>
+         </link>
+       </head>
+       <body bgcolor="#ffffff">
+   <xsl:call-template name="headerback"/>
+
+<xsl:if test="string-length(cdash/alert)>0">
+<b><xsl:value-of select="cdash/alert"/></b>
+</xsl:if>
+<br/><br/>
+
+<form name="form1" enctype="multipart/form-data" method="post" action="">
+<table border="0">
+ <xsl:for-each select="cdash/file">
+  <tr>
+    <td><div align="right"><b>Unparsed File:</b></div></td>
+    <td><div align="left"><a>
+		<xsl:attribute name="href">
+		<xsl:value-of select="fullpath"/>
+		</xsl:attribute>
+		<xsl:value-of select="name"/></a></div></td>
+  </tr> 
+	</xsl:for-each> 
+</table>
+</form>
+
+<br/>
+<!-- FOOTER -->
+<br/>
+<xsl:call-template name="footer"/>
+        </body>
+      </html>
+    </xsl:template>
+</xsl:stylesheet>
