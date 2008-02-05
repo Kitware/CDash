1c312294f549066cb3c0d230f6d5c10379fc42fc
Julien Jomier <julien.jomier@kitware.com>
2008-02-05 22:33:53 +0100

    ENH: First checkin

---
+++ getIPFromApacheAccessLog.php
@@ -0,0 +1,51 @@
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $RCSfile: common.php,v $
+  Language:  PHP
+  Date:      $Date: 2008-02-04 14:19:42 -0500 (Mon, 04 Feb 2008) $
+  Version:   $Revision: 430 $
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+// Open the database connection
+include("config.php");
+include("common.php");
+
+$db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+mysql_select_db("$CDASH_DB_NAME",$db);
+
+$apacheaccesslog = "access.txt";
+$contents = file_get_contents($apacheaccesslog);
+
+// Loop through the sites
+$site = mysql_query("SELECT name FROM site WHERE latitude='' AND longitude=''");
+while($site_array = mysql_fetch_array($site))
+{
+  $sitename = $site_array["name"];
+  $pos = strpos($contents,$sitename);
+  if($pos !== FALSE)
+    {  
+    // Find the IP in the log
+    $beginip = strrpos(substr($contents,$pos-500,500),"\n")+$pos-500+1;
+    $endip = strpos($contents," ",$beginip);
+    $ip = substr($contents,$beginip,$endip-$beginip);
+    echo "IP=".$ip."*<br>";
+    $location = get_geolocation($ip);
+    $latitude = $location['latitude'];
+    $longitude = $location['longitude'];
+  
+    $sql = "UPDATE site SET latitude='$latitude',longitude='$longitude' WHERE name='$sitename'";
+    //mysql_query($sql);
+    echo mysql_error();
+    }
+}
+
+?>
