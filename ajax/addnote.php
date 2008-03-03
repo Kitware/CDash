190672e00c5fc92c864bf50bdbbb1f93aa446448
Julien Jomier <julien.jomier@kitware.com>
2008-03-03 22:56:50 +0100

    ENH: First checkin

---
+++ ajax/addnote.php
@@ -0,0 +1,86 @@
+<html>
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $RCSfile: common.php,v $
+  Language:  PHP
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+include("../config.php");
+include("../common.php");
+$noforcelogin = 1;
+include('../login.php');
+  
+$buildid = $_GET["buildid"];
+$userid = $_GET["userid"];
+
+if(!$userid || !$buildid || !isset($_SESSION['cdash']))
+  {
+  echo "Not valid id";
+  return;
+  }
+  
+$db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+mysql_select_db("$CDASH_DB_NAME",$db);
+
+@$AddNote = $_POST["AddNote"];
+if($AddNote)
+  {
+  $TextNote = $_POST["TextNote"];
+  $Status = $_POST["Status"];
+  if(strlen($TextNote)>0)
+    {
+    $now = gmdate("Y-m-d H:i:s");
+    mysql_query("INSERT INTO buildnote (buildid,userid,note,timestamp,status) 
+                   VALUES ('$buildid','$userid','$TextNote','$now','$Status')");
+    }
+  $url = "../buildSummary.php?buildid=".$buildid."&message=noteaddded";
+  header("location: ".$url);
+  }
+?>
+
+<head>
+<style type="text/css">
+  .submitLink {
+   color: #00f;
+   background-color: transparent;
+   text-decoration: underline;
+   border: none;
+   cursor: pointer;
+   cursor: hand;
+  }
+</style>
+</head>
+ <form method="post" action="ajax/addnote.php?buildid=<?php echo $buildid?>&userid=<?php echo $userid; ?>">
+ <table>
+ <tr>
+ <td><b>Note:</b></td>
+ <td> <textarea name="TextNote" cols="50" rows="5"></textarea></td>
+ </tr>
+ <tr>
+ <td><b>Status:</b></td>
+ <td><select name="Status">
+  <option value="0">Simple Note</option>
+  <option value="1">Fix in progress</option>
+  <option value="2">Fixed</option>
+ </select></td>
+ </tr>
+ <tr>
+ <td></td>
+ <td> <input name="AddNote" type="submit" value="Add Note"></td>
+ </tr>
+ </table>
+
+
+ </form>
+</html>
---
+++ ajax/buildnote.php
@@ -0,0 +1,45 @@
+<html>
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $RCSfile: common.php,v $
+  Language:  PHP
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+include("../config.php");
+include("../common.php");
+
+$buildid = $_GET["buildid"];
+
+$db = mysql_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+mysql_select_db("$CDASH_DB_NAME",$db);
+
+// Find the notes 
+$note = mysql_query("SELECT * FROM buildnote WHERE buildid='$buildid' ORDER BY timestamp ASC");
+while($note_array = mysql_fetch_array($note))
+  {
+  $userid = $note_array["userid"];
+  $user_array = mysql_fetch_array(mysql_query("SELECT firstname,lastname FROM user WHERE id='$userid'"));
+  $timestamp = strtotime($note_array["timestamp"]." UTC");
+  switch($note_array["status"])
+    {
+    case 0: echo "<b>[note] </b>"; break;
+    case 1: echo "<b>[fix in progress] </b>"; break;
+    case 2: echo "<b>[fixed] </b>"; break;
+    }
+  echo "by <b>".$user_array["firstname"]." ".$user_array["lastname"]."</b>"." (".date("H:i:s T",$timestamp).")";
+   echo "<pre>".substr($note_array["note"],0,100)."</pre>"; // limit 100 chars
+  }
+?>
+
+</html>
