2cd4f927a089290b8eae3d269bb737e4cf955b3b
David Cole <david.cole@kitware.com>
2008-03-21 17:30:42 -0400

    ENH: Add a sql script to create a repository table and a project2repositories relational table.

---
+++ sql/cdash-table-repositories.sql
@@ -0,0 +1,20 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `repositories`
+--
+
+CREATE TABLE IF NOT EXISTS `repositories` (
+  `id` int(11) NOT NULL auto_increment,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `project2repositories`
+--
+
+CREATE TABLE IF NOT EXISTS `project2repositories` (
+  `projectid` int(11) NOT NULL,
+  `repositoryid` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
