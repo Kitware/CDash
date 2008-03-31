8601a8f33915434769ed6001a280a45dbaee0312
Julien Jomier <julien.jomier@kitware.com>
2008-03-31 21:29:47 +0200

    BUG: Fixed filename for CoverageLog

--- common.php
+++ common.php
@@ -309,12 +309,18 @@ function backup_xml_file($parser,$contents,$projectid)
   
  // If the file is other we append a number until we get a non existing file
  $i=1;
- while(($file=="Other.xml" || $file="CoverageLog.xml") && file_exists($filename))
-  {
-  $filename = $CDASH_BACKUP_DIRECTORY."/".get_project_name($projectid)."_".$sitename."_".$name."_".$stamp."_Other.".$i.".xml";
-  $i++;
-  }
- 
+ while(($file=="Other.xml" && file_exists($filename))
+   {
+   $filename = $CDASH_BACKUP_DIRECTORY."/".get_project_name($projectid)."_".$sitename."_".$name."_".$stamp."_Other.".$i.".xml";
+   $i++;
+   }
+   
+ while(($file=="CoverageLog.xml" && file_exists($filename))
+   {
+   $filename = $CDASH_BACKUP_DIRECTORY."/".get_project_name($projectid)."_".$sitename."_".$name."_".$stamp."_CoverageLog.".$i.".xml";
+   $i++;
+   }
+   
   if (!$handle = fopen($filename, 'w')) 
     {
     echo "Cannot open file ($filename)";
