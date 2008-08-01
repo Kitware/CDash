20ce7a5425c1a3ef045eeae074393b7d5d1e19ae
Julien Jomier <julien.jomier@kitware.com>
2008-08-01 14:22:09 +0200

    STYLE: Added extra line

--- viewBuildError.php
+++ viewBuildError.php
@@ -61,6 +61,7 @@ $buildname = $build_array["name"];
 $starttime = $build_array["starttime"];
 
 $xml .= get_cdash_dashboard_xml_by_name($projectname,$date);
+
 $xml .= "<menu>";
 $xml .= add_XML_value("back","index.php?project=".$projectname."&date=".get_dashboard_date_from_build_starttime($build_array["starttime"],$project_array["nightlytime"]));
 $previousbuildid = get_previous_buildid($projectid,$siteid,$buildtype,$buildname,$starttime);
