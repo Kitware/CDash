b69c13c2a88390dcc6eca7fe19dd877aebfe9d5f
Julien Jomier <julien.jomier@kitware.com>
2008-08-19 19:31:31 +0200

    COMP: Warnings

--- dailyupdates.php
+++ dailyupdates.php
@@ -509,7 +509,7 @@ function addDailyChanges($projectid)
   $project_array = pdo_fetch_array(pdo_query("SELECT nightlytime,name FROM project WHERE id='$projectid'"));
   $date = ""; // now
   list ($previousdate, $currentstarttime, $nextdate) = get_dates($date,$project_array["nightlytime"]);
-  $date = gmdate("Y-m-d", $currentstarttime);
+  $date = gmdate("Ymd", $currentstarttime);
   
   // Check if we already have it somwhere
   $query = pdo_query("SELECT id FROM dailyupdate WHERE projectid='$projectid' AND date='$date'");
@@ -520,7 +520,7 @@ function addDailyChanges($projectid)
     
     $updateid = pdo_insert_id("dailyupdate");    
     
-    $dates = get_related_dates($project_array["nightlytime"], $date);
+    $dates = get_related_dates($project_array["nightlytime"],$date);
     $commits = get_repository_commits($projectid, $dates);
     // Insert the commits
     foreach($commits as $commit)
