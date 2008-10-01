4a36a78388eb474ac0d896eaa680951c2bf67289
Julien Jomier <julien.jomier@kitware.com>
2008-10-01 13:59:52 +0200

    BUG: Missing config.php

--- sendemail.php
+++ sendemail.php
@@ -39,7 +39,8 @@ function checkEmailPreferences($emailcategory,$nwarnings,$nerrors,$nfailingtests
 function sendsummaryemail($projectid,$projectname,$dashboarddate,$groupid,
                           $nbuildwarnings,$nbuilderrors,$nfailingtests)
 {
-
+  include("config.php");
+  
   // Find the current updaters from the night using the dailyupdatefile table
   $summaryEmail = "";
   $query = "SELECT ".qid("user").".email,user2project.emailcategory FROM ".qid("user").",user2project,dailyupdate,dailyupdatefile WHERE 
@@ -101,8 +102,7 @@ function sendsummaryemail($projectid,$projectname,$dashboarddate,$groupid,
     $summaryemail_array = pdo_fetch_array(pdo_query("SELECT name FROM buildgroup WHERE id=$groupid"));
     add_last_sql_error("sendsummaryemail");
 
-    $title = "CDash [".$projectname."] - ".$summaryemail_array["name"];
-    $title .= " ".date(FMT_DATETIMETZ,strtotime($starttime." UTC"));
+    $title = "CDash [".$projectname."] - ".$summaryemail_array["name"]." Failures";
       
     $messagePlainText = "The \"".$summaryemail_array["name"]."\" group has either errors, warnings or test failures.\n";
     $messagePlainText .= "You have been identified as one of the authors who have checked in changes that are part of this submission ";
