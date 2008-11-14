9bc259142c8ac40bbc3a64ee313c3eabcc6d49e6
Julien Jomier <julien.jomier@kitware.com>
2008-11-14 21:47:35 +0100

    BUG: 8103: Timing test did not fail

--- ctestparser.php
+++ ctestparser.php
@@ -723,18 +723,26 @@ function compute_test_timing($buildid)
         $previoustimestd = $previoustest_array["timestd"];
         $previoustimestatus = $previoustest_array["timestatus"];
         
-        if($teststatus == "passed")
-          {  
-          // Update the mean and std
-          $timemean = (1-$weight)*$previoustimemean+$weight*$testtime;
-          $timestd = sqrt((1-$weight)*$previoustimestd*$previoustimestd + $weight*($testtime-$timemean)*($testtime-$timemean));
+        if($teststatus == "passed") // if the current test passed
+          {
+          if($timestatus>0 && $timestatus<=$projecttestmaxstatus) // if we are currently detecting the time changed we should use previous mean std
+            {
+            $timemean = $previoustimemean;
+            $timestd = $previoustimestd;
+            }
+          else
+            {  
+            // Update the mean and std
+            $timemean = (1-$weight)*$previoustimemean+$weight*$testtime;
+            $timestd = sqrt((1-$weight)*$previoustimestd*$previoustimestd + $weight*($testtime-$timemean)*($testtime-$timemean));
+            }
             
           // Check the current status
           if($previoustimestd<$projecttimestdthreshold)
             {
             $previoustimestd = $projecttimestdthreshold;
             }
-          
+            
           if($testtime > $previoustimemean+$projecttimestd*$previoustimestd) // only do positive std
             {
             $timestatus = $previoustimestatus+1; // flag
@@ -743,6 +751,7 @@ function compute_test_timing($buildid)
             {
             $timestatus = 0; // reset the time status to 0
             }
+          
           }
         else // the test failed so we just replicate the previous test time
           {
