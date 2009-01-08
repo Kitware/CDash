b81d33cf3ef12288204ef8f6ff9ea4a3f7851095
David Cole <david.cole@kitware.com>
2009-01-08 17:57:59 -0500

    BUG: Fix path to test.php -- so we don't accidentally pick up the test.php in php/PEAR.

--- models/build.php
+++ models/build.php
@@ -16,7 +16,7 @@
 
 =========================================================================*/
 // It is assumed that appropriate headers should be included before including this file
-include('test.php');
+include('models/test.php');
 
 class BuildUserNote
 {
