07d34d7f44b3ad214f051adfb900b6fd9f74a0b1
Patrice Cavezzan <pcavezzan@gmail.com>
2009-02-25 00:31:11 +0100

    Added tests data

--- testing/test_projectwebpage.php
+++ testing/test_projectwebpage.php
@@ -26,24 +26,46 @@ class ProjectWebPageTestCase extends KWWebTestCase
   function testAccessToWebPageProjectTest()
     {
     $this->login();
-    $this->createProjectTest4Db();
+    // first project necessary for testing
+    $name = 'BatchmakeExample';
+    $description = 'Project Batchmake test for cdash testing';
+    $this->createProject($name,$description);
+    $name = 'InsightExample';
+    $description = 'Project Insight test for cdash testing';
+    $this->createProject($name,$description);
 //    $this->clickLink('ProjectTest4Db');
-    $content = $this->connect($this->url.'/index.php?project=ProjectTest4Db');
+    $content = $this->connect($this->url.'/index.php?project=BatchmakeExample');
     if(!$content)
       {
       return;
       }
-    $this->assertText('ProjectTest4Db Dashboard');
+    $this->assertText('BatchmakeExample Dashboard');
+    $this->submission('BatchmakeExample');
+    $this->submission('InsightExample');
     }
-    
+
+
+  function testSubmission()
+    {
+    $query  = "SELECT id, stamp, name, type, generator,command FROM build";
+    $result = $this->db->query($query);
+    $expected = array('id'        => '1',
+                      'stamp'     => '20090223-0100-Nightly',
+                      'name'      => 'Win32-MSVC2009',
+                      'type'      => 'Nightly',
+                      'generator' => 'ctest2.6-patch 0',
+                      'command'   => 'F:\PROGRA~1\MICROS~1.0\Common7\IDE\VCExpress.exe BatchMake.sln /build Release /project ALL_BUILD'
+                      );
+    $this->assertEqual($result[0],$expected);
+    }
   
   // In case of the project does not exist yet
-  function createProjectTest4Db()
+  function createProject($name,$description)
     {
     $this->clickLink('[Create new project]');
-    $this->setField('name','ProjectTest4Db');
-    $this->setField('description','This is a project test for cdash');
-    $this->setField('public','0');
+    $this->setField('name',$name);
+    $this->setField('description',$description);
+    $this->setField('public','1');
     $this->clickSubmit('Create Project');
     return $this->clickLink('BACK');
     }
@@ -56,5 +78,46 @@ class ProjectWebPageTestCase extends KWWebTestCase
     $this->setField('passwd','simpletest');
     return $this->clickSubmit('Login >>');
     }
+    
+  function submission($projectname)
+    {
+    if(!strcmp($projectname,'BatchmakeExample'))
+      {
+      $rep = "data/BatchmakeNightlyExample";
+      $url = $this->url.'/submit.php?project=BatchmakeExample';
+      $this->uploadfile($url,"$rep/BatchMake_Dash20.kitware_Win32-MSVC2009_20090223-0100-Nightly_Build.xml");
+      $this->uploadfile($url,"$rep/BatchMake_Dash20.kitware_Win32-MSVC2009_20090223-0100-Nightly_Configure.xml");
+      $this->uploadfile($url,"$rep/BatchMake_Dash20.kitware_Win32-MSVC2009_20090223-0100-Nightly_Notes.xml");
+      $this->uploadfile($url,"$rep/BatchMake_Dash20.kitware_Win32-MSVC2009_20090223-0100-Nightly_Test.xml");
+      $this->uploadfile($url,"$rep/BatchMake_Dash20.kitware_Win32-MSVC2009_20090223-0100-Nightly_Update.xml");
+      return true;
+      }
+    elseif(!strcmp($projectname,'InsightExample'))
+      {
+      $url = $this->url.'/submit.php?project=InsightExample';
+      $rep = "data/InsightExperimentalExample";
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_Build.xml");
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_Configure.xml");
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_CoverageLog.xml");
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_Coverage.xml");
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_DynamicAnalysis.xml");
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_Notes.xml");
+      $this->uploadfile($url,"$rep/Insight_camelot.kitware_Linux-g++-4.1-LesionSizingSandbox_Debug_20090223-0710-Experimental_Test.xml");
+      return true;
+      }
+    }
+    
+  function uploadfile($url,$filename)
+    {
+    $fp = fopen($filename, 'r');
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_UPLOAD, 1);
+    curl_setopt($ch, CURLOPT_INFILE, $fp);
+    curl_setopt($ch, CURLOPT_INFILESIZE, filesize($filename));
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+    }  
 }
 ?>
