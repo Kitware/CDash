b53e584c3a8afcdd6944c0cca8011f86f6a420d1
Julien Jomier <julien.jomier@kitware.com>
2009-02-24 19:53:55 +0100

    ENH: Javascript

---
+++ javascript/cdashUserLabels.js
@@ -0,0 +1,22 @@
+var opt = new OptionTransfer("movelabels[]","emaillabels[]");
+opt.setAutoSort(true);
+opt.setDelimiter(",");
+
+function rightTransfer()
+{
+  opt.transferRight();
+  saveChanges();
+}
+
+function leftTransfer()
+{
+  opt.transferLeft();
+  saveChanges();  
+}
+
+function SubmitForm()
+{  
+  $('#emaillabels option').each(function(i) {  
+   $(this).attr("selected", "selected");  
+   });  
+}
