4ec389bbcc992317c2f176550fca4348d97236d1
Julien Jomier <julien.jomier@kitware.com>
2009-02-28 01:33:37 +0100

    ENH: Some fixes

--- testing/test_pubproject.php
+++ testing/test_pubproject.php
@@ -9,27 +9,17 @@ class ProjectInDbTestCase extends KWWebTestCase
   var $db  = null;
   
   function __construct()
-    {
-    parent::__construct();
-    require('config.test.php');
-    $this->url = $configure['urlwebsite'];
-    $this->db  =& new database($db['type']);
-    $this->db->setDb($db['name']);
-    $this->db->setHost($db['host']);
-    $this->db->setUser($db['login']);
-    $this->db->setPassword($db['pwd']);
-    }
- 
-  /** Helper function to login */
-  function login()
-    {
-    $this->clickLink('Login');
-    $this->setField('login','simpletest@localhost');
-    $this->setField('passwd','simpletest');
-    return $this->clickSubmit('Login >>');
-    }    
-    
-  /** Test the creation of the project */
+   {
+   parent::__construct();
+   require('config.test.php');
+   $this->url = $configure['urlwebsite'];
+   $this->db  =& new database($db['type']);
+   $this->db->setDb($db['name']);
+   $this->db->setHost($db['host']);
+   $this->db->setUser($db['login']);
+   $this->db->setPassword($db['pwd']);
+   }
+  
   function testCreateProject()
     {
     $content = $this->connect($this->url);
@@ -43,16 +33,15 @@ class ProjectInDbTestCase extends KWWebTestCase
       return;
       }
     
-    // Create the project
     $this->setField('name','ProjectTest');
     $this->setField('description','This is a project test for cdash');
     $this->setField('public','1');
     $this->clickSubmit('Create Project');
-         
-    // Make sure the project is in the database
+
+
     $query = "SELECT COUNT(*) FROM project";
     $result = $this->db->query($query);
-    if($this->db->getType() == "pgsql" && 
+    if( strcmp($this->db->getType(),"pgsql") == 0 && 
         $result[0]['count'] < 1)
       {
       $result = $result[0]['count'];  
@@ -61,7 +50,7 @@ class ProjectInDbTestCase extends KWWebTestCase
       $this->assertEqual($result[0]['count'],'1',$errormsg);
       return;
       }
-    else if($this->db->getType() == "mysql" && 
+    elseif(strcmp($this->db->getType(),"mysql") == 0 && 
            $result[0]['COUNT(*)'] < 1)
       {
       $result = $result[0]['COUNT(*)']; 
@@ -73,8 +62,7 @@ class ProjectInDbTestCase extends KWWebTestCase
     $this->assertText('The project ProjectTest has been created successfully.');
     }
   
-  /** Check that the project values in the database are correct */
-  function testProjectTestInDatabase()
+    function testProjectTestInDatabase()
     {
     $query = "SELECT name,description,public FROM project WHERE name = 'ProjectTest'";
     $result = $this->db->query($query);
@@ -87,24 +75,28 @@ class ProjectInDbTestCase extends KWWebTestCase
     $this->assertEqual($result[0],$expected);
     }
   
-  /** Test that we can access the project page */
   function testIndexProjectTest()
     {
     $content = $this->get($this->url.'/index.php?project=ProjectTest');
     $this->assertTitle('CDash - ProjectTest');
     }
   
-  /** Test the edition of the project */
   function testEditProject()
     {
     $content = $this->connect($this->url);
-    $this->assert($content);
-    
+    if(!$content)
+      {
+      return;
+      }
     $this->login();
     $projectid = $this->db->query("SELECT id FROM project WHERE name = 'ProjectTest'");
     $content = $this->connect($this->url.'/createProject.php?projectid='.$projectid[0]['id']);
-    $this->assert($content);
-  
+    if(!$content)
+      {
+      return;
+      }
+//  $this->analyse($this->clickLink('[Edit project]'));
+//  echo $this->analyse($this->setField('projectSelection','ProjectTest'));
     $description = $this->_browser->getField('description');
     $public      = $this->_browser->getField('public');
     $descriptionExpected = 'This is a project test for cdash';
@@ -141,8 +133,9 @@ class ProjectInDbTestCase extends KWWebTestCase
       $msg .= "The deletion of ProjectTest failed\n";
       $this->assertTrue(false,$msg);
       }
+  
     }
- 
+
   /** Test the deletion of a project */
   function testDeleteProject()
     {
@@ -169,7 +162,7 @@ class ProjectInDbTestCase extends KWWebTestCase
       }
    
     // Delete the project
-    $this->clickSubmit('Delete Project');$result[0]['count'];
+    $this->clickSubmit('Delete Project');
     
     // Check that it has been deleted correctly
     $result = $this->db->query("SELECT COUNT(*) FROM project");      
@@ -189,8 +182,17 @@ class ProjectInDbTestCase extends KWWebTestCase
       }
     $this->pass();  
     }
-    
-}
 
+
+
+  
+  function login()
+    {
+    $this->clickLink('Login');
+    $this->setField('login','simpletest@localhost');
+    $this->setField('passwd','simpletest');
+    return $this->clickSubmit('Login >>');
+    }
+}
 
 ?>
