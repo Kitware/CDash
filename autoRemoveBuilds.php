be9affbe547cb5a459a641e1a25af47892bbf30a
Julien Jomier <julien.jomier@kitware.com>
2009-04-09 20:39:46 +0200

    ENH: Added autoremove

---
+++ autoRemoveBuilds.php
@@ -0,0 +1,56 @@
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $Id$
+  Language:  PHP
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+include("cdash/autoremove.php");
+// Open the database connection
+include("cdash/config.php");
+require_once("cdash/pdo.php");
+
+if($argc != 2)
+{
+  print "Usage: php $argv[0] project_name\n";
+  exit(-1);
+}
+
+$db = pdo_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN","$CDASH_DB_PASS");
+pdo_select_db("$CDASH_DB_NAME",$db);
+set_time_limit(0);
+
+// Force to autoremove
+$CDASH_AUTOREMOVE_BUILDS='1';
+
+$projectname=$argv[1];
+print "removing builds for $projectname \n";
+$sql="";
+if($projectname == "all")
+  {
+  $sql = " WHERE name='".$projectname."'";
+  }
+
+$project = pdo_query("SELECT id,autoremovetimeframe,autoremovemaxbuilds FROM project".$sql);
+if(!$project)
+  {
+  add_last_sql_error("autoRemoveBuilds");
+  return false;
+  }  
+while($project_array = pdo_fetch_array($project))
+  {
+  $removeFirstBuilds($project_array["id"],$project_array["autoremovetimeframe"],$project_array["autoremovemaxbuilds"]);
+  }
+
+exit(0);
+?>
