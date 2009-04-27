577704bd6dcb97274d1428dd9927ad34a72829c8
Julien Jomier <julien.jomier@kitware.com>
2009-04-27 15:59:50 +0200

    ENH: Trying to fix iphone display

--- iphone/project.php
+++ iphone/project.php
@@ -5,9 +5,13 @@ set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
 $NoXSLGenerate = 1;
 include("../index.php");
+include("../models/project.php");
 
 @$projectname = $_GET["project"];
 $projectid = get_project_id($projectname);
+$Project = new Project();
+$Project->Id = $projectid;
+
 @$date = $_GET["date"];
 $xml = generate_main_dashboard_XML($projectid,$date);
 // Now doing the xslt transition
