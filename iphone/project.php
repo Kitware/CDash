6095db0d59e7c5bf9f914930ebee528dceda81bc
Julien Jomier <julien.jomier@kitware.com>
2009-04-27 15:55:58 +0200

    ENH: Trying to fix iphone display

--- iphone/project.php
+++ iphone/project.php
@@ -1,4 +1,8 @@
 <?php
+// Put the CDash root directory in the path
+$path = join(array_slice(split( "/" ,dirname(__FILE__)),0,-1),"/");
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
 $NoXSLGenerate = 1;
 include("../index.php");
 
