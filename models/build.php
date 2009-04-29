35863d370a4dc91b4a1be845cc02800fa62137b4
Julien Jomier <julien.jomier@kitware.com>
2009-04-29 13:29:19 +0200

    STYLE: Fix warning message

--- models/build.php
+++ models/build.php
@@ -996,7 +996,7 @@ class Build
              
       // Find the previous userstatistics
       $previous = pdo_query("SELECT totalupdatedfiles,totalbuilds,nfixedwarnings,nfailedwarnings,nfixederrors,nfailederrors,nfixedtests,nfailedtests
-                             FROM userstatistics WHERE userid='$userid' AND projectid=".qnum($this->ProjectId)." AND checkindate<'$checkindate' ORDER BY checkindate DESC LIMIT 1");
+                             FROM userstatistics WHERE userid='$userid' AND projectid='$projectid' AND checkindate<'$checkindate' ORDER BY checkindate DESC LIMIT 1");
       add_last_sql_error("compute_update_statistics");             
       if(pdo_num_rows($previous)>0)
         {
@@ -1130,7 +1130,7 @@ class Build
     {
     if(!$this->Id)
       {
-      echo "Build::GetNumberOfErrors(): BuildId not set";
+      echo "Build::GetNumberOfErrors(): Id not set";
       return false;    
       }
    
@@ -1148,7 +1148,7 @@ class Build
     {
     if(!$this->Id)
       {
-      echo "Build::GetNumberOfWarnings(): BuildId not set";
+      echo "Build::GetNumberOfWarnings(): Id not set";
       return false;    
       }
    
