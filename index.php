0fa1b1f142500969ba749285d7387925837d40ea
Julien Jomier <julien.jomier@kitware.com>
2009-08-04 16:18:11 +0200

    BUG: 9277: Database update is always needed

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ function generate_index_table()
   $xml .= "<date>".date("r")."</date>";
 
   // Check if the database is up to date
-  if(!pdo_query("SELECT cookiekey FROM user LIMIT 1"))
+  if(!pdo_query("SELECT cookiekey FROM ".qid("user")." LIMIT 1"))
     {  
     $xml .= "<upgradewarning>1</upgradewarning>";
     }
