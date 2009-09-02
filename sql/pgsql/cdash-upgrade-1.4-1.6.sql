6fcddb5dd90b4c527337ba46a91ee0ef985da6bd
Julien Jomier <julien.jomier@kitware.com>
2009-09-02 20:27:29 +0200

    ENH: Added table submission

--- sql/pgsql/cdash-upgrade-1.4-1.6.sql
+++ sql/pgsql/cdash-upgrade-1.4-1.6.sql
@@ -11,3 +11,16 @@ CREATE TABLE "coveragefilepriority" (
 CREATE INDEX "coveragefilepriority_priority" on "coveragefilepriority" ("priority");
 CREATE INDEX "coveragefilepriority_fullpath" on "coveragefilepriority" ("fullpath");
 CREATE INDEX "coveragefilepriority_projectid" on "coveragefilepriority" ("projectid");
+
+--
+-- Table: submission
+--
+CREATE TABLE "submission" (
+  "id" serial NOT NULL,
+  "fullpath" character varying(255) DEFAULT '' NOT NULL,
+  "projectid" bigint  NOT NULL,
+  "status" smallint NOT NULL,
+  PRIMARY KEY ("id")
+);
+CREATE INDEX "submission_projectid" on "submission" ("projectid");
+CREATE INDEX "submission_status" on "submission" ("status");
