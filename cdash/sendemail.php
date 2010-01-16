3e8233a66b9f728fd05e04b0d805080725b877f1
Julien Jomier <julien.jomier@kitware.com>
2010-01-16 01:19:23 +0100

    ENH: Added log

--- cdash/sendemail.php
+++ cdash/sendemail.php
@@ -872,7 +872,7 @@ function send_email_fix_to_user($userid,$emailtext,$Build,$Project)
     if(mail("$email", $title, $messagePlainText,
      "From: CDash <".$CDASH_EMAIL_FROM.">\nReply-To: ".$CDASH_EMAIL_REPLY."\nX-Mailer: PHP/" . phpversion()."\nMIME-Version: 1.0" ))
       {
-      add_log("email sent to: ".$email." with errors ".$titleerrors." for build ".$Build->Id,"sendemail ".$Project->Name,LOG_INFO);
+      add_log("email sent to: ".$email." with fixes ".$titleerrors." for build ".$Build->Id,"sendemail ".$Project->Name,LOG_INFO);
     
       // Record that we have send the email
       set_email_sent($userid,$Build->Id,$emailtext);
