4df22b7f822fee9242d56444819d409abacf9d0b
Zack Galbreath <zack.galbreath@kitware.com>
2010-06-16 13:15:50 -0400

    ENH: new coverage test for viewMap.php

---
+++ testing/test_viewmap.php
@@ -0,0 +1,36 @@
+<?php
+// kwtest library
+require_once('kwtest/kw_web_tester.php');
+require_once('kwtest/kw_db.php');
+
+class ViewMapTestCase extends KWWebTestCase
+{
+  var $url           = null;
+  var $db            = null;
+  var $projecttestid = null;
+  
+  function __construct()
+    {
+    parent::__construct();
+    require('config.test.php');
+    $this->url = $configure['urlwebsite'];
+    $this->db  =& new database($db['type']);
+    $this->db->setDb($db['name']);
+    $this->db->setHost($db['host']);
+    $this->db->setUser($db['login']);
+    $this->db->setPassword($db['pwd']);
+    $this->projectid = -1;
+    }
+
+  function testViewMap()
+    {
+    $this->get($this->url."/viewMap.php?project=InsightExample");
+    if(strpos($this->getBrowser()->getContentAsText(), "Maintainer") === false)
+      {
+      $this->fail("'Maintainer' not found when expected");
+      return 1;
+      }
+    $this->pass("Passed");
+    }
+}
+?>
