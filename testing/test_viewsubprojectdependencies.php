c0d90f3586422fd7124a7772666b73135b76380c
Zack Galbreath <zack.galbreath@kitware.com>
2010-06-16 11:34:48 -0400

    ENH: new coverage test for viewSubprojectDependencies.php

---
+++ testing/test_viewsubprojectdependencies.php
@@ -0,0 +1,37 @@
+<?php
+// kwtest library
+require_once('kwtest/kw_web_tester.php');
+require_once('kwtest/kw_db.php');
+
+class ViewSubprojectDependenciesTestCase extends KWWebTestCase
+{
+  var $url           = null;
+  var $db            = null;
+  var $projecttestid = null;
+  
+  function __construct()
+    {
+    parent::__construct();
+    require('config.test.php');
+    $this->url = $configure['urlwebsite'];
+    $this->db  =& new database($db['type']);
+    $this->db->setDb($db['name']);
+    $this->db->setHost($db['host']);
+    $this->db->setUser($db['login']);
+    $this->db->setPassword($db['pwd']);
+    $this->projectid = -1;
+    }
+
+  function testViewSubprojectDependencies()
+    {
+    $this->login();
+    $this->get($this->url."/viewSubprojectDependencies.php?project=SubprojectExample");
+    if(strpos($this->getBrowser()->getContentAsText(), "Komplex") === false)
+      {
+      $this->fail("'Komplex' not found when expected");
+      return 1;
+      }
+    $this->pass("Passed");
+    }
+}
+?>
