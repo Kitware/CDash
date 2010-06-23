22f6ae245ee673338e1917459a2321c0adc1a48d
Zack Galbreath <zack.galbreath@kitware.com>
2010-06-23 14:59:36 -0400

    ENH: new test for findusers.php; rename previous test to finduserproject

--- testing/selenium/test_finduserproject.php
+++ testing/selenium/test_finduserproject.php
@@ -15,7 +15,7 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->webPath = $configure['webpath'];
   }
 
-  public function testFindUsers()
+  public function testFindUserProject()
   {
     $this->open($this->webPath."/index.php");
     $this->click("link=Login");
@@ -24,17 +24,18 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->type("passwd", "simpletest");
     $this->click("sent");
     $this->waitForPageToLoad("30000");
-    $this->click("link=[Manage users]");
+    $this->click("//tr[5]/td[2]/a[7]/img");
     $this->waitForPageToLoad("30000");
+    $this->click("//div[@id='wizard']/ul/li[2]/a/span");
     $this->type("search", "simple");
     $this->keyUp("search", "e");
     sleep(1);
-    $this->click("makeadmin");
+    $this->select("//div[@id='newuser']/table/tbody/tr[1]/td[2]/font[1]/form/select", "label=Site maintainer");
+    $this->select("//div[@id='newuser']/table/tbody/tr[1]/td[2]/font[1]/form/select", "label=Normal User");
+    $this->type("//div[@id='newuser']/table/tbody/tr[1]/td[2]/font[1]/form/input[2]", "simple");
+    $this->click("adduser");
     $this->waitForPageToLoad("30000");
-    $this->type("search", "simpl");
-    $this->keyUp("search", "l");
-    sleep(1);
-    $this->click("makenormaluser");
+    $this->click("//div[@id='fragment-1']/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td[6]/input[2]");
     $this->waitForPageToLoad("30000");
   }
 }
