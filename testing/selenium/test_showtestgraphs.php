8a269b697bc7d2b1cca0b4b91369306ae4625c6d
Zack Galbreath <zack.galbreath@kitware.com>
2010-06-23 12:30:00 -0400

    ENH: new selenium test for testing graphs

---
+++ testing/selenium/test_showtestgraphs.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $path = dirname(__FILE__)."/..";
+    set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+    require('config.test.php');
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl($configure['webserver']);
+    $this->webPath = $configure['webpath'];
+  }
+
+  public function testShowTestGraphs()
+  {
+    $this->open($this->webPath."/index.php?project=EmailProjectExample&date=2009-02-23");
+    $this->open("/CDash/index.php?project=EmailProjectExample&date=2009-02-23");
+    $this->click("//table[@id='project_3_7']/tbody[1]/tr[1]/td[12]/div/a");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Failed");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=[Show Test Time Graph]");
+    $this->click("link=[Zoom out]");
+    $this->click("link=[Show Test Time Graph]");
+    $this->click("link=[Show Failing/Passing Graph]");
+    $this->click("link=[Zoom out]");
+    $this->click("link=[Show Failing/Passing Graph]");
+    $this->click("link=DashboardSendTest");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=[Show Test Failure Trend]");
+    $this->click("link=[Zoom out]");
+    $this->click("link=[Show Test Failure Trend]");
+  }
+}
+?>
