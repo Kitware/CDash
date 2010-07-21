d63d2836a3f94419a607c652a646236391efcfa9
Julien Jomier <julien.jomier@kitware.com>
2010-07-21 15:23:42 +0200

    ENH: Trying to fix PgSQL errors

--- siteStatistics.php
+++ siteStatistics.php
@@ -38,24 +38,47 @@ $xml .= "<title>CDash - Sites Statistics</title>";
 $xml .= "<menutitle>CDash</menutitle>";
 $xml .= "<menusubtitle>Site Statistics</menusubtitle>";
 
-$query = pdo_query("SELECT siteid,sitename, SEC_TO_TIME(SUM(elapsed)) AS busytime FROM
-(
-SELECT site.id AS siteid,site.name AS sitename, project.name AS projectname, build.name AS buildname, build.type, 
-AVG(TIME_TO_SEC(TIMEDIFF(submittime, buildupdate.starttime))) AS elapsed
-FROM build, buildupdate, project, site
-WHERE
-  submittime > TIMESTAMPADD(".qiv("HOUR").", -168, NOW())
-  AND buildupdate.buildid = build.id
-  AND site.id = build.siteid
-  AND build.projectid = project.id
-  GROUP BY sitename,projectname,buildname,type
-  ORDER BY elapsed DESC
-)
-AS summary
-GROUP BY sitename
-ORDER BY busytime DESC
-");
 
+if($CDASH_DB_TYPE == "pgsql") 
+  {
+  $query = pdo_query("SELECT siteid,sitename, SUM(elapsed) AS busytime FROM
+  (
+  SELECT site.id AS siteid,site.name AS sitename, project.name AS projectname, build.name AS buildname, build.type, 
+  AVG(submittime - buildupdate.starttime) AS elapsed
+  FROM build, buildupdate, project, site
+  WHERE
+    submittime > NOW()- interval '168 hours'
+    AND buildupdate.buildid = build.id
+    AND site.id = build.siteid
+    AND build.projectid = project.id
+    GROUP BY sitename,projectname,buildname,build.type,site.id
+    ORDER BY elapsed DESC
+  )
+  AS summary
+  GROUP BY sitename,summary.siteid
+  ORDER BY busytime DESC
+  ");
+  }
+else
+  {
+  $query = pdo_query("SELECT siteid,sitename, SEC_TO_TIME(SUM(elapsed)) AS busytime FROM
+  (
+  SELECT site.id AS siteid,site.name AS sitename, project.name AS projectname, build.name AS buildname, build.type, 
+  AVG(TIME_TO_SEC(TIMEDIFF(submittime, buildupdate.starttime))) AS elapsed
+  FROM build, buildupdate, project, site
+  WHERE
+    submittime > TIMESTAMPADD(".qiv("HOUR").", -168, NOW())
+    AND buildupdate.buildid = build.id
+    AND site.id = build.siteid
+    AND build.projectid = project.id
+    GROUP BY sitename,projectname,buildname,build.type
+    ORDER BY elapsed DESC
+  )
+  AS summary
+  GROUP BY sitename
+  ORDER BY busytime DESC
+  "); 
+  }  
 echo pdo_error();
 while($query_array = pdo_fetch_array($query))
 {
