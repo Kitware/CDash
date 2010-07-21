cf7ea4b00dd431ffe90ea6587a50bac4dded2d8f
Julien Jomier <julien.jomier@kitware.com>
2010-07-21 13:48:39 +0200

    BUG: Fixing issue with PgSQL

--- api/api_build.php
+++ api/api_build.php
@@ -191,8 +191,7 @@ class BuildAPI extends CDashAPI
     
     $date = date("Y-m-d");
     list ($previousdate, $currentstarttime, $nextdate) = get_dates($date,$project_array["nightlytime"]); 
-    $currentUTCTime =  gmdate(FMT_DATETIME,$currentstarttime);
-    
+    $currentUTCTime =  date(FMT_DATETIME,$currentstarttime);
     
     // Get all the unique builds for the section of the dashboard
     if($CDASH_DB_TYPE == "pgsql") 
@@ -202,7 +201,9 @@ class BuildAPI extends CDashAPI
                FROM build AS b, site AS s, siteinformation AS si, buildgroup AS bg, build2group AS b2g
                WHERE b.projectid=".$projectid." AND b.siteid=s.id AND si.siteid=s.id 
                AND bg.name='".$group."' AND b.testfailed>0 AND b2g.buildid=b.id AND b2g.groupid=bg.id
-               AND b.starttime>'$currentUTCTime' AND b.starttime<NOW() GROUP BY fullname ORDER BY buildid"); 
+               AND b.starttime>'$currentUTCTime' AND b.starttime<NOW() GROUP BY fullname,
+               s.name,b.name,si.totalphysicalmemory,si.processorclockfrequency
+               ORDER BY buildid"); 
       }
     else
       {  
@@ -230,6 +231,11 @@ class BuildAPI extends CDashAPI
       $sites[$query_array['buildid']] = $site;
       } 
 
+    if(empty($sites))
+      {
+      return $sites;
+      }
+      
     $query = pdo_query("SELECT bt.buildid AS buildid,t.name AS testname,t.id AS testid
               FROM build2test AS bt,test as t
               WHERE bt.buildid IN (".$buildids.") AND bt.testid=t.id AND bt.status='failed'");
