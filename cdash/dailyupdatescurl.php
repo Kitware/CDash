502deebf7146ba51c0d7c1a4abb5334a6accf97c
David Cole <david.cole@kitware.com>
2010-08-20 18:20:50 -0400

    Fix include path for dailyupdatescurl.php. Use the simpler expression that was developed and proven to work as part of the recent tests re-factoring. A bad include path (on Windows) was the root cause of the failed showupdategraph test on the hut11.kitware dashboard.

--- cdash/dailyupdatescurl.php
+++ cdash/dailyupdatescurl.php
@@ -1,9 +1,12 @@
 <?php
-// Put the CDash root directory in the path
-$path = join(array_slice(split( "/" ,dirname(__FILE__)),0,-1),"/");
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+// To be able to access files in this CDash installation regardless
+// of getcwd() value:
+//
+$cdashpath = dirname(dirname(__FILE__));
+set_include_path($cdashpath . PATH_SEPARATOR . get_include_path());
 
-include("cdash/dailyupdates.php");
+require_once("cdash/dailyupdates.php");
+
 $projectid = $_GET['projectid'];
 addDailyChanges($projectid);
 ?>
--- cdash/processsubmissions.php
+++ cdash/processsubmissions.php
@@ -19,7 +19,7 @@
 // To be able to access files in this CDash installation regardless
 // of getcwd() value:
 //
-$cdashpath = str_replace('\\', '/', dirname(dirname(__FILE__)));
+$cdashpath = dirname(dirname(__FILE__));
 set_include_path($cdashpath . PATH_SEPARATOR . get_include_path());
 
 require_once("cdash/common.php");
