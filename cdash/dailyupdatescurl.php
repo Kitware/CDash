5a983b527f9545604c945e4f3c1f078b07e00126
Zach Mullen <zach.mullen@kitware.com>
2010-09-20 09:31:33 -0400

    BUG: Add ignore_user_abort call in dailyupdatescurl so that the script won't stop running after the 1 second timeout.

--- cdash/dailyupdatescurl.php
+++ cdash/dailyupdatescurl.php
@@ -7,6 +7,10 @@ set_include_path($cdashpath . PATH_SEPARATOR . get_include_path());
 
 require_once("cdash/dailyupdates.php");
 
+ob_start();
+set_time_limit(0);
+ignore_user_abort(TRUE);
+
 $projectid = $_GET['projectid'];
 addDailyChanges($projectid);
 ?>
