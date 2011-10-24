b30814d035d7acd2c1c852932dc75375eefcc026
David Cole <david.cole@kitware.com>
2011-10-24 11:08:37 -0400

    BUG: Add js.projectid to the GROUP BY clause since it is not aggregated. Thanks to Postgres for being more strict than MySQL.

--- models/clientjobschedule.php
+++ models/clientjobschedule.php
@@ -672,7 +672,7 @@ class ClientJobSchedule
       AND (os.osid IS NULL OR os.osid=s.osid)
       AND js.startdate<'".$now."' AND (js.enddate='1980-01-01 00:00:00' OR js.enddate>'".$now."')
       AND js.enable=1
-      GROUP BY js.id,js.lastrun,js.starttime,js.repeattime
+      GROUP BY js.id,js.lastrun,js.starttime,js.repeattime,js.projectid
       ";
     $query=pdo_query($sql);
     if(!$query)
