98cfbaf967386630f0f2afe749b517a274284b50
Julien Jomier <julien.jomier@kitware.com>
2012-12-19 10:15:15 +0100

    BUG: Missing SQL new tables

--- sql/mysql/cdash.sql
+++ sql/mysql/cdash.sql
@@ -1338,6 +1338,17 @@ CREATE TABLE IF NOT EXISTS `submission2ip` (
   PRIMARY KEY (`submissionid`)
 );
 
+CREATE TABLE IF NOT EXISTS `measurement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `projectid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `testpage` tinyint(1) NOT NULL,
+  `summarypage` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `projectid` (`projectid`),
+  KEY `name` (`name`)
+);
+
 --
 -- Change the table maximum size to be more than 4GB
 -- 
