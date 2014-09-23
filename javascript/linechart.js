2242d78e2f82969613aed167ee79a3b5f0862031
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-09-23 12:22:08 -0400

    ENH: Tighten up X axis on new line charts
    From: Zack Galbreath <zack.galbreath@kitware.com>

--- javascript/linechart.js
+++ javascript/linechart.js
@@ -1,5 +1,7 @@
 function makeLineChart(chartName, elementName, inputData, date) {
   jQuery(function(){
+
+    // setup the chart
     var chart = $.jqplot (elementName, [inputData], {
       axes:{
         xaxis:{
@@ -15,5 +17,14 @@ function makeLineChart(chartName, elementName, inputData, date) {
         show: false
       }
     });
+
+    // Change X axis to tightly fit the data.
+    var highest_value = chart.axes.xaxis._dataBounds.max;
+    chart.axes.xaxis.max = highest_value;
+
+    var lowest_value = chart.axes.xaxis._dataBounds.min;
+    chart.axes.xaxis.min = lowest_value;
+
+    chart.replot();
   });
 }
