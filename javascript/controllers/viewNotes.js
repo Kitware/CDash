bc991a7b4c96ca66740ad4f377bac7d842696eec
Betsy McPhail <betsy.mcphail@gmail.com>
2015-09-09 15:23:14 -0400

    Add table of contents to top of View Notes page
    This way, it is easy to navigate to different notes.

--- javascript/controllers/viewNotes.js
+++ javascript/controllers/viewNotes.js
@@ -1,13 +1,28 @@
-CDash.controller('ViewNotesController',
-  function ViewNotesController($scope, $http) {
-    $scope.loading = true;
-    $http({
-      url: 'api/v1/viewNotes.php',
-      method: 'GET',
-      params: queryString
-    }).success(function(cdash) {
-      $scope.cdash = cdash;
-    }).finally(function() {
-      $scope.loading = false;
-    });
-});
+function ViewNotesController($scope, $http, $location, $anchorScroll) {
+  $scope.loading = true;
+  $http({
+    url: 'api/v1/viewNotes.php',
+    method: 'GET',
+    params: queryString
+  }).success(function(cdash) {
+    $scope.cdash = cdash;
+  }).finally(function() {
+    $scope.loading = false;
+  });
+
+  $scope.gotoNote = function(x) {
+    var newHash = 'note' + x;
+    if ($location.hash() !== newHash) {
+      // set the $location.hash to `newHash` and
+      // $anchorScroll will automatically scroll to it
+      $location.hash('note' + x);
+    } else {
+      // call $anchorScroll() explicitly,
+      // since $location.hash hasn't changed
+      $anchorScroll();
+    }
+  };
+}
+
+CDash.controller('ViewNotesController', ['$scope', '$http', '$location', '$anchorScroll',
+                                          ViewNotesController]);
