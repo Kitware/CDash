1dd86402f36bc1493ad84ea7b44743a206ed394f
Zack Galbreath <zack.galbreath@kitware.com>
2015-10-21 08:53:30 -0400

    pagination for viewTest
    This prevents sluggish performance when a build has lots of tests.

--- javascript/cdash_angular.js
+++ javascript/cdash_angular.js
@@ -106,3 +106,17 @@ CDash.factory('multisort', function () {
     }
   };
 });
+
+CDash.directive('convertToNumber', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(val) {
+        return parseInt(val, 10);
+      });
+      ngModel.$formatters.push(function(val) {
+        return '' + val;
+      });
+    }
+  };
+});
