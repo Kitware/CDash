8945d34ed309c97f89c2a9135d45799aba9c8d01
Zack Galbreath <zack.galbreath@kitware.com>
2015-10-23 16:16:39 +0000

    Fix XML encoding error
    We encountered a problem where manageProjectRoles.php would not load
    properly.  The PHP error log had the following clue:
    
    Warning:  DOMDocument::loadXML():
    Input is not proper UTF-8, indicate encoding !
    
    I eventually tracked this down to a user with an accented 'i' in their
    name.

--- cdash/common.php
+++ cdash/common.php
@@ -1923,7 +1923,7 @@ function begin_XML_for_XSLT()
       }
   }
 
-    $xml = '<?xml version="1.0" encoding="utf-8"?><cdash>';
+    $xml = '<?xml version="1.0" encoding="ISO-8859-1"?><cdash>';
     $xml .= add_XML_value("cssfile", $CDASH_CSS_FILE);
     $xml .= add_XML_value("version", $CDASH_VERSION);
     return $xml;
