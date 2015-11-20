7e570a9e21b4f540e690691e314b48d57225ec9a
Zack Galbreath <zack.galbreath@kitware.com>
2015-11-20 09:19:21 -0500

    Modify iphone test to cover redirects

--- tests/test_iphone.php
+++ tests/test_iphone.php
@@ -14,25 +14,30 @@ class iPhoneTestCase extends KWWebTestCase
 
     public function testiPhone()
     {
-        $this->get($this->url."/mobile/index.php");
-        if (strpos($this->getBrowser()->getContentAsText(), "BatchmakeExample") === false) {
-            $this->fail("'BatchmakeExample' not found when expected");
-            return 1;
-        }
-        $this->get($this->url."/mobile/project.php?project=BatchmakeExample");
-        if (strpos($this->getBrowser()->getContentAsText(), "Continuous") === false) {
-            $this->fail("'Continuous' not found when expected");
-            return 1;
-        }
-        $this->get($this->url."/mobile/user.php");
-        if (strpos($this->getBrowser()->getContentAsText(), "Wrong login or password") === false) {
-            $this->fail("'Wrong login or password' not found when expected");
-            return 1;
-        }
-        $this->get($this->url."/mobile/buildsummary.php?buildid=1");
-        if (strpos($this->getBrowser()->getContentAsText(), "Number of Updates") === false) {
-            $this->fail("'Number of Updates' not found when expected");
-            return 1;
+        // We now redirect from 'iphone' to 'mobile'.
+        // Make sure both URLs work properly.
+        $dirs = array('mobile', 'iphone');
+        foreach ($dirs as $dir) {
+            $this->get($this->url."/$dir/index.php");
+            if (strpos($this->getBrowser()->getContentAsText(), "BatchmakeExample") === false) {
+                $this->fail("'BatchmakeExample' not found when expected");
+                return 1;
+            }
+            $this->get($this->url."/$dir/project.php?project=BatchmakeExample");
+            if (strpos($this->getBrowser()->getContentAsText(), "Continuous") === false) {
+                $this->fail("'Continuous' not found when expected");
+                return 1;
+            }
+            $this->get($this->url."/$dir/user.php");
+            if (strpos($this->getBrowser()->getContentAsText(), "Wrong login or password") === false) {
+                $this->fail("'Wrong login or password' not found when expected");
+                return 1;
+            }
+            $this->get($this->url."/$dir/buildsummary.php?buildid=1");
+            if (strpos($this->getBrowser()->getContentAsText(), "Number of Updates") === false) {
+                $this->fail("'Number of Updates' not found when expected");
+                return 1;
+            }
         }
         $this->pass("Passed");
     }
