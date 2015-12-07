70025a5be423b7d1971d1a7cc16c6fa7e9e115b7
Zack Galbreath <zack.galbreath@kitware.com>
2015-12-07 14:30:35 -0500

    Use gulp to uglify instead of grunt
    One less dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,75 @@
 (function () {
 
-    var gulp = require('gulp'),
-        eslint = require('gulp-eslint');
+  var gulp = require('gulp'),
+      eslint = require('gulp-eslint'),
+      del = require('del'),
+      sourcemaps = require('gulp-sourcemaps'),
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify'),
+      rename = require("gulp-rename"),
+      replace = require('gulp-replace'),
+      timestamp = new Date().getTime();
 
-    gulp.task('quality', function() {
 
-        gulp.src(['javascript/**/*_angular.js',
-                  'javascript/controllers/**.js'])
-            .pipe(eslint({}))
-            .pipe(eslint.format())
-            .pipe(eslint.failAfterError());
-    });
+  gulp.task('quality', function() {
+    gulp.src(['javascript/**/*_angular.js',
+              'javascript/controllers/**.js'])
+        .pipe(eslint({}))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+  });
 
-    gulp.task('default', ['quality']);
+
+  gulp.task('clean', function () {
+    return del.sync([
+      'public/build/*',
+      'public/js/CDash_*.min.js*'
+    ]);
+  });
+
+
+ gulp.task('uglify', function() {
+   gulp.src(['public/js/jquery-1.10.2.js',
+             'public/js/jquery-ui-1.10.4.min.js',
+             'public/js/jquery.cookie.js',
+             'public/js/jquery.flot.min.js',
+             'public/js/jquery.flot.navigate.min.js',
+             'public/js/jquery.flot.selection.min.js',
+             'public/js/jquery.flot.time.min.js',
+             'public/js/jquery.qtip.min.js',
+             'public/js/jqModal.js',
+             'public/js/bootstrap.min.js',
+             'public/js/tooltip.js',
+             'public/js/cdashmenu.js',
+             'public/js/cdashIndexTable.js',
+             'public/js/cdashSortable.js',
+             'public/js/tabNavigation.js',
+             'public/js/je_compare.js',
+             'public/js/angular-1.4.7.min.js',
+             'public/js/angular-animate.min.js',
+             'public/js/angular-ui-sortable.min.js',
+             'public/js/ui-bootstrap-tpls-0.14.2.min.js',
+             'public/js/cdash_angular.js',
+             'public/js/controllers/**.js'])
+       .pipe(sourcemaps.init())
+       .pipe(concat('CDash.concat.js'))
+       .pipe(uglify({mangle: false}))
+       .pipe(rename('CDash_' + timestamp + '.min.js'))
+       .pipe(sourcemaps.write('./'))
+       .pipe(gulp.dest('public/js'));
+ });
+
+
+  gulp.task('replace', function(){
+    gulp.src(['public/views/*.html'])
+        .pipe(replace('@@timestamp', timestamp))
+        .pipe(gulp.dest('public/build/views/'));
+
+    gulp.src(['public/local/views/*.html'])
+        .pipe(replace('@@timestamp', timestamp))
+        .pipe(gulp.dest('public/build/local/views/'));
+  });
+
+
+  gulp.task('default', ['quality', 'clean', 'uglify', 'replace']);
 }());
