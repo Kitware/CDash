3780d15712c894aae553197f7fa408f3af601ce5
Jamie Snape <jamiesnape@users.noreply.github.com>
2016-03-01 10:12:45 -0500

    Add missing license header

--- tests/test_aggregatecoverage.php
+++ tests/test_aggregatecoverage.php
@@ -1,4 +1,17 @@
 <?php
+/*=========================================================================
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $Id$
+  Language:  PHP
+  Date:      $Date$
+  Version:   $Revision$
+  Copyright (c) Kitware, Inc. All rights reserved.
+  See LICENSE or http://www.cdash.org/licensing/ for details.
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE. See the above copyright notices for more information.
+=========================================================================*/
+
 //
 // After including cdash_test_case.php, subsequent require_once calls are
 // relative to the top of the CDash source tree
