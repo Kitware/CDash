6758548e48aadf898bce13f2c2b1b7ca144e3b58
Zack Galbreath <zack.galbreath@kitware.com>
2016-03-07 10:29:14 -0500

    Simplify api/v1/compareCoverage.php
    Reduce the complexity of this file by eliminating its support
    for filters, which weren't available to the users anyway.
    
    If at some point in the future we decide to support filters on
    this page we can add them to this now-simplified version.

--- public/compareCoverage.php
+++ public/compareCoverage.php
@@ -1,4 +1,9 @@
 <?php
+// If the project name is not set we display the table of projects.
+if (!isset($_GET['project'])) {
+    header('Location: viewProjects.php');
+    exit;
+}
 require_once dirname(__DIR__).'/config/config.php';
 include_once 'include/common.php';
 load_view('compareCoverage');
