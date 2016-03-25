e0c51a9113c4573a0a429e0cdc915369cfe0535d
Jamie Snape <jamie.snape@kitware.com>
2016-03-25 13:42:47 -0400

    Use system temp directory when extracting coverage files

--- xml_handlers/GcovTar_handler.php
+++ xml_handlers/GcovTar_handler.php
@@ -60,19 +60,10 @@ class GCovTarHandler
     /**
      * Parse a tarball of .gcov files.
      **/
-    public function Parse($handle)
+    public function Parse($filename)
     {
-        global $CDASH_BACKUP_DIRECTORY;
-
-        // This function receives an open file handle, but we really just need
-        // the path to this file so that we can extract it.
-        $meta_data = stream_get_meta_data($handle);
-        $filename = $meta_data['uri'];
-        fclose($handle);
-
         // Create a new directory where we can extract our tarball.
-        $pathParts = pathinfo($filename);
-        $dirName = $CDASH_BACKUP_DIRECTORY . '/' . $pathParts['filename'];
+        $dirName = sys_get_temp_dir() . '/' . pathinfo($filename, PATHINFO_FILENAME);
         mkdir($dirName);
 
         // Extract the tarball.
