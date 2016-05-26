a116cba09d8ee083ed477b10ecd73c46f70b6bb4
Zack Galbreath <zack.galbreath@kitware.com>
2016-05-26 10:13:37 -0400

    Upgrade path for dynamicanalysissummary
    Create entries in the summary table for previously existing
    dynamic analysis builds.  This commit also adds a test case
    for this upgrade function.

--- tests/test_dynamicanalysissummary.php
+++ tests/test_dynamicanalysissummary.php
@@ -24,6 +24,9 @@ class DynamicAnalysisSummaryTestCase extends KWWebTestCase
         parent::__construct();
         $this->deleteLog($this->logfilename);
         $this->DataDir = dirname(__FILE__) . '/data/DynamicAnalysisSummary';
+        $this->ParentId = 0;
+        $this->StandaloneBuildId = 0;
+        $this->ChildIds = array();
     }
 
     public function testDynamicAnalysisSummary()
@@ -35,16 +38,7 @@ class DynamicAnalysisSummaryTestCase extends KWWebTestCase
             $this->fail("Failed to submit $file");
             return 1;
         }
-
-        // Verify the expected number of defects.
-        $row = pdo_single_row_query("
-                SELECT das.numdefects FROM build AS b
-                INNER JOIN dynamicanalysissummary AS das ON (das.buildid=b.id)
-                WHERE b.name = 'Linux-g++-4.1-LesionSizingSandbox_Debug'");
-        $numdefects = $row['numdefects'];
-        if ($numdefects != 225) {
-            $this->fail("Expected 225, found $numdefects\n");
-        }
+        $this->VerifyStandaloneBuild();
         $this->pass('Test passed');
     }
 
@@ -58,35 +52,34 @@ class DynamicAnalysisSummaryTestCase extends KWWebTestCase
                 return 1;
             }
         }
+        $this->VerifySubProjectBuild();
+        $this->pass('Test passed');
+    }
 
-        $result = pdo_query("
-                SELECT b.id, b.parentid, das.numdefects FROM build AS b
-                INNER JOIN dynamicanalysissummary AS das ON (das.buildid=b.id)
-                WHERE b.name = 'cross_subproject_DA_example'");
-        $num_builds = pdo_num_rows($result);
-        if ($num_builds != 4) {
-            $this->fail("Expected 4 builds, found $num_builds");
-        }
+    public function testDynamicAnalysisSummaryUpgrade()
+    {
+        // Delete the summaries examined by this test.
+        $ids = $this->ChildIds;
+        $ids[] = $this->ParentId;
+        $ids[] = $this->StandaloneBuildId;
+        $id_arg = implode(', ', $ids);
+        pdo_query(
+            "DELETE FROM dynamicanalysissummary WHERE buildid IN ($id_arg)");
 
-        $parentid = 0;
-        while ($row = pdo_fetch_array($result)) {
-            $numdefects = $row['numdefects'];
-            if ($row['parentid'] == -1) {
-                // Parent case.
-                $parentid = $row['id'];
-                if ($numdefects != 3) {
-                    $this->fail("Expected 3 defects for parent, found $numdefects");
-                }
-            } else {
-                // Child case.
-                if ($numdefects != 1) {
-                    $this->fail("Expected 1 defect for child, found $numdefects");
-                }
-            }
-        }
+        // Run the upgrade function.
+        require_once 'include/upgrade_functions.php';
+        PopulateDynamicAnalysisSummaryTable();
 
+        // Verify the summaries are still correct.
+        $this->VerifyStandaloneBuild();
+        $this->VerifySubProjectBuild();
+        $this->pass('Test passed');
+    }
+
+    public function testDynamicAnalysisSummaryGetsDeleted()
+    {
         // Remove the builds we just created.
-        remove_build($parentid);
+        remove_build($this->ParentId);
 
         // Verify that the dynamicanalysisssummary rows got deleted.
         $result = pdo_query("
@@ -98,5 +91,50 @@ class DynamicAnalysisSummaryTestCase extends KWWebTestCase
             $this->fail("Expected 0 builds after deletion, found $num_builds");
         }
         $this->pass('Test passed');
+    }
+
+    public function VerifyStandaloneBuild()
+    {
+        // Verify the expected number of defects.
+        // Get the ID of this build.
+        $row = pdo_single_row_query("
+                SELECT b.id, das.numdefects FROM build AS b
+                INNER JOIN dynamicanalysissummary AS das ON (das.buildid=b.id)
+                WHERE name = 'Linux-g++-4.1-LesionSizingSandbox_Debug'");
+        $this->StandaloneBuildId = $row['id'];
+        $numdefects = $row['numdefects'];
+        if ($numdefects != 225) {
+            $this->fail("Expected 225, found $numdefects\n");
+        }
+    }
+
+    public function VerifySubProjectBuild()
+    {
+        $result = pdo_query("
+                SELECT b.id, b.parentid, das.numdefects FROM build AS b
+                INNER JOIN dynamicanalysissummary AS das ON (das.buildid=b.id)
+                WHERE b.name = 'cross_subproject_DA_example'");
+        $num_builds = pdo_num_rows($result);
+        if ($num_builds != 4) {
+            $this->fail("Expected 4 builds, found $num_builds");
+        }
+
+        $this->ParentId = 0;
+        while ($row = pdo_fetch_array($result)) {
+            $numdefects = $row['numdefects'];
+            if ($row['parentid'] == -1) {
+                // Parent case.
+                $this->ParentId = $row['id'];
+                if ($numdefects != 3) {
+                    $this->fail("Expected 3 defects for parent, found $numdefects");
+                }
+            } else {
+                // Child case.
+                $this->ChildIds[] = $row['id'];
+                if ($numdefects != 1) {
+                    $this->fail("Expected 1 defect for child, found $numdefects");
+                }
+            }
+        }
     }
 }
