2d933898c1b6b66481d4991f0d232f817412f3c8
Zack Galbreath <zack.galbreath@kitware.com>
2017-02-08 12:00:07 -0500

    Use models and PDO in viewDynamicAnalysis.php
    Stop using pdo_query in this script

--- public/viewDynamicAnalysis.php
+++ public/viewDynamicAnalysis.php
@@ -14,40 +14,40 @@
   PURPOSE. See the above copyright notices for more information.
 =========================================================================*/
 
-$noforcelogin = 1;
-include dirname(__DIR__) . '/config/config.php';
+require_once dirname(__DIR__) . '/config/config.php';
 require_once 'include/pdo.php';
+$noforcelogin = 1;
 include 'public/login.php';
-include_once 'include/common.php';
-include 'include/version.php';
+require_once 'include/common.php';
+require_once 'include/version.php';
+require_once 'models/build.php';
+require_once 'models/project.php';
+require_once 'models/site.php';
 
-@$buildid = $_GET['buildid'];
-if ($buildid != null) {
-    $buildid = pdo_real_escape_numeric($buildid);
+// Make sure a valid buildid was specified.
+if (!isset($_GET['buildid']) || !is_numeric($_GET['buildid'])) {
+    echo 'Not a valid buildid!';
+    return;
 }
+$buildid = $_GET['buildid'];
 
 @$date = $_GET['date'];
 if ($date != null) {
     $date = htmlspecialchars(pdo_real_escape_string($date));
 }
 
-// Checks
-if (!isset($buildid) || !is_numeric($buildid)) {
-    echo 'Not a valid buildid!';
-    return;
-}
-
-$db = pdo_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN", "$CDASH_DB_PASS");
-pdo_select_db("$CDASH_DB_NAME", $db);
-
-$build_array = pdo_fetch_array(pdo_query("SELECT starttime,projectid,siteid,type,name FROM build WHERE id='$buildid'"));
-$projectid = $build_array['projectid'];
-if (!isset($projectid) || $projectid == 0) {
+$build = new Build();
+$build->Id = $buildid;
+if (!$build->Exists()) {
     echo "This build doesn't exist. Maybe it has been deleted.";
     exit();
 }
 
-checkUserPolicy(@$_SESSION['cdash']['loginid'], $projectid);
+$build->FillFromId($buildid);
+
+checkUserPolicy(@$_SESSION['cdash']['loginid'], $build->ProjectId);
+
+$pdo = get_link_identifier()->getPdo();
 
 // lookup table to make the reported defect types more understandable.
 // feel free to expand as necessary.
@@ -56,32 +56,25 @@ $defect_nice_names = array(
     'IPR' => 'Invalid Pointer Read',
     'IPW' => 'Invalid Pointer Write');
 
-$project = pdo_query("SELECT * FROM project WHERE id='$projectid'");
-if (pdo_num_rows($project) > 0) {
-    $project_array = pdo_fetch_array($project);
-    $projectname = $project_array['name'];
-}
+$project = new Project();
+$project->Id = $build->ProjectId;
+$project->Fill();
 
 $xml = begin_XML_for_XSLT();
-$xml .= '<title>CDash : ' . $projectname . '</title>';
+$xml .= '<title>CDash : ' . $project->Name . '</title>';
 
-$xml .= get_cdash_dashboard_xml_by_name($projectname, $date);
-
-$siteid = $build_array['siteid'];
-$buildtype = $build_array['type'];
-$buildname = $build_array['name'];
-$starttime = $build_array['starttime'];
+$xml .= get_cdash_dashboard_xml_by_name($project->Name, $date);
 
 $xml .= '<menu>';
-$xml .= add_XML_value('back', 'index.php?project=' . urlencode($projectname) . '&date=' . get_dashboard_date_from_build_starttime($build_array['starttime'], $project_array['nightlytime']));
-$previousbuildid = get_previous_buildid_dynamicanalysis($projectid, $siteid, $buildtype, $buildname, $starttime);
+$xml .= add_XML_value('back', 'index.php?project=' . urlencode($project->Name) . '&date=' . get_dashboard_date_from_build_starttime($build->StartTime, $project->NightlyTime));
+$previousbuildid = get_previous_buildid_dynamicanalysis($build->ProjectId, $build->SiteId, $build->Type, $build->Name, $build->StartTime);
 if ($previousbuildid > 0) {
     $xml .= add_XML_value('previous', 'viewDynamicAnalysis.php?buildid=' . $previousbuildid);
 } else {
     $xml .= add_XML_value('noprevious', '1');
 }
-$xml .= add_XML_value('current', 'viewDynamicAnalysis.php?buildid=' . get_last_buildid_dynamicanalysis($projectid, $siteid, $buildtype, $buildname, $starttime));
-$nextbuildid = get_next_buildid_dynamicanalysis($projectid, $siteid, $buildtype, $buildname, $starttime);
+$xml .= add_XML_value('current', 'viewDynamicAnalysis.php?buildid=' . get_last_buildid_dynamicanalysis($build->ProjectId, $build->SiteId, $build->Type, $build->Name, $build->StartTime));
+$nextbuildid = get_next_buildid_dynamicanalysis($build->ProjectId, $build->SiteId, $build->Type, $build->Name, $build->StartTime);
 if ($nextbuildid > 0) {
     $xml .= add_XML_value('next', 'viewDynamicAnalysis.php?buildid=' . $nextbuildid);
 } else {
@@ -91,32 +84,39 @@ $xml .= '</menu>';
 
 // Build
 $xml .= '<build>';
-$site_array = pdo_fetch_array(pdo_query("SELECT name FROM site WHERE id='$siteid'"));
-$xml .= add_XML_value('site', $site_array['name']);
-$xml .= add_XML_value('buildname', $build_array['name']);
+$site = new Site();
+$site->Id = $build->SiteId;
+$site_name = $site->GetName();
+$xml .= add_XML_value('site', $site_name);
+$xml .= add_XML_value('buildname', $build->Name);
 $xml .= add_XML_value('buildid', $buildid);
-$xml .= add_XML_value('buildtime', $build_array['starttime']);
+$xml .= add_XML_value('buildtime', $build->StartTime);
 $xml .= '</build>';
 
 // dynamic analysis
 $i = 0;
-$dynamicanalysis = pdo_query("SELECT * FROM dynamicanalysis WHERE buildid='$buildid' ORDER BY status DESC");
+$DA_stmt = $pdo->prepare(
+    'SELECT * FROM dynamicanalysis WHERE buildid = ? ORDER BY status DESC');
+pdo_execute($DA_stmt, [$buildid]);
+
 $defect_types = array();
-while ($dynamicanalysis_array = pdo_fetch_array($dynamicanalysis)) {
+while ($DA_row = $DA_stmt->fetch()) {
     $xml .= '<dynamicanalysis>';
     if ($i % 2 == 0) {
         $xml .= add_XML_value('bgcolor', '#b0c4de');
     }
     $i++;
-    $xml .= add_XML_value('status', ucfirst($dynamicanalysis_array['status']));
-    $xml .= add_XML_value('name', $dynamicanalysis_array['name']);
-    $xml .= add_XML_value('id', $dynamicanalysis_array['id']);
+    $xml .= add_XML_value('status', ucfirst($DA_row['status']));
+    $xml .= add_XML_value('name', $DA_row['name']);
+    $xml .= add_XML_value('id', $DA_row['id']);
 
-    $dynid = $dynamicanalysis_array['id'];
-    $defects = pdo_query("SELECT * FROM dynamicanalysisdefect WHERE dynamicanalysisid='$dynid'");
-    while ($defects_array = pdo_fetch_array($defects)) {
+    $dynid = $DA_row['id'];
+    $defects_stmt = $pdo->prepare(
+        'SELECT * FROM dynamicanalysisdefect WHERE dynamicanalysisid = ?');
+    pdo_execute($defects_stmt, [$dynid]);
+    while ($defects_row = $defects_stmt->fetch()) {
         // defects
-        $defect_type = $defects_array['type'];
+        $defect_type = $defects_row['type'];
         if (array_key_exists($defect_type, $defect_nice_names)) {
             $defect_type = $defect_nice_names[$defect_type];
         }
@@ -127,7 +127,7 @@ while ($dynamicanalysis_array = pdo_fetch_array($dynamicanalysis)) {
 
         $xml .= '<defect>';
         $xml .= add_XML_value('column', $column);
-        $xml .= add_XML_value('value', $defects_array['value']);
+        $xml .= add_XML_value('value', $defects_row['value']);
         $xml .= '</defect>';
     }
 
