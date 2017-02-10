fe0c16d5225ebb9ec234cd5f7c534370e9b0ff9c
Bryon Bean <bryon.bean@kitware.com>
2017-02-10 07:29:01 -0500

    modified ui to accomodate missing tests

--- tests/js/e2e_tests/viewTest.js
+++ tests/js/e2e_tests/viewTest.js
@@ -5,4 +5,34 @@ describe("viewTest", function() {
     expect(browser.getPageSource()).toContain("kwsys.testHashSTL");
   });
 
+  describe("Missing Tests", function() {
+
+    beforeEach(function(){
+        /* browser.get('index.php?project=EmailProjectExample');
+         element(by.cssContainingText('a','Win32-MSVC2009')).click(); */
+
+        // Test number one cannot have a filtered applied via the query string
+        // so for now this id needs to be hard coded
+        browser.get('viewTest.php?buildid=98835');
+    });
+
+    it("should indicate the number of tests missing", function(){
+      var h3 = element(by.id('test-totals-indicator'));
+
+      expect(h3.getText()).toEqual('2 passed, 3 failed, 0 timed out, 0 not run, 3 missing.');
+    });
+
+    it("should display missing tests", function() {
+
+
+      var parser1Test1 = element(by.cssContainingText('tr','Parser1Test1'));
+      var dashboardSendTest = element(by.cssContainingText('tr','DashboardSendTest'));
+      var systemInfoTest = element(by.cssContainingText('tr','SystemInfoTest'));
+
+      expect(parser1Test1.getText()).toEqual('Parser1Test1 Missing');
+      expect(dashboardSendTest.getText()).toEqual('DashboardSendTest Missing');
+      expect(systemInfoTest.getText()).toEqual('SystemInfoTest Missing');
+    });
+  })
+
 });
