fc57eef867b35384db5c40ffb9e4f7457956fd4b
Zack Galbreath <zack.galbreath@kitware.com>
2017-04-21 15:25:06 -0400

    Remove redundant database connection
    This happens automatically when you include pdo.php

--- public/user.php
+++ public/user.php
@@ -38,8 +38,6 @@ $userid = $_SESSION['cdash']['loginid'];
 $xml = begin_XML_for_XSLT();
 $xml .= add_XML_value('manageclient', $CDASH_MANAGE_CLIENTS);
 
-$db = pdo_connect("$CDASH_DB_HOST", "$CDASH_DB_LOGIN", "$CDASH_DB_PASS");
-pdo_select_db("$CDASH_DB_NAME", $db);
 $xml .= add_XML_value('title', 'CDash - My Profile');
 
 $user = new User();
