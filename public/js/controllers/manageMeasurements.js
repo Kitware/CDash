cb64a0e40d26ec65753af0ce34459980157e8c5a
Zack Galbreath <zack.galbreath@kitware.com>
2017-08-29 14:59:32 -0400

    restore DELETE functionality

--- public/js/controllers/manageMeasurements.js
+++ public/js/controllers/manageMeasurements.js
@@ -1,4 +1,31 @@
 CDash.controller('ManageMeasurementsController',
-  function ManageMeasurementsController($scope, apiLoader) {
+  function ManageMeasurementsController($http, $scope, apiLoader, modalSvc) {
     apiLoader.loadPageData($scope, 'api/v1/manageMeasurements.php');
+
+    // Display confirmation dialog.
+    $scope.confirmDelete = function (measurement) {
+      modalSvc.showModal(measurement.id, $scope.removeMeasurement, 'modal-template');
+    }
+
+    // Remove measurement upon confirmation.
+    $scope.removeMeasurement = function(id_to_remove) {
+      var parameters = {
+        projectid: $scope.cdash.projectid,
+        id: id_to_remove
+      };
+      $http({
+        url: 'api/v1/manageMeasurements.php',
+        method: 'DELETE',
+        params: parameters
+      }).then(function success() {
+        // Find the measurement to remove.
+        for (var i = 0, len = $scope.cdash.measurements.length; i < len; i++) {
+          if ($scope.cdash.measurements[i].id === id_to_remove) {
+            // Remove it from our scope.
+            $scope.cdash.measurements.splice(i, 1);
+            break;
+          }
+        }
+      });
+    };
 });
