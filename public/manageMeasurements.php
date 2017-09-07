019b86f794b5a6a5450b14a330580ea6d6270b12
Zack Galbreath <zack.galbreath@kitware.com>
2017-09-07 12:28:48 -0400

    Fix test measurements
    * Restore manageMeasurements.php to working order
    * Update manageMeasurements.php to use PDO & AngularJS
    * Restore display of additional test columns on viewTest.php and testSummary.php
    * Add tests to protect these features

--- public/manageMeasurements.php
+++ public/manageMeasurements.php
@@ -4,6 +4,7 @@
   Module:    $Id$
   Language:  PHP
   Date:      $Date$
+  Version:   $Revision$
 
   Copyright (c) Kitware, Inc. All rights reserved.
   See LICENSE or http://www.cdash.org/licensing/ for details.
@@ -11,121 +12,8 @@
   This software is distributed WITHOUT ANY WARRANTY; without even
   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
   PURPOSE. See the above copyright notices for more information.
-  Copyright (c) 2012 Volkan Gezer <volkangezer@gmail.com>
 =========================================================================*/
 
-include dirname(__DIR__) . '/config/config.php';
-require_once 'include/pdo.php';
-include 'public/login.php';
+require_once dirname(__DIR__) . '/config/config.php';
 include_once 'include/common.php';
-include_once 'models/project.php';
-include_once 'models/user.php';
-
-if ($session_OK) {
-    $projectid = pdo_real_escape_numeric($_REQUEST['projectid']);
-    checkUserPolicy(@$_SESSION['cdash']['loginid'], $projectid);
-
-    // Checks
-    if (!isset($projectid) || !is_numeric($projectid)) {
-        echo 'Not a valid projectid!';
-        return;
-    }
-
-    $project = pdo_query("SELECT * FROM project WHERE id='$projectid'");
-    if (pdo_num_rows($project) > 0) {
-        $project_array = pdo_fetch_array($project);
-        $projectname = $project_array['name'];
-        $nightlytime = $project_array['nightlytime'];
-    }
-
-    $submit = $_POST['submit'];
-    $nameN = htmlspecialchars(pdo_real_escape_string($_POST['nameN']));
-    $showTN = htmlspecialchars(pdo_real_escape_string($_POST['showTN']));
-    $showSN = htmlspecialchars(pdo_real_escape_string($_POST['showSN']));
-
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-
-    // Start operation if it is submitted
-    if ($submit == 'Save') {
-        if ($nameN) {
-            pdo_query("INSERT INTO measurement (projectid,name,testpage,summarypage) VALUES ('$projectid','$nameN','$showTN','$showSN')"); // only write a new entry if new field is filled
-        }
-        $i = 0;
-
-        if (count($_POST['name'])) {
-            foreach ($name as $newName) { // everytime update all test attributes
-                $showT = $_POST['showT'];
-                $showS = $_POST['showS'];
-                if ($showT[$id[$i]] == '') {
-                    $showT[$id[$i]] = 0;
-                }
-                if ($showS[$id[$i]] == '') {
-                    $showS[$id[$i]] = 0;
-                }
-                pdo_query("UPDATE measurement SET name='$newName', testpage='" . $showT[$id[$i]] . "', summarypage='" . $showS[$id[$i]] . "' WHERE id='" . $id[$i] . "'");
-                $i++;
-            }
-        }
-    }
-    $selection = $_POST['select'];
-    if ($_POST['del'] && count($selection) > 0) { // if user chose any named measurement delete them
-        foreach ($selection as $del) {
-            pdo_query("DELETE FROM measurement WHERE id='$del'");
-        }
-    }
-
-    $xml = begin_XML_for_XSLT();
-    $xml .= '<backurl>user.php</backurl>';
-    $xml .= '<title>CDash - ' . $projectname . ' Measurements</title>';
-    $xml .= '<menutitle>' . $projectname . '</menutitle>';
-    $xml .= '<menusubtitle>Measurements</menusubtitle>';
-
-    if ($projectid > 0) {
-        $Project = new Project;
-        $Project->Id = $projectid;
-        $xml .= '<project>';
-        $xml .= add_XML_value('id', $projectid);
-        $xml .= add_XML_value('name', $Project->GetName());
-        $xml .= add_XML_value('name_encoded', urlencode($Project->GetName()));
-
-        $xml .= '</project>';
-    }
-
-    // Menu
-    $xml .= '<menu>';
-
-    $nightlytime = get_project_property($projectname, 'nightlytime');
-    $xml .= add_XML_value('back', 'index.php?project=' . urlencode($projectname) . '&date=' . get_dashboard_date_from_build_starttime($build_array['starttime'], $nightlytime));
-
-    $xml .= add_XML_value('noprevious', '1');
-    $xml .= add_XML_value('nonext', '1');
-    $xml .= '</menu>';
-    {
-        $userid = $_SESSION['cdash']['loginid'];
-        $user->Id = $userid;
-        $user->Fill();
-        $xml .= '<user>';
-        $xml .= add_XML_value('id', $userid);
-        $xml .= add_XML_value('admin', $user->Admin);
-        $xml .= '</user>';
-    }
-
-    //get any measurements associated with this test
-    $xml .= '<measurements>';
-    $query = "SELECT id,name,testpage,summarypage FROM measurement WHERE projectid='$projectid' ORDER BY name ASC";
-    $result = pdo_query($query);
-    while ($row = pdo_fetch_array($result)) {
-        $xml .= '<measurement>';
-        $xml .= add_XML_value('id', $row['id']);
-        $xml .= add_XML_value('name', $row['name']);
-        $xml .= add_XML_value('showT', $row['testpage']);
-        $xml .= add_XML_value('showS', $row['summarypage']);
-        $xml .= '</measurement>';
-    }
-    $xml .= '</measurements>';
-    $xml .= '</cdash>';
-
-    // Now doing the xslt transition
-    generate_XSLT($xml, 'manageMeasurements');
-}
+load_view('manageMeasurements');
---
+++ tests/test_managemeasurements.php
@@ -0,0 +1,158 @@
+<?php
+//
+// After including cdash_test_case.php, subsequent require_once calls are
+// relative to the top of the CDash source tree
+//
+require_once dirname(__FILE__) . '/cdash_test_case.php';
+
+require_once 'include/common.php';
+require_once 'include/pdo.php';
+require_once 'models/project.php';
+
+class ManageMeasurementsTestCase extends KWWebTestCase
+{
+    private $testDataFiles;
+    private $testDataDir;
+    private $builds;
+    private $parentBuilds;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->PDO = get_link_identifier()->getPdo();
+        $this->BuildId = null;
+        $this->MeasurementId = null;
+    }
+
+    public function __destruct()
+    {
+        if (!is_null($this->BuildId)) {
+            remove_build($this->BuildId);
+        }
+        if (!is_null($this->MeasurementId)) {
+            $this->PDO->exec(
+                "DELETE FROM measurement WHERE id = $this->MeasurementId");
+        }
+    }
+
+    public function testManageMeasurements()
+    {
+        // Submit a test file with a named measurement.
+        $testDataFile = dirname(__FILE__) .  '/data/TestMeasurements/Test.xml';
+        if (!$this->submission('InsightExample', $testDataFile)) {
+            $this->fail("Failed to submit test data");
+            return false;
+        }
+
+        // Get the ID of the build we just created.
+        $stmt = $this->PDO->query(
+            "SELECT id FROM build WHERE name = 'test_measurements_example'");
+        $this->BuildId = $stmt->fetchColumn();
+        if (!$this->BuildId > 0) {
+            $this->fail("Expected positive integer for build ID, found $this->BuildId");
+        }
+
+        // Login as admin.
+        $client = new GuzzleHttp\Client(['cookies' => true]);
+        global $CDASH_BASE_URL;
+        try {
+            $response = $client->request('POST',
+                    $CDASH_BASE_URL . '/user.php',
+                    ['form_params' => [
+                        'login' => 'simpletest@localhost',
+                        'passwd' => 'simpletest',
+                        'sent' => 'Login >>']]);
+        } catch (GuzzleHttp\Exception\ClientException $e) {
+            $this->fail($e->getMessage());
+            return false;
+        }
+
+        // POST to manageMeasurements.php to add 'Processors' as a
+        // test measurement for this project.
+        $projectid = get_project_id('InsightExample');
+        $measurements = [];
+        $measurements[] = [
+            'id' => -1,
+            'name' => 'Processors',
+            'summarypage' => 1,
+            'testpage' => 1
+        ];
+        try {
+            $response = $client->request('POST',
+                    $CDASH_BASE_URL . '/api/v1/manageMeasurements.php',
+                    ['json' => ['projectid' => $projectid, 'measurements' => $measurements]]);
+        } catch (GuzzleHttp\Exception\ClientException $e) {
+            $this->fail($e->getMessage());
+            return false;
+        }
+
+        // Response should have the ID of the newly created measurement.
+        $response_array = json_decode($response->getBody(), true);
+        $this->MeasurementId = $response_array['id'];
+        if (!$this->MeasurementId > 0) {
+            $this->fail("Expected positive integer for measurement ID, found $this->MeasurementId");
+        }
+
+        // Check that the measurement actually got added to the database.
+        $stmt = $this->PDO->query(
+            "SELECT id FROM measurement WHERE id = $this->MeasurementId");
+        $found = $stmt->fetchColumn();
+        if ($found != $this->MeasurementId) {
+            $this->fail("Expected $this->MeasurementId but found $found for DB measurement ID");
+        }
+
+        // Verify that the 'Processors' measurement is displayed on viewTest.php.
+        $this->get($this->url . "/api/v1/viewTest.php?buildid=$this->BuildId");
+        $content = $this->getBrowser()->getContent();
+        $jsonobj = json_decode($content, true);
+        $found = $jsonobj['columncount'];
+        if ($found != 1) {
+            $this->fail("Expected 1 extra column on viewTest.php, found $found");
+        }
+        $found = $jsonobj['columnnames'][0];
+        if ($found != 'Processors') {
+            $this->fail("Expected extra column to be called 'Processors', found $found");
+        }
+        $found = count($jsonobj['tests']);
+        if ($found != 2) {
+            $this->fail("Expected two tests, found $found");
+        }
+        foreach ($jsonobj['tests'] as $test) {
+            $test_name = $test['name'];
+            $num_procs = $test['measurements'][0];
+            if ($test_name == 'Test3Procs') {
+                if ($num_procs != 3) {
+                    $this->fail("Expected 3 processors on viewTest.php, found $num_procs");
+                }
+            } elseif ($test_name == 'Test5Procs') {
+                if ($num_procs != 5) {
+                    $this->fail("Expected 5 processors on viewTest.php, found $num_procs");
+                }
+            } else {
+                $this->fail("Unexpected test $test_name");
+            }
+        }
+
+        // Verify that 'Processors' is also displayed on testSummary.php.
+        $this->get($this->url . "/api/v1/testSummary.php?project=$projectid&name=Test5Procs&date=2017-08-29");
+        $content = $this->getBrowser()->getContent();
+        $jsonobj = json_decode($content, true);
+        $found = $jsonobj['columncount'];
+        if ($found != 1) {
+            $this->fail("Expected 1 extra column on testSummary.php, found $found");
+        }
+        $found = $jsonobj['columns'][0];
+        if ($found != 'Processors') {
+            $this->fail("Expected extra column to be called 'Processors', found $found");
+        }
+        $found = count($jsonobj['builds']);
+        if ($found != 1) {
+            $this->fail("Expected one build, found $found");
+        }
+        $found = $jsonobj['builds'][0]['measurements'][0];
+        if ($found != 5) {
+            $this->fail("Expected 5 processors on testSummary.php, found $found");
+        }
+    }
+}
