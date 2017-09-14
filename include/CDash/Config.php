639bc5426f6632e9eac8c239bc6437c52645d18a
Bryon Bean <bryon.bean@kitware.com>
2017-09-14 11:13:13 -0400

    feat: adds Config and Singleton classes bryon.bean@kitware.com

---
+++ include/CDash/Config.php
@@ -0,0 +1,88 @@
+<?php
+namespace CDash;
+
+use CDash\Singleton;
+
+class Config extends Singleton
+{
+    private $_config;
+
+    protected function __construct()
+    {
+        include 'config/config.php';
+        $this->_config = get_defined_vars();
+    }
+
+    public function get($name)
+    {
+        if (isset($this->_config[$name])) {
+            return $this->_config[$name];
+        }
+    }
+
+    public function set($name, $value)
+    {
+        $this->_config[$name] = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getServer()
+    {
+        $server = $this->get('CDASH_SERVER_NAME');
+        if (empty($server)) {
+            $server = $_SERVER['SERVER_NAME'];
+        }
+        return $server;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProtocol()
+    {
+        $protocol = 'http';
+        if ($this->get('CDASH_USE_HTTPS') ||
+            (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443))
+        {
+            $protocol = 'https';
+        }
+        return $protocol;
+    }
+
+    /**
+     * @return string
+     */
+    public function getServerPort()
+    {
+        if (isset($_SERVER['SERVER_PORT'])
+            && $_SERVER['SERVER_PORT'] != 80
+            && $_SERVER['SERVER_PORT'] != 443)
+        {
+            return $_SERVER['SERVER_PORT'];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        $uri = $this->get('CDASH_BASE_URL');
+
+        if (!$uri) {
+            $protocol = $this->getProtocol();
+            $server = $this->getServer();
+            $port = $this->getServerPort();
+            $uri = "{$protocol}://{$server}";
+
+            if ($port) {
+                $uri = "{$uri}:{$port}";
+            }
+
+            $uri = "{$uri}{$_SERVER['REQUEST_URI']}";
+        }
+        return $uri;
+    }
+}
---
+++ include/CDash/Singleton.php
@@ -0,0 +1,23 @@
+<?php
+namespace CDash;
+
+abstract class Singleton
+{
+    private static $_instances = [];
+
+    /**
+     * @return mixed
+     */
+    public static function getInstance()
+    {
+        if (!isset(self::$_instances[static::class])) {
+            self::$_instances[static::class] = new static;
+        }
+        return self::$_instances[static::class];
+    }
+
+    private function __construct() {}
+    private function __clone() {}
+    private function __sleep() {}
+    private function __wakeup() {}
+}
