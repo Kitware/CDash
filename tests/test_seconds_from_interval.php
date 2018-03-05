8a98804f250972e975b231cb551a2bc2cfa7f1d7
Bryon Bean <bryon.bean@kitware.com>
2018-03-05 09:54:56 -0500

    fix: logic takes into account extra day not needed between JAN 01 - JAN 28

--- tests/test_seconds_from_interval.php
+++ tests/test_seconds_from_interval.php
@@ -36,7 +36,10 @@ class SecondsFromIntervalTestCase extends KWWebTestCase
         $this->intervalTest('8 days 17h 24m 43s', 753883);
         // Not testing months since they have a variable number of seconds.
 
-        $leapYearSeconds = date('L', strtotime('2 years', time())) ? 86400 : 0;
+        // if 2 years ago was a leap year and the month is greater than Feb (2)
+        $extraDay = date('L', strtotime('2 years')) && date('n') > 2;
+        
+        $leapYearSeconds = $extraDay ? 86400 : 0;
         $this->intervalTest('2 years', 63072000 + $leapYearSeconds);
         $this->intervalTest('2 years 43s', 63072043 + $leapYearSeconds);
         $this->intervalTest('2 years 24m', 63073440 + $leapYearSeconds);
