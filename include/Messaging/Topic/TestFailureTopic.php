553385326e782ba733cc11c9fe1ebf33c776511c
Bryon Bean <bryon.bean@kitware.com>
2018-05-20 13:12:27 -0400

    test: adds doc and test for test failure topic

--- include/Messaging/Topic/TestFailureTopic.php
+++ include/Messaging/Topic/TestFailureTopic.php
@@ -4,6 +4,7 @@ namespace CDash\Messaging\Topic;
 use CDash\Model\Build;
 use CDash\Collection\CollectionCollection;
 use CDash\Collection\TestCollection;
+use CDash\Model\Test;
 
 class TestFailureTopic extends Topic implements DecoratableInterface
 {
@@ -15,6 +16,8 @@ class TestFailureTopic extends Topic implements DecoratableInterface
     }
 
     /**
+     * This method queries the build to check for failed tests
+     *
      * @param Build $build
      * @return bool
      */
@@ -26,6 +29,12 @@ class TestFailureTopic extends Topic implements DecoratableInterface
         return $subscribe;
     }
 
+    /**
+     * This method sets a build's failed tests in a TestCollection
+     *
+     * @param Build $build
+     * @return void
+     */
     public function setTopicData(Build $build)
     {
         $collection = $this->getTopicCollection();
@@ -37,6 +46,11 @@ class TestFailureTopic extends Topic implements DecoratableInterface
         }
     }
 
+    /**
+     * This method returns the TestCollection containing a build's failed tests
+     *
+     * @return \CDash\Collection\CollectionInterface|TestCollection
+     */
     public function getTopicCollection()
     {
         if (!$this->collection) {
@@ -45,11 +59,27 @@ class TestFailureTopic extends Topic implements DecoratableInterface
         return $this->collection;
     }
 
+    /**
+     * This method returns the subject of the topic
+     *
+     * @return string
+     *
+     * TODO: is it possible to create a convention where this method can be abstracted to simply:
+     *   return __CLASS__;
+     */
     public function getTopicName()
     {
         return self::TEST_FAILURE;
     }
 
+    /**
+     * This method checks its subscriber collection for the presence of labels which the subscriber
+     * is subscribed to. It then attempts to match those labels to the build's tests, returning
+     * true upon a match and false otherwise.
+     *
+     * @param Build $build
+     * @return bool
+     */
     public function hasLabels(Build $build)
     {
         $subscribed_labels = $this->subscriber->getLabels();
@@ -63,13 +93,19 @@ class TestFailureTopic extends Topic implements DecoratableInterface
         return false;
     }
 
-    public function getSubjectLabelCollection(\Test $subject)
+    /**
+     * This method will return the LabelCollection from a \CDash\Model\Test
+     *
+     * @param Test $subject
+     * @return \CDash\Collection\LabelCollection
+     */
+    public function getSubjectLabelCollection(Test $subject)
     {
         return $subject->GetLabelCollection();
     }
 
     /**
-     * @return \string[]
+     * @return string[]
      */
     public function getLabels()
     {
@@ -88,13 +124,17 @@ class TestFailureTopic extends Topic implements DecoratableInterface
     }
 
     /**
+     * This method will determine which of a Build's tests meet the criteria for adding to this
+     * topic's TestCollection.
+     *
      * @param Build $build
-     * @param $item
+     * @param Test $item
      * @return boolean
+     *
+     * TODO: maybe access on this should be protected?
      */
     public function itemHasTopicSubject(Build $build, $item)
     {
-        /** @var \Test $item */
         $criteria = $this->getTopicCallables();
         $hasTopicSubject = $item->HasFailed();
         foreach ($criteria as $criterion) {
--- include/Test/Case/Messaging/Topic/TestFailureTopicTest.php
+++ include/Test/Case/Messaging/Topic/TestFailureTopicTest.php
@@ -1,8 +1,54 @@
 <?php
 use CDash\Messaging\Topic\TestFailureTopic;
+use CDash\Messaging\Topic\Topic;
+use CDash\Model\Build;
 
 class TestFailureTopicTest extends \CDash\Test\CDashTestCase
 {
+    public function testSubscribesToBuild()
+    {
+        $sut = new TestFailureTopic();
+        $build = new Build();
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        $build->TestFailedCount = 0;
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        $build->TestFailedCount = 1;
+        $this->assertTrue($sut->subscribesToBuild($build));
+
+        $build->TestFailedCount = '1';
+        $this->assertTrue($sut->subscribesToBuild($build));
+
+        // Topics are decorators, so the creation of this mock topic is merely
+        // for testing all possible states of our SUT
+        $mock_topic = $this->getMockForAbstractClass(Topic::class);
+        $mock_topic
+            ->method('subscribesToBuild')
+            ->willReturnOnConsecutiveCalls(false, false, false, true, true);
+
+        $sut = new TestFailureTopic($mock_topic);
+        $build = new Build();
+        // parent does not subscribe to build and TestFailedCount is default value
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        // parent does not subscribe to build and TestFailedCount is 0
+        $build->TestFailedCount = 0;
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        // parent does not subscribe to build and TestFailedCount is 1
+        $build->TestFailedCount = 1;
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        // parent *subscribes* to build and TestFailedCount is 0
+        $build->TestFailedCount = 0;
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        // parent *subscribes* to build and TestFailedCount is 1
+        $build->TestFailedCount = 1;
+        $this->assertTrue($sut->subscribesToBuild($build));
+    }
+
     public function testGetTopicName()
     {
         $sut = new TestFailureTopic();
