06877120a10999db78dac92b3cb0eb4b8b3825d5
Bryon Bean <bryon.bean@kitware.com>
2018-05-20 13:07:29 -0400

    feat: adds DynamicAnalysisTopic and tests

--- include/Messaging/Topic/DynamicAnalysisTopic.php
+++ include/Messaging/Topic/DynamicAnalysisTopic.php
@@ -1,17 +1,50 @@
 <?php
 namespace CDash\Messaging\Topic;
 
+use CDash\Collection\DynamicAnalysisCollection;
 use CDash\Model\Build;
+use CDash\Model\DynamicAnalysis;
 
 class DynamicAnalysisTopic extends Topic implements DecoratableInterface
 {
+    protected static $statuses = [DynamicAnalysis::NOTRUN, DynamicAnalysis::FAILED];
+
+    /** @var DynamicAnalysisCollection $collection */
+    private $collection;
+
     /**
      * @param Build $build
      * @return bool
      */
     public function subscribesToBuild(Build $build)
     {
-        // TODO: Implement subscribesToBuild() method.
+        $ancestorSubscribe = is_null($this->topic) ? true : $this->topic->subscribesToBuild($build);
+        $collection = $build->GetDynamicAnalysisCollection();
+        $subscribe = false;
+        if ($ancestorSubscribe) {
+            foreach ($collection as $analysis) {
+                if (in_array($analysis->Status, self::$statuses)) {
+                    $subscribe = true;
+                    break;
+                }
+            }
+        }
+        return $subscribe;
+    }
+
+    /**
+     * @param Build $build
+     * @return Topic|void
+     */
+    public function setTopicData(Build $build)
+    {
+        $collection = $build->GetDynamicAnalysisCollection();
+        $analyses = $this->getTopicCollection();
+        foreach ($collection as $analysis) {
+            if ($this->itemHasTopicSubject($build, $analysis)) {
+                $analyses->add($analysis);
+            }
+        }
     }
 
     /**
@@ -19,7 +52,9 @@ class DynamicAnalysisTopic extends Topic implements DecoratableInterface
      */
     public function getTopicCount()
     {
-        // TODO: Implement getTopicCount() method.
+        if ($this->collection) {
+            return $this->collection->count();
+        }
     }
 
     /**
@@ -29,6 +64,25 @@ class DynamicAnalysisTopic extends Topic implements DecoratableInterface
      */
     public function itemHasTopicSubject(Build $build, $item)
     {
-        // TODO: Implement itemHasTopicSubject() method.
+        $criteria = $this->getTopicCallables();
+        $hasTopicSubject = in_array($item->Status, self::$statuses);
+        foreach ($criteria as $criterion) {
+            $hasTopicSubject = $hasTopicSubject && $criterion($build, $item);
+            if (!$hasTopicSubject) {
+                break;
+            }
+        }
+        return $hasTopicSubject;
+    }
+
+    /**
+     * @return \CDash\Collection\CollectionInterface|DynamicAnalysisCollection
+     */
+    public function getTopicCollection()
+    {
+        if (!$this->collection) {
+            $this->collection = new DynamicAnalysisCollection();
+        }
+        return $this->collection;
     }
 }
---
+++ include/Test/Case/Messaging/Topic/DynamicAnalysisTopicTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * =========================================================================
+ *   Program:   CDash - Cross-Platform Dashboard System
+ *   Module:    $Id$
+ *   Language:  PHP
+ *   Date:      $Date$
+ *   Version:   $Revision$
+ *   Copyright (c) Kitware, Inc. All rights reserved.
+ *   See LICENSE or http://www.cdash.org/licensing/ for details.
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE. See the above copyright notices for more information.
+ * =========================================================================
+ */
+
+use CDash\Collection\DynamicAnalysisCollection;
+use CDash\Messaging\Topic\DynamicAnalysisTopic;
+use CDash\Model\Build;
+use CDash\Model\DynamicAnalysis;
+
+class DynamicAnalysisTopicTest extends PHPUnit_Framework_TestCase
+{
+    public function testSubscribesToBuild()
+    {
+        $sut = new DynamicAnalysisTopic();
+        $build = new Build();
+
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        $analysis = new DynamicAnalysis();
+        $build->AddDynamicAnalysis($analysis);
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        $analysis->Status = DynamicAnalysis::PASSED;
+        $this->assertFalse($sut->subscribesToBuild($build));
+
+        $analysis->Status = DynamicAnalysis::FAILED;
+        $this->assertTrue($sut->subscribesToBuild($build));
+
+        $analysis->Status = DynamicAnalysis::NOTRUN;
+        $this->assertTrue($sut->subscribesToBuild($build));
+    }
+
+    public function testItemHasTopicSubject()
+    {
+        $sut = new DynamicAnalysisTopic();
+        $build = new Build();
+        $analysis = new DynamicAnalysis();
+
+        $this->assertFalse($sut->itemHasTopicSubject($build, $analysis));
+
+        $analysis->Status = DynamicAnalysis::PASSED;
+        $this->assertFalse($sut->itemHasTopicSubject($build, $analysis));
+
+        $analysis->Status = DynamicAnalysis::FAILED;
+        $this->assertTrue($sut->itemHasTopicSubject($build, $analysis));
+
+        $analysis->Status = DynamicAnalysis::NOTRUN;
+        $this->assertTrue($sut->itemHasTopicSubject($build, $analysis));
+    }
+
+    public function testGetTopicCollection()
+    {
+        $sut = new DynamicAnalysisTopic();
+        $collection = $sut->getTopicCollection();
+        $this->assertInstanceOf(DynamicAnalysisCollection::class, $collection);
+    }
+
+    public function testSetTopicData()
+    {
+        $sut = new DynamicAnalysisTopic();
+        $build = new Build();
+
+        $a1 = new DynamicAnalysis();
+        $a2 = new DynamicAnalysis();
+        $a3 = new DynamicAnalysis();
+
+        $a1->Status = DynamicAnalysis::NOTRUN;
+        $a2->Status = DynamicAnalysis::PASSED;
+        $a3->Status = DynamicAnalysis::FAILED;
+
+        $a1->Name = 'A';
+        $a2->Name = 'B';
+        $a3->Name = 'C';
+
+        $build
+            ->AddDynamicAnalysis($a1)
+            ->AddDynamicAnalysis($a2)
+            ->AddDynamicAnalysis($a3);
+
+        $sut->setTopicData($build);
+        $collection = $sut->getTopicCollection();
+
+        $this->assertEquals(2, $sut->getTopicCount());
+        $this->assertSame($a1, $collection->get('A'));
+        $this->assertSame($a3, $collection->get('C'));
+        $this->assertNull($collection->get('B'));
+    }
+}
