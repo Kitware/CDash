5e0d81bd6e977f20da9df3a437d3e50a04d1a301
Zack Galbreath <zack.galbreath@kitware.com>
2018-06-18 15:20:56 -0400

    Simplify and fix queryTests filter test
    This test wasn't actually verifying the behavior it intended to.
    We simplify the test to specify the filters via the query string
    rather than manipulating the UI.

--- tests/js/e2e_tests/queryTests.js
+++ tests/js/e2e_tests/queryTests.js
@@ -1,40 +1,16 @@
 describe("queryTests", function() {
-  it("show filters", function() {
-    browser.get('queryTests.php?project=InsightExample');
-    var link = element(by.id('label_showfilters'));
-    link.click();
-  });
-
   function filter_test(field, compare, value, num_builds) {
-    // Click clear and wait for the page to reload.
-    element(by.name('clear')).click();
-    return browser.driver.wait(function() {
-      return browser.driver.getCurrentUrl().then(function(url) {
-        return /filtercount=0/.test(url);
-      });
-    }, 10000);
-
-    // Apply our filter parameters.
-    element(by.id('id_field1')).$('[value="' + field + '"]').click();
-    element(by.id('id_compare1')).$('[value="' + compare + '"]').click();
-    element(by.id('id_value1')).sendKeys(value);
-    element(by.name('apply')).click();
-
-    // Wait for the page to load.
-    var regexp = new RegExp(field);
-    return browser.driver.wait(function() {
-      return browser.driver.getCurrentUrl().then(function(url) {
-        return regexp.test(url);
-      });
-    }, 10000);
+    // Load the filtered page.
+    browser.get('queryTests.php?project=InsightExample&filtercount=1&showfilters=1&field1=' + field + '&compare1=' + compare + '&value1=' + value);
 
     // Make sure the expected number of rows are displayed.
-    expect(element.all(by.repeater('build in cdash.builds')).count()).toBe(num_builds);
+    expect(element.all(by.repeater('build in pagination.filteredBuilds')).count()).toBe(num_builds);
   }
 
   it("filter on build name", function() {
     filter_test("buildname", "63", "simple", 3);
   });
+
 
   it("filter on build time", function() {
     filter_test("buildstarttime", "83", "yesterday", 5);
@@ -49,6 +25,7 @@ describe("queryTests", function() {
   });
 
   it("filter on time", function() {
-    filter_test("site", "41", "0", 5);
+    filter_test("time", "41", "0", 5);
   });
+
 });
