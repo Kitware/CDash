a4ca634e907e7647e79ea2e8d54601f62ec17f81
Bryon Bean <bryon.bean@kitware.com>
2018-09-07 10:05:17 -0400

    fix: fixes variable initialization

--- include/Messaging/Notification/Email/Decorator/PreambleDecorator.php
+++ include/Messaging/Notification/Email/Decorator/PreambleDecorator.php
@@ -6,14 +6,17 @@ use CDash\Messaging\Topic\Topic;
 
 class PreambleDecorator extends Decorator
 {
-    private $template = 'A submission to CDash for the project {{ project_name }} has '
-                . '{{ topic_list }}. You have been identified as one of the authors '
-                . 'who have checked in changes that are part of this submission '
-                . 'or you are listed in the default contact list.' . "\n\n"
-                . 'Details on the submission can be found at {{ project_url }}' . "\n";
+    private $template = [
+        'A submission to CDash for the project {{ project_name }} has ',
+        '{{ topic_list }}. You have been identified as one of the authors ',
+        'who have checked in changes that are part of this submission ',
+        "or you are listed in the default contact list.\n\n",
+        "Details on the submission can be found at {{ project_url }}\n",
+    ];
 
     public function createPreamble(SubscriptionInterface $subscription)
     {
+        $template = implode('', $this->template);
         $summary = $subscription->getBuildSummary();
         $descriptions = array_column($summary['topics'], 'description');
         $data = [
@@ -21,7 +24,7 @@ class PreambleDecorator extends Decorator
             'topic_list' => implode(' and ', array_map('strtolower', $descriptions)),
             'project_url' => $summary['project_url'],
         ];
-        $this->text = $this->decorateWith($this->template, $data);
+        $this->text = $this->decorateWith($template, $data);
     }
 
     /**
