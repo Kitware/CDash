5771a79dd40e4d418bbf3957c9fa398b190db462
Bryon Bean <bryon.bean@kitware.com>
2018-09-14 11:20:21 -0400

    fix: fixes requirements for redundant emails

--- include/Messaging/Topic/EmailSentTopic.php
+++ include/Messaging/Topic/EmailSentTopic.php
@@ -18,7 +18,6 @@ namespace CDash\Messaging\Topic;
 
 use CDash\Model\ActionableTypes;
 use CDash\Model\Build;
-use CDash\Model\BuildEmail;
 
 class EmailSentTopic extends Topic
 {
@@ -31,14 +30,47 @@ class EmailSentTopic extends Topic
         $parentTopic = is_null($this->topic) ? true : $this->topic->subscribesToBuild($build);
         $subscribe = false;
         if ($parentTopic) {
-            $type = $this->getTopicName();
-            $category = ActionableTypes::$categories[$type];
-            $collection = $build->GetBuildEmailCollection($category);
-            $email = $this->subscriber->getAddress();
-            $buildEmails = $collection->get($email);
-            $subscribe = is_null($buildEmails);
+            $subscribe =
+              ! $this->hasPreviousBuild($build)     ||
+                $this->hasPreviousBuildDiff($build) &&
+              ! $this->isEmailForBuildSent($build);
         }
         return $subscribe;
+    }
+
+    public function hasPreviousBuild(Build $build)
+    {
+        $previous_build_id = $build->GetPreviousBuildId();
+        return $previous_build_id > 0;
+    }
+
+    /**
+     * @param Build $build
+     * @return bool
+     */
+    public function hasPreviousBuildDiff(Build $build)
+    {
+        // TODO: do we want to only check the diff that is the subject of $this->topic?
+        $diff = $build->GetErrorDifferences();
+        return $diff['buildwarningspositive'] > 0
+            || $diff['builderrorspositive'] > 0
+            || $diff['configurewarnings'] > 0
+            || $diff['configureerrors'] > 0
+            || $diff['testfailedpositive'] > 0
+            || $diff['testnotrunpositive'] > 0;
+    }
+
+    /**
+     * @param Build $build
+     * @return bool
+     */
+    public function isEmailForBuildSent(Build $build)
+    {
+        $type = $this->getTopicName();
+        $category = ActionableTypes::$categories[$type];
+        $collection = $build->GetBuildEmailCollection($category);
+        $email = $this->subscriber->getAddress();
+        return $collection->has($email);
     }
 
     /**
