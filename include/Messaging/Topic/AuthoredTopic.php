d5b57ef1255e3cc036af41500a94d4c585e1c9cf
Bryon Bean <bryon.bean@kitware.com>
2018-09-18 14:22:22 -0400

    test: for AuthoredTopic

--- include/Messaging/Topic/AuthoredTopic.php
+++ include/Messaging/Topic/AuthoredTopic.php
@@ -11,8 +11,9 @@ class AuthoredTopic extends Topic
      */
     public function subscribesToBuild(Build $build)
     {
-        $parentTopic = is_null($this->topic) ? true : $this->topic->subscribesToBuild($build);
-        return $parentTopic && $build->HasAuthor($this->subscriber->getAddress());
+        $subscribe = $this->topic->subscribesToBuild($build)
+            && $build->HasAuthor($this->subscriber->getAddress());
+        return $subscribe;
     }
 
     /**
@@ -22,11 +23,6 @@ class AuthoredTopic extends Topic
      */
     public function itemHasTopicSubject(Build $build, $item)
     {
-        // b808c5746ff9f69e3bfc78f01f5880bf13456ebe
-        // TODO: q: do we need to do this again here?
-        // a: Only if subscribesToBuild has not yet been called, but if it has been called
-        //    how do we determine that the build passed in here is the same build that was
-        //    verified in subscribesToBuild?
-        return $build->HasAuthor($this->subscriber->getAddress());
+        return $this->topic->itemHasTopicSubject($build, $item);
     }
 }
