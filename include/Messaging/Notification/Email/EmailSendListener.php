eb9a7ea7b017d4b94b8473ed38600d7421d8c651
Bryon Bean <bryon.bean@kitware.com>
2018-09-25 13:15:05 -0400

    fix: prevents sendmail startup from ocurring during testing

--- include/Messaging/Notification/Email/EmailSendListener.php
+++ include/Messaging/Notification/Email/EmailSendListener.php
@@ -17,8 +17,11 @@
 namespace CDash\Messaging\Notification\Email;
 
 use Swift_Events_SendEvent;
+use Swift_Events_TransportChangeEvent;
+use Swift_Events_TransportChangeListener;
+
 // TODO: refactor as specifically a EmailSendTestListener
-class EmailSendListener implements \Swift_Events_SendListener
+class EmailSendListener implements \Swift_Events_SendListener, \Swift_Events_TransportChangeListener
 {
     protected $mail;
     protected $cancelBubble;
@@ -48,5 +51,45 @@ class EmailSendListener implements \Swift_Events_SendListener
     public function sendPerformed(Swift_Events_SendEvent $evt)
     {
         // TODO: Implement sendPerformed() method.
+    }
+
+    /**
+     * Invoked just before a Transport is started.
+     *
+     * @param Swift_Events_TransportChangeEvent $evt
+     */
+    public function beforeTransportStarted(Swift_Events_TransportChangeEvent $evt)
+    {
+        $evt->cancelBubble($this->cancelBubble);
+    }
+
+    /**
+     * Invoked immediately after the Transport is started.
+     *
+     * @param Swift_Events_TransportChangeEvent $evt
+     */
+    public function transportStarted(Swift_Events_TransportChangeEvent $evt)
+    {
+        // TODO: Implement transportStarted() method.
+    }
+
+    /**
+     * Invoked just before a Transport is stopped.
+     *
+     * @param Swift_Events_TransportChangeEvent $evt
+     */
+    public function beforeTransportStopped(Swift_Events_TransportChangeEvent $evt)
+    {
+        $evt->cancelBubble($this->cancelBubble);
+    }
+
+    /**
+     * Invoked immediately after the Transport is stopped.
+     *
+     * @param Swift_Events_TransportChangeEvent $evt
+     */
+    public function transportStopped(Swift_Events_TransportChangeEvent $evt)
+    {
+        // TODO: Implement transportStopped() method.
     }
 }
