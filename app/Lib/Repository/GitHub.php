e7d1fcca7dac9ce398387ffd31e9dd5564ce163c
Bryon Bean <bryon.bean@kitware.com>
2018-10-24 13:24:51 -0400

    feat: adds GitHub service with setStatus feature

---
+++ app/Lib/Repository/GitHub.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * =========================================================================
+ *   Program:   CDash - Cross-Platform Dashboard System
+ *   Module:    $Id$
+ *   Language:  PHP
+ *   Date:      $Date$
+ *   Version:   $Revision$
+ *   Copyright (c) Kitware, Inc. All rights reserved.
+ *   See LICENSE or http://www.cdash.org/licensing/ for details.
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE. See the above copyright notices for more information.
+ * =========================================================================
+ */
+
+namespace CDash\Lib\Repository;
+
+use CDash\Model\Build;
+use GuzzleHttp\ClientInterface;
+
+/**
+ * Class GitHub
+ * @package CDash\Lib\Repository
+ */
+class GitHub implements RepositoryInterface
+{
+    const BASE_URI = 'https://api.github.com';
+
+    /** @var string $token */
+    private $token;
+
+    /** @var string $owner */
+    private $owner;
+
+    /** @var string $repo */
+    private $repo;
+
+    /** @var string $hash */
+    private $hash;
+
+    /**
+     * GitHub constructor.
+     * @param $token
+     * @param $owner
+     * @param $repo
+     * @param $hash
+     */
+    public function __construct($token, $owner, $repo)
+    {
+        $this->token = $token;
+        $this->owner = $owner;
+        $this->repo = $repo;
+    }
+
+    /**
+     * Sets the status of a commit. Possible status' are pending, success, error and
+     * failure. The options argument MUST contain the following properties:
+     *   - commit_hash
+     *   - state
+     *
+     * The options argument MAY contain the following additional properties:
+     *   - context (GitHub default is 'default')
+     *   - target_url
+     *   - description
+     *
+     * @see https://developer.github.com/v3/repos/statuses/ for property descriptions
+     *
+     * @param ClientInterface $client
+     * @param array $options
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function setStatus(ClientInterface $client, array $options)
+    {
+        $path = "/repos/{$this->owner}/{$this->repo}/statuses/{$options['commit_hash']}";
+        $uri  = self::BASE_URI . $path;
+
+        $body = array_filter($options, function ($key) {
+            return in_array($key, ['state', 'context', 'description', 'target_url']);
+        }, ARRAY_FILTER_USE_KEY);
+
+        $options = [
+            'headers' => [
+                'Authorization' => "token {$this->token}"
+            ],
+            'body' => json_encode($body),
+        ];
+
+        return $client->requestAsync('POST', $uri, $options);
+    }
+}
---
+++ tests/case/CDash/Lib/Repository/GitHubTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * =========================================================================
+ *   Program:   CDash - Cross-Platform Dashboard System
+ *   Module:    $Id$
+ *   Language:  PHP
+ *   Date:      $Date$
+ *   Version:   $Revision$
+ *   Copyright (c) Kitware, Inc. All rights reserved.
+ *   See LICENSE or http://www.cdash.org/licensing/ for details.
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE. See the above copyright notices for more information.
+ * =========================================================================
+ */
+
+use CDash\Config;
+use CDash\Lib\Repository\GitHub;
+use GuzzleHttp\ClientInterface;
+use Ramsey\Uuid\Uuid;
+
+class GitHubTest extends PHPUnit_Framework_TestCase
+{
+    public function testSetStatus()
+    {
+        $base_url = Config::getInstance()->getBaseUrl();
+
+        $token = Uuid::uuid4()->toString();
+        $owner = 'Foo';
+        $repo = 'Bar';
+        $hash = str_replace('-', '', Uuid::uuid4()->toString());
+        $sut = new GitHub($token, $owner, $repo);
+
+        $options = [
+            'commit_hash' => $hash,
+            'context' => 'CDash by Kitware',
+            'description' => 'Build suchnsuch from site sonso',
+            'state' => 'pending',
+            'target_url' => "{$base_url}/buildSummary.php?buildid=1010",
+        ];
+
+        $json = [
+            'context' => $options['context'],
+            'description' => $options['description'],
+            'state' => $options['state'],
+            'target_url' => $options['target_url']
+        ];
+
+        /** @var ClientInterface|PHPUnit_Framework_MockObject_MockObject $client */
+        $client = $this->getMockBuilder(ClientInterface::class)
+            ->getMockForAbstractClass();
+
+        $client_options = [
+            'headers' => ['Authorization' => "token {$token}"],
+            'body' => json_encode($json),
+        ];
+
+        $uri = GitHub::BASE_URI . "/repos/{$owner}/{$repo}/statuses/{$hash}";
+
+        $client->expects($this->once())
+            ->method('requestAsync')
+            ->with('POST', $uri, $client_options);
+
+        $sut->setStatus($client, $options);
+    }
+}
