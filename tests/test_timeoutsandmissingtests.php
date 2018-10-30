1f10cf8f5e7020df31fc18a59960e91ea9db656f
Bryon Bean <bryonbean@users.noreply.github.com>
2018-10-30 10:21:20 -0400

    Refactor Email Tests Performing Char x Char Testing
    Somewhat reduces the brittleness of email tests using KWWebTestCase::compareLog.
    Removes brittle JS test queryTests::filter on time
    Preps for Laravel integration

--- tests/test_timeoutsandmissingtests.php
+++ tests/test_timeoutsandmissingtests.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bryonbean
- * Date: 2/6/17
- * Time: 12:32 PM
- */
+use CDash\Config;
 
 require_once dirname(__FILE__) . '/cdash_test_case.php';
 require_once 'include/common.php';
@@ -43,11 +38,34 @@ class TimeoutsAndMissingTestsTestCase extends KWWebTestCase
         if (!$this->submission('EmailProjectExample', $file)) {
             return;
         }
-
-        if (!$this->compareLog($this->logfilename, "{$rep}/cdash_5.log")) {
-            return;
+        $url = Config::getInstance()->getBaseUrl();
+        $expected = [
+            'simpletest@localhost',
+            'FAILED (t=3, m=3): EmailProjectExample - Win32-MSVC2009 - Nightly',
+            'A submission to CDash for the project EmailProjectExample has failing tests and missing tests.',
+            'You have been identified as one of the authors who have checked in changes that are part of this',
+            "Details on the submission can be found at {$url}/buildSummary.php?buildid=",
+            'Project: EmailProjectExample',
+            'Site: Dash20.kitware',
+            'Build Name: Win32-MSVC2009',
+            'Build Time: 2009-02-26T05:04:00 EST',
+            'Type: Nightly',
+            'Tests not passing: 3',
+            'Missing tests: 3',
+            '*Tests failing*',
+            "curl | Completed | ({$url}/testDetails.php?test=",
+            "StringActionsTest | Completed (OTHER_FAULT) | ({$url}/testDetails.php?test=",
+            "MathActionsTest | Completed (OTHER_FAULT) | ({$url}/testDetails.php?test=",
+            '*Missing tests*',
+            "DashboardSendTest ({$url}/viewTest.php?buildid=",
+            "Parser1Test1 ({$url}/viewTest.php?buildid=",
+            "SystemInfoTest({$url}/viewTest.php?buildid=",
+            '-CDash on',
+            'function',
+        ];
+        if ($this->assertLogContains($expected, 31)) {
+            $this->pass('Passed');
         }
-        $this->pass('Passed');
     }
 
     public function testMissingTestsSummarizedInViewTestAPI()
@@ -86,9 +104,27 @@ class TimeoutsAndMissingTestsTestCase extends KWWebTestCase
         if (!$this->submission('EmailProjectExample', $file)) {
             return;
         }
-        if (!$this->compareLog($this->logfilename, "$rep/cdash_4.log")) {
-            return;
+        $url = Config::getInstance()->getBaseUrl();
+        $expected = [
+            'simpletest@localhost',
+            'FAILED (t=2): EmailProjectExample - OSX-SIERRA-10.12.1 - Nightly',
+            'A submission to CDash for the project EmailProjectExample has failing tests',
+            'You have been identified as one of the authors who have checked in changes that are part of this',
+            "Details on the submission can be found at {$url}/buildSummary.php?buildid=",
+            'Project: EmailProjectExample',
+            'Site: Dash20.kitware',
+            'Build Name: OSX-SIERRA-10.12.1',
+            'Build Time: 2009-02-23T05:04:13 EST',
+            'Type: Nightly',
+            'Tests not passing: 2',
+            '*Tests failing*',
+            "SleepTimer1 | Completed (Timeout) | ({$url}/testDetails.php?test=",
+            "SleepTimer2 | Completed (Timeout) | ({$url}/testDetails.php?test=",
+            '-CDash on',
+            'function',
+        ];
+        if ($this->assertLogContains($expected, 23)) {
+            $this->pass('Passed');
         }
-        $this->pass('Passed');
     }
 }
