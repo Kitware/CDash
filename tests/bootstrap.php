3e31d796b841f9015ddb083599b89c48c82b32e8
Bryon Bean <bryon.bean@kitware.com>
2018-11-05 10:10:12 -0500

    chore: remove unecessary require from bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,3 @@ set_include_path(get_include_path() . PATH_SEPARATOR . $cdash_root);
 // Config now configures autoloader
 require_once 'config/config.php';
 
-// Require files that do not adhere to any naming convention below...
-require_once 'xml_handlers/actionable_build_interface.php';
