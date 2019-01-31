dfb0ac1546c05959845b2568424fd8586e355453
Bryon Bean <bryon.bean@kitware.com>
2019-01-31 13:09:00 -0500

    refactor: favor Laravel integration test vs fix for Simpletest
    see: https://github.com/bryonbean/cdash/pull/4/commits/0ebcece23d380112734592ec71464dce464abe6d

--- public/displayImage.php
+++ public/displayImage.php
@@ -14,39 +14,5 @@
   PURPOSE. See the above copyright notices for more information.
 =========================================================================*/
 
-include dirname(__DIR__) . '/config/config.php';
-require_once 'include/pdo.php';
-
-use CDash\Model\Image;
-
-if (array_key_exists('imgid', $_GET)) {
-    $imgid = $_GET['imgid'];
-}
-// Checks
-if (empty($imgid) || !is_numeric($imgid)) {
-    echo 'Not a valid imgid!';
-    return;
-}
-
-$image = new Image();
-$image->Id = $imgid;
-$image->Load();
-
-switch ($image->Extension) {
-    case 'image/jpg':
-        header('Content-type: image/jpeg');
-        break;
-    case 'image/jpeg':
-        header('Content-type: image/jpeg');
-        break;
-    case 'image/gif':
-        header('Content-type: image/gif');
-        break;
-    case 'image/png':
-        header('Content-type: image/png');
-        break;
-    default:
-        echo "Unknown image type: $image->Extension";
-        return;
-}
-echo $image->Data;
+$imgid = request('imgid');
+return redirect("/image/{$imgid}", 301);
