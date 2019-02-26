eeb1a99028b6dfb0c99178c75f16b6db3a7083fb
Bryon Bean <bryon.bean@kitware.com>
2019-02-26 06:45:14 -0500

    fix: fixes test_buildproperties.php

--- public/api/v1/buildProperties.php
+++ public/api/v1/buildProperties.php
@@ -22,7 +22,123 @@ require_once 'include/version.php';
 
 use CDash\Database;
 use CDash\Model\Project;
-use Session;
+
+if (!function_exists('get_defects_for_builds')) {
+    function get_defects_for_builds()
+    {
+        if (!array_key_exists('buildid', $_GET)) {
+            json_error_response('Missing parameter: buildid');
+        }
+        if (!array_key_exists('defect', $_GET)) {
+            json_error_response('Missing parameter: defect');
+        }
+
+        if (!is_array($_GET['buildid']) || count($_GET['buildid']) < 1) {
+            json_error_response("No builds specified");
+        }
+        if (!is_array($_GET['defect']) || count($_GET['defect']) < 1) {
+            json_error_response("No defects specified");
+        }
+
+        $pdo = Database::getInstance()->getPdo();
+        $placeholder_str = str_repeat('?,', count($_GET['buildid']) - 1). '?';
+
+        if (!function_exists('gather_defects')) {
+            function gather_defects($stmt, $prettyname, &$defects_response)
+            {
+                $results_found = false;
+                while ($row = $stmt->fetch()) {
+                    $results_found = true;
+                    $descr = $row['descr'];
+                    $idx = array_search($descr, array_column($defects_response, 'descr'));
+                    if ($idx === false) {
+                        $defects_response[] = [
+                            'descr' => $descr,
+                            'type' => $prettyname,
+                            'builds' => []
+                        ];
+                        $idx = count($defects_response) - 1;
+                    }
+                    $defects_response[$idx]['builds'][] = $row['buildid'];
+                }
+                return $results_found;
+            }
+        }
+
+        $defects_response = [];
+        foreach ($_GET['defect'] as $defect) {
+            $valid_defect = false;
+            $sql = '';
+            $sql2 = null;
+
+            $error_defect = false;
+            if ($defect == 'builderrors') {
+                $error_defect = true;
+                $prettyname = 'Error';
+                $type = 0;
+            } elseif ($defect == 'buildwarnings') {
+                $error_defect = true;
+                $prettyname = 'Warning';
+                $type = 1;
+            }
+
+            if ($error_defect) {
+                $valid_defect = true;
+                // Query builderror table.
+                $sql =
+                    "SELECT buildid, text AS descr
+                FROM builderror
+                WHERE type = $type AND
+                buildid IN ($placeholder_str)";
+                // Query buildfailure table.
+                $sql2 =
+                    "SELECT bf.buildid, bfd.stderror AS descr
+                FROM buildfailure bf
+                JOIN buildfailuredetails bfd ON bf.detailsid = bfd.id
+                WHERE bfd.type = $type AND
+                bf.buildid IN ($placeholder_str)";
+            } elseif ($defect == 'testfailed') {
+                $valid_defect = true;
+                $prettyname = 'Test Failure';
+                $sql =
+                    "SELECT t.name AS descr, b.id AS buildid
+                FROM test t
+                JOIN build2test b2t ON b2t.testid = t.id
+                JOIN build b ON b.id = b2t.buildid
+                WHERE b2t.status = 'failed' AND
+                b.id IN ($placeholder_str)";
+            }
+
+            if (!$valid_defect) {
+                continue;
+            }
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($_GET['buildid']);
+
+            if (!gather_defects($stmt, $prettyname, $defects_response) &&
+                !is_null($sql2)) {
+                $stmt = $pdo->prepare($sql2);
+                $stmt->execute($_GET['buildid']);
+                gather_defects($stmt, $prettyname, $defects_response);
+            }
+        }
+
+        $response = [];
+        $response['defects'] = $defects_response;
+        echo json_encode(cast_data_for_JSON($response));
+    }
+}
+
+// From now on, we are only concerned with the types of defects that were
+// selected by the user (or by default).
+// Filter out those that were not selected.
+if (!function_exists('defect_type_selected')) {
+    function defect_type_selected($defect_type)
+    {
+        return $defect_type['selected'];
+    }
+}
 
 $start = microtime_float();
 $response = [];
@@ -127,13 +243,6 @@ if (isset($_GET['defects'])) {
 }
 $response['defecttypes'] = $defect_types;
 
-// From now on, we are only concerned with the types of defects that were
-// selected by the user (or by default).
-// Filter out those that were not selected.
-function defect_type_selected($defect_type)
-{
-    return $defect_type['selected'];
-}
 $defect_types = array_filter($defect_types, 'defect_type_selected');
 
 // Construct an SQL SELECT clause for the requested types of defects.
@@ -194,7 +303,7 @@ $response['properties'] = $all_properties;
 
 // Timeline chart needs to know what defects we care about
 // and what page we're coming from.
-Session::put('defecttypes', $defect_types);
+\Session::put('defecttypes', $defect_types);
 
 //$_SESSION['defecttypes'] = $defect_types;
 $response['filterdata']['pageId'] = 'buildProperties.php';
@@ -202,107 +311,3 @@ $response['filterdata']['pageId'] = 'buildProperties.php';
 $end = microtime_float();
 $response['generationtime'] = round($end - $start, 3);
 echo json_encode(cast_data_for_JSON($response));
-
-
-function get_defects_for_builds()
-{
-    if (!array_key_exists('buildid', $_GET)) {
-        json_error_response('Missing parameter: buildid');
-    }
-    if (!array_key_exists('defect', $_GET)) {
-        json_error_response('Missing parameter: defect');
-    }
-
-    if (!is_array($_GET['buildid']) || count($_GET['buildid']) < 1) {
-        json_error_response("No builds specified");
-    }
-    if (!is_array($_GET['defect']) || count($_GET['defect']) < 1) {
-        json_error_response("No defects specified");
-    }
-
-    $pdo = Database::getInstance()->getPdo();
-    $placeholder_str = str_repeat('?,', count($_GET['buildid']) - 1). '?';
-
-    function gather_defects($stmt, $prettyname, &$defects_response)
-    {
-        $results_found = false;
-        while ($row = $stmt->fetch()) {
-            $results_found = true;
-            $descr = $row['descr'];
-            $idx = array_search($descr, array_column($defects_response, 'descr'));
-            if ($idx === false) {
-                $defects_response[] = [
-                    'descr' => $descr,
-                    'type' => $prettyname,
-                    'builds' => []
-                        ];
-                $idx = count($defects_response) - 1;
-            }
-            $defects_response[$idx]['builds'][] = $row['buildid'];
-        }
-        return $results_found;
-    }
-
-    $defects_response = [];
-    foreach ($_GET['defect'] as $defect) {
-        $valid_defect = false;
-        $sql = '';
-        $sql2 = null;
-
-        $error_defect = false;
-        if ($defect == 'builderrors') {
-            $error_defect = true;
-            $prettyname = 'Error';
-            $type = 0;
-        } elseif ($defect == 'buildwarnings') {
-            $error_defect = true;
-            $prettyname = 'Warning';
-            $type = 1;
-        }
-
-        if ($error_defect) {
-            $valid_defect = true;
-            // Query builderror table.
-            $sql =
-                "SELECT buildid, text AS descr
-                FROM builderror
-                WHERE type = $type AND
-                buildid IN ($placeholder_str)";
-            // Query buildfailure table.
-            $sql2 =
-                "SELECT bf.buildid, bfd.stderror AS descr
-                FROM buildfailure bf
-                JOIN buildfailuredetails bfd ON bf.detailsid = bfd.id
-                WHERE bfd.type = $type AND
-                bf.buildid IN ($placeholder_str)";
-        } elseif ($defect == 'testfailed') {
-            $valid_defect = true;
-            $prettyname = 'Test Failure';
-            $sql =
-                "SELECT t.name AS descr, b.id AS buildid
-                FROM test t
-                JOIN build2test b2t ON b2t.testid = t.id
-                JOIN build b ON b.id = b2t.buildid
-                WHERE b2t.status = 'failed' AND
-                b.id IN ($placeholder_str)";
-        }
-
-        if (!$valid_defect) {
-            continue;
-        }
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute($_GET['buildid']);
-
-        if (!gather_defects($stmt, $prettyname, $defects_response) &&
-                !is_null($sql2)) {
-            $stmt = $pdo->prepare($sql2);
-            $stmt->execute($_GET['buildid']);
-            gather_defects($stmt, $prettyname, $defects_response);
-        }
-    }
-
-    $response = [];
-    $response['defects'] = $defects_response;
-    echo json_encode(cast_data_for_JSON($response));
-}
