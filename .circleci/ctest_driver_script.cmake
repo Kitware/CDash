6f0909cc1e3b5ea774caa40fee51f2c93c94e2c1
Zack Galbreath <zack.galbreath@kitware.com>
2019-03-22 12:58:55 -0400

    Send status context from CircleCI to CDash

--- .circleci/ctest_driver_script.cmake
+++ .circleci/ctest_driver_script.cmake
@@ -6,9 +6,6 @@ set(CTEST_BINARY_DIRECTORY "/home/kitware/cdash/_build")
 set(CTEST_UPDATE_COMMAND git)
 set(CTEST_UPDATE_VERSION_ONLY 1)
 
-ctest_start(Continuous)
-ctest_update()
-
 set(cfg_options
   "-DCDASH_TESTING_RENAME_LOGS=true"
   "-DCDASH_DIR_NAME=cdash"
@@ -18,14 +15,23 @@ if (postgres)
   list(APPEND cfg_options
     "-DCDASH_DB_TYPE=pgsql"
     "-DCDASH_DB_LOGIN=postgres")
+  file(WRITE ${CTEST_BINARY_DIRECTORY}/props.json
+          [=[ { "status context": "Postgres" } ]=])
 else()
   list(APPEND cfg_options "-DCDASH_DB_LOGIN=root")
+  file(WRITE ${CTEST_BINARY_DIRECTORY}/props.json
+          [=[ { "status context": "MySQL" } ]=])
 endif()
-ctest_configure(OPTIONS "${cfg_options}")
 
+ctest_start(Continuous)
+ctest_update()
+# Send Update and BuildProperties ASAP so we can set pending status on GitHub.
+ctest_submit(PARTS Update)
+ctest_submit(CDASH_UPLOAD "${CTEST_BINARY_DIRECTORY}/props.json" CDASH_UPLOAD_TYPE BuildPropertiesJSON)
+
+ctest_configure(OPTIONS "${cfg_options}")
 ctest_test(RETURN_VALUE test_status CAPTURE_CMAKE_ERROR cmake_errors)
 if (NOT "${test_status}" EQUAL 0)
   message(SEND_ERROR "some tests did not pass cleanly")
 endif()
-
-ctest_submit()
+ctest_submit(PARTS Configure Test)
