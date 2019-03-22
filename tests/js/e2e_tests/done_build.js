98291605e11edc3c1ec47a81cf58c2918f694a58
Zack Galbreath <zack.galbreath@kitware.com>
2019-03-22 12:04:27 -0400

    Fix "done_build" test for newer versions of CTest
    CTest v3.14 now marks builds as done automatically. We update the done_build
    test so that it no longer assumes a build is done or not when it starts.
    Instead it tests that it is able to toggle the doneness of a build on and off.

--- tests/js/e2e_tests/done_build.js
+++ tests/js/e2e_tests/done_build.js
@@ -1,10 +1,30 @@
 var LoginPage = require('../pages/login.page.js');
 describe("done_build", function() {
 
+  function validate_test(index_url, link, old_text, new_text) {
+    link.click();
+    browser.waitForAngular();
+
+    // Refresh the page to make sure this build's "doneness" was changed.
+    browser.get(index_url);
+    element(by.repeater('build in buildgroup.pagination.filteredBuilds').row(0)).element(by.name('adminoptions')).click();
+    expect(element(by.partialLinkText(new_text)).isPresent()).toBe(true);
+
+    // Toggle it back to its original state.
+    link = element(by.partialLinkText(new_text));
+    link.click();
+    browser.waitForAngular();
+
+    // Refresh & verify.
+    browser.get(index_url);
+    element(by.repeater('build in buildgroup.pagination.filteredBuilds').row(0)).element(by.name('adminoptions')).click();
+    expect(element(by.partialLinkText(old_text)).isPresent()).toBe(true);
+  }
+
   function toggle_done(index_url) {
     browser.get(index_url);
 
-    // Locate the folder icon
+    // Locate the folder icon.
     var folderIcon = element(by.repeater('build in buildgroup.pagination.filteredBuilds').row(0)).element(by.name('adminoptions'));
 
     // Make sure that we located the right img.
@@ -13,25 +33,17 @@ describe("done_build", function() {
     // Click the icon to expand the menu.
     folderIcon.click();
 
-    // Find the 'mark as done' link and click it.
+    // Find the 'mark as [not] done' link and click it.
     var link = element(by.partialLinkText('mark as done'));
-    link.click();
-    browser.waitForAngular();
-
-    // Refresh the page to make sure this build is now done.
-    browser.get(index_url);
-    element(by.repeater('build in buildgroup.pagination.filteredBuilds').row(0)).element(by.name('adminoptions')).click();
-    expect(element(by.partialLinkText('mark as not done')).isPresent()).toBe(true);
-
-    // Make it not done again.
-    link = element(by.partialLinkText('mark as not done'));
-    link.click();
-    browser.waitForAngular();
-
-    // Refresh & verify.
-    browser.get(index_url);
-    element(by.repeater('build in buildgroup.pagination.filteredBuilds').row(0)).element(by.name('adminoptions')).click();
-    expect(element(by.partialLinkText('mark as done')).isPresent()).toBe(true);
+    link.isPresent().then(function(result) {
+      if (result) {
+        validate_test(index_url, link, 'mark as done', 'mark as not done');
+      } else {
+        // Newer versions of CTest mark builds as done automatically.
+        link = element(by.partialLinkText('mark as not done'));
+        validate_test(index_url, link, 'mark as not done', 'mark as done');
+      }
+    });
   }
 
   it("toggle done for normal build", function() {
