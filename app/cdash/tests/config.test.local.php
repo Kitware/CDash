34c47abd4081197c58c2ac47bf39aa90fe31a5da
Bryon Bean <bryon.bean@kitware.com>
2019-06-23 11:17:50 -0400

    chore: moves CDash into app directory

---
+++ app/cdash/tests/config.test.local.php
@@ -0,0 +1,14 @@
+<?php
+$configure = array(
+  'urlwebsite'       => 'http://cdash.dev',
+  'webserver'        => 'http://cdash.dev/',
+  'webpath'          => '/',
+  'outputdirectory'  => '/Users/bryonbean/Projects/CDash/_build/tmp',
+  'type'             => 'Nightly',
+  'site'             => 'bryons-mbp.kitware.com',
+  'buildname'        => 'inner',
+  'cdash'            => 'http://www.cdash.org/CDash',
+  'svnroot'          => '/Users/bryonbean/Projects/CDash',
+  'cdashpro'         => 'OFF',
+  );
+?>
