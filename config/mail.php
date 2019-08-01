03161b71002f2a2da47ce159c848ed2abba19314
Zack Galbreath <zack.galbreath@kitware.com>
2019-08-01 16:19:40 -0400

    Use "email from" setting if not overridden by .env

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', config('cdash.deprecated.EMAIL_FROM')),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
