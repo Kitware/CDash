6c60c1ff3401181b15eca498a3d6dfcdebe09984
Bryon Bean <bryon.bean@kitware.com>
2019-09-19 13:07:35 -0400

    feat: configuration of LDAP authentication

--- config/ldap.php
+++ config/ldap.php
@@ -24,7 +24,7 @@ return [
     |
     */
 
-    'logging' => env('LDAP_LOGGING', false),
+    'logging' => env('LDAP_LOGGING', true),
 
     /*
     |--------------------------------------------------------------------------
