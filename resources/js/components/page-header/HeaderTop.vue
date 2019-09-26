897c2bbdfa44afc3934e81a0bbc40e0a3c2ed288
Bryon Bean <bryon.bean@kitware.com>
2019-09-26 13:42:56 -0400

    feat: creates #headertop vue component

---
+++ resources/js/components/page-header/HeaderTop.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="headertop">
+        <div id="topmenu">
+            <a :class="{ 'hidden': loggedIn}"  href="/login">Login</a>
+            <a :class="{ 'hidden': loggedIn}"  href="register.php">Register</a>
+            <a :class="{ 'hidden': !loggedIn}" href="user.php">My CDash</a>
+            <a :class="{ 'hidden': !loggedIn}" href="/logout">Log out</a>
+            <a :class="{ 'hidden': isHome}"   :href="home">All Dashboards</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderTop",
+        props: ['user', 'location'],
+        data () {
+            return {
+                home: 'viewProjects.php'
+            }
+        },
+
+        computed: {
+            loggedIn () {
+                return this.user && this.user.id;
+            },
+
+            isHome () {
+                return this.location === this.home;
+            },
+        },
+    }
+</script>
+
+<style scoped>
+ .hidden {
+     display: none;
+ }
+</style>
---
+++ tests/Spec/header-top.spec.js
@@ -0,0 +1,75 @@
+import { mount } from 'vue-test-utils';
+import HeaderTop from "../../resources/js/components/page-header/HeaderTop.vue";
+import expect from 'expect';
+
+describe('HeaderTop', () => {
+  let component;
+
+  beforeEach(() => {
+    component = mount(HeaderTop);
+  });
+
+  it ('has an id of #headertop', () => {
+    const el = component.find('#headertop');
+    expect(el.is('div')).toBe(true);
+  });
+
+  it('has child of #headertop with id #topmenu', () => {
+      const el = component.find('#headertop');
+      expect(el.contains('#topmenu')).toBe(true);
+  });
+
+  it('hides the "Login" link if user is logged in', () => {
+    const user = {id: 1};
+    const link = component.find('a[href="/login"]');
+    expect(link.text()).toBe('Login');
+    expect(link.classes()).not.toContain('hidden');
+
+    component.setData({user: user});
+    expect(link.classes()).toContain('hidden');
+  });
+
+  it('hides the "All Dashboards" link if url is not viewProjects.php', () => {
+    const link = component.find('a[href="viewProjects.php"]');
+    expect(link.classes()).not.toContain('hidden');
+
+    component.setData({location: 'viewProjects.php'});
+
+    expect(link.classes()).toContain('hidden');
+  });
+
+  it( 'hides the "Register" link if user is logged in', () => {
+    const user = {id: 1};
+    const link = component.find('a[href="register.php"]');
+
+    expect(link.classes()).not.toContain('hidden');
+
+    component.setData({user: user});
+
+    expect(link.classes()).toContain('hidden');
+  });
+
+  it('hides the "My CDash" link if user is not logged in', () => {
+    const user = {id: 1};
+    const link = component.find('a[href="user.php"]');
+    expect(link.text()).toBe('My CDash');
+
+    expect(link.classes()).toContain('hidden');
+
+    component.setData({user: user});
+
+    expect(link.classes()).not.toContain('hidden');
+  });
+
+  it('hides the "Log out" link if user is not logged in', () => {
+    const user = {id: 1};
+    const link = component.find('a[href="/logout"]');
+    expect(link.text()).toBe('Log out');
+
+    expect(link.classes()).toContain('hidden');
+
+    component.setData({user: user});
+
+    expect(link.classes()).not.toContain('hidden');
+  });
+});
