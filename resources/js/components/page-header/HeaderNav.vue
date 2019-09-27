28e1e8b235511713a7d6c29d6b3e6f9298dcb4e6
Bryon Bean <bryon.bean@kitware.com>
2019-09-27 08:51:00 -0400

    feat: adds #headernav vue component

---
+++ resources/js/components/page-header/HeaderNav.vue
@@ -0,0 +1,114 @@
+<template>
+    <nav class="projectnav clearfix">
+        <ul class="projectnav_controls clearfix">
+            <li id="header-nav-previous-btn" :class="previousClass">
+
+                <a :href="previous">
+                    <svg id="i-chevron-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+                        <path d="M20 30 L8 16 20 2" />
+                    </svg>
+                    PREV
+                </a>
+            </li>
+            <li id="header-nav-current-btn" :class="currentClass">
+                <a :href="current">CURRENT</a>
+            </li>
+            <li id="header-nav-next-btn" :class="nextClass">
+                <a :href="next">
+                    NEXT
+                    <svg id="i-chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+                        <path d="M12 30 L24 16 12 2" />
+                    </svg>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "HeaderNav",
+        props: ['previous', 'current', 'next'],
+        computed: {
+            previousClass () {
+                return typeof this.previous === 'undefined' ? 'btn-disabled' : 'btn';
+            },
+
+            currentClass () {
+                return typeof this.current === 'undefined' ? 'btn-disabled' : 'btn';
+            },
+
+            nextClass () {
+                return typeof this.next === 'undefined' ? 'btn-disabled' : 'btn';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    nav {
+        display: block;
+        margin: 0 auto;
+        padding: 19px;
+        order: 3;
+    }
+
+    .projectnav_controls {
+        border-radius: 3px;
+        display: inline-block;
+        list-style: none;
+        margin: 0;
+        overflow: hidden;
+        padding: 0;
+    }
+
+
+    .btn {
+        background: #555;
+        border: 1px solid #212121;
+        font-size: 11px;
+        margin-right: -1px;
+        text-transform: uppercase;
+        float: left;
+        cursor: pointer;
+    }
+
+    .btn-disabled {
+        background: #444;
+        border: 1px solid #212121;
+        font-size: 11px;
+        margin-right: -1px;
+        text-transform: uppercase;
+        float: left;
+    }
+
+    .btn-disabled a {
+        color: rgba(255, 255, 255, 0.5);
+    }
+
+    #header-nav-previous-btn {
+        border-right: 1px solid #212121;
+        text-align: left;
+    }
+
+    #header-nav-current-btn {
+        border-left: 1px solid #999;
+        border-right: 1px solid #212121;
+        text-align: center;
+    }
+
+    #header-nav-next-btn {
+        border-left: 1px solid #999;
+        text-align: right;
+    }
+
+    a {
+        color: white;
+        display: block;
+        font-size: 11px;
+        padding: 5px 12px 3px;
+        height: 100%;
+        text-decoration: none;
+        width: 100%;
+    }
+</style>
---
+++ tests/Spec/header-nav.spec.js
@@ -0,0 +1,33 @@
+import { mount } from 'vue-test-utils';
+import HeaderNav from "../../resources/js/components/page-header/HeaderNav.vue";
+import expect from 'expect';
+
+describe('HeaderNav', () => {
+  let component;
+
+  const testButtonState = (button) => {
+    const selector = `#header-nav-${button}-btn`;
+    const el = component.find(selector);
+    expect(el.classes()).toContain('btn-disabled');
+
+    component.setData({[button]: '/some/endpoint'});
+    expect(el.classes()).not.toContain('btn-disabled');
+    expect(el.classes()).toContain('btn');
+  };
+
+  beforeEach(() => {
+    component = mount(HeaderNav);
+  });
+
+  it('has a "Prev" button that is disabled when no previous href property exists', () => {
+    testButtonState('previous');
+  });
+
+  it('has a "Current" button that is disabled when no current href property exists', () => {
+    testButtonState('current');
+  });
+
+  it('has a "Next" button that is disabled when no next url property exists', () => {
+    testButtonState('next');
+  });
+});
