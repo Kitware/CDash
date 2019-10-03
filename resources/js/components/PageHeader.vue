72dd37070b5534dd631c7d517b33a2c821a5cb93
Bryon Bean <bryon.bean@kitware.com>
2019-10-03 11:16:12 -0400

    feat: adds PageHeader vue component

---
+++ resources/js/components/PageHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <HeaderTop :user="user" />
+    <HeaderBottom
+      :date="date"
+      :logo="logo"
+      :projectname="projectname"
+    />
+  </div>
+</template>
+
+<script>
+import HeaderTop from "./page-header/HeaderTop";
+import HeaderBottom from "./page-header/HeaderBottom";
+export default {
+  name: "Header",
+  components: {HeaderBottom, HeaderTop},
+  props: {
+    date: {
+      type: String,
+      default: '',
+    },
+    logo: {
+      type: String,
+      default: '',
+    },
+    projectname: {
+      type: String,
+      default: '',
+    },
+    user: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
