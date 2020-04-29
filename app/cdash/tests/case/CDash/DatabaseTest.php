fbd9f5ef11e3891f20c2b45530d359465addef49
Zack Galbreath <zack.galbreath@kitware.com>
2020-04-29 14:24:58 -0400

    Remove unused Database code

--- app/cdash/tests/case/CDash/DatabaseTest.php
+++ app/cdash/tests/case/CDash/DatabaseTest.php
@@ -57,74 +57,9 @@ class DatabaseTest extends CDashTestCase
 
     public function testGetPdoReturnsInstanceOfDBALSymphonyDriver()
     {
-        $config = Config::getInstance();
-        $config->set('CDASH_DB_NAME', 'my_abcd_database');
-        $config->set('CDASH_DB_HOST', 'localhost');
-        $config->set('CDASH_DB_PORT', '3307');
-        $config->set('CDASH_DB_PASS', 'ABCD123abcd!');
-        $config->set('CDASH_DB_TYPE', Database::DB_TYPE_PGSQL);
-        $config->set('CDASH_DB_LOGIN', 'thumper10-51-1');
-        $config->set('CDASH_DB_CONNECTION_TYPE', Database::DB_CONNECTION_TYPE_HOST);
-
         $db = Database::getInstance();
         $pdo = $db->getPdo();
-
         $this->assertInstanceOf('Doctrine\DBAL\Driver\PDOConnection', $pdo);
-    }
-
-    public function testBuildDsn()
-    {
-        $config = Config::getInstance();
-        $config->set('CDASH_DB_NAME', 'my_abcd_database');
-        $config->set('CDASH_DB_HOST', 'cdash.someserver.dev');
-        $config->set('CDASH_DB_PORT', null);
-        $config->set('CDASH_DB_PASS', 'ABCD123abcd!');
-        $config->set('CDASH_DB_TYPE', Database::DB_TYPE_PGSQL);
-        $config->set('CDASH_DB_LOGIN', 'thumper10-51-1');
-        $config->set('CDASH_DB_CONNECTION_TYPE', Database::DB_CONNECTION_TYPE_HOST);
-
-        $expected = 'pgsql:host=cdash.someserver.dev';
-
-        $db = Database::getInstance();
-        $actual = $db->buildDsn();
-
-        $this->assertEquals($expected, $actual);
-
-        // Add a port to the configuration, ensure port is included in dsn
-        $config->set('CDASH_DB_PORT', '3307');
-        $expected = 'pgsql:host=cdash.someserver.dev;port=3307';
-
-        Database::setInstance(Database::class, null);
-        $db = Database::getInstance();
-        $actual = $db->buildDsn();
-
-        // Pass database name as argument to buildDsn, ensure name is included in dsn
-        $this->assertEquals($expected, $actual);
-        $expected = 'pgsql:host=cdash.someserver.dev;port=3307;dbname=my_abcd_database';
-        $actual = $db->buildDsn($config->get('CDASH_DB_NAME'));
-
-        $this->assertEquals($expected, $actual);
-
-        // Change the database type to MySQL, ensure dns is properly formed
-        $config->set('CDASH_DB_TYPE', Database::DB_TYPE_MYSQL);
-        Database::setInstance(Database::class, null);
-        $db = Database::getInstance();
-
-        $expected = 'mysql:host=cdash.someserver.dev;port=3307;dbname=my_abcd_database';
-        $actual = $db->buildDsn($config->get('CDASH_DB_NAME'));
-
-        $this->assertEquals($expected, $actual);
-
-        // Change connection type to unix_socket, ensure that port is not set on dsn
-        Database::setInstance(Database::class, null);
-        $config->set('CDASH_DB_CONNECTION_TYPE', Database::DB_CONNECTION_TYPE_SOCKET);
-        $config->set('CDASH_DB_HOST', '/tmp/mysql.sock');
-        $db = Database::getInstance();
-
-        $expected = 'mysql:unix_socket=/tmp/mysql.sock';
-        $actual = $db->buildDsn();
-
-        $this->assertEquals($expected, $actual);
     }
 
     public function testExecute()
