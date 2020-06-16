eaae19da46eb8047c2bd641f604d658b35a8b529
Zack Galbreath <zack.galbreath@kitware.com>
2020-06-16 10:39:14 -0400

    Change default storage path to public/upload

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('upload'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
