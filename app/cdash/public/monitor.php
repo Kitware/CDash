caeec7944192bd647b3c169dc7b1c52750d7e5a3
Zack Galbreath <zack.galbreath@kitware.com>
2021-08-16 10:37:59 -0400

    Better formatting for monitor.php

--- app/cdash/public/monitor.php
+++ app/cdash/public/monitor.php
@@ -41,7 +41,7 @@ function echo_currently_processing_submissions()
 
     $sep = ', ';
 
-    echo '<h3>Currently Processing Submissions as of ' . $current_time[0] . ' UTC</h3>';
+    echo '<h1>Currently Processing Submissions as of ' . $current_time[0] . ' UTC</h1>';
     echo '<pre>';
     if (count($rows) > 0) {
         echo 'project name, backlog in hours' . "\n";
@@ -82,7 +82,7 @@ function echo_pending_submissions()
 
     $sep = ', ';
 
-    echo '<h3>Pending Submissions</h3>';
+    echo '<h1>Pending Submissions</h1>';
     echo '<pre>';
     if (count($rows) > 0) {
         echo 'project.name, project.id, count of pending queued submissions' . "\n";
@@ -102,50 +102,60 @@ function echo_pending_submissions()
 
 function echo_average_wait_time($projectid)
 {
+    $project_name = get_project_name($projectid);
     $config = Config::getInstance();
     if ($config->get('CDASH_DB_TYPE') == 'pgsql') {
-        $sql_query = 'SELECT extract(EPOCH FROM now() - created)/3600 as hours_ago, ' .
-            'current_time AS time_local, ' .
-            'count(created) AS num_files, ' .
-            'round(avg((extract(EPOCH FROM started - created)/3600)::numeric), 1) AS avg_hours_delay, ' .
-            'avg(extract(EPOCH FROM finished - started)) AS mean, ' .
-            'min(extract(EPOCH FROM finished - started)) AS shortest, ' .
-            'max(extract(EPOCH FROM finished - started)) AS longest ' .
-            "FROM submission WHERE status = 2 AND projectid = $projectid " .
-            'GROUP BY hours_ago ORDER BY hours_ago ASC LIMIT 48';
+        $sql_query = "SELECT extract(EPOCH FROM now() - created)/3600 as hours_ago,
+            current_time AS time_local,
+            count(created) AS num_files,
+            round(avg((extract(EPOCH FROM started - created)/3600)::numeric), 1) AS avg_hours_delay,
+            avg(extract(EPOCH FROM finished - started)) AS mean,
+            min(extract(EPOCH FROM finished - started)) AS shortest,
+            max(extract(EPOCH FROM finished - started)) AS longest
+            FROM submission WHERE status = 2 AND projectid = $projectid
+            GROUP BY hours_ago ORDER BY hours_ago ASC LIMIT 48";
     } else {
-        $sql_query = 'SELECT TIMESTAMPDIFF(HOUR, created, UTC_TIMESTAMP) as hours_ago, ' .
-            "TIME_FORMAT(CONVERT_TZ(created, '+00:00', 'SYSTEM'), '%l:00 %p') AS time_local, " .
-            'COUNT(created) AS num_files, ' .
-            'ROUND(AVG(TIMESTAMPDIFF(SECOND, created, started))/3600, 1) AS avg_hours_delay, ' .
-            'AVG(TIMESTAMPDIFF(SECOND, started, finished)) AS mean, ' .
-            'MIN(TIMESTAMPDIFF(SECOND, started, finished)) AS shortest, ' .
-            'MAX(TIMESTAMPDIFF(SECOND, started, finished)) AS longest ' .
-            "FROM submission WHERE status = 2 AND projectid = $projectid " .
-            'GROUP BY hours_ago ORDER BY hours_ago ASC LIMIT 48';
+        $sql_query = "SELECT TIMESTAMPDIFF(HOUR, created, UTC_TIMESTAMP) as hours_ago,
+            TIME_FORMAT(CONVERT_TZ(created, '+00:00', 'SYSTEM'), '%l:00 %p') AS time_local,
+            COUNT(created) AS num_files,
+            ROUND(AVG(TIMESTAMPDIFF(SECOND, created, started))/3600, 1) AS avg_hours_delay,
+            AVG(TIMESTAMPDIFF(SECOND, started, finished)) AS mean,
+            MIN(TIMESTAMPDIFF(SECOND, started, finished)) AS shortest,
+            MAX(TIMESTAMPDIFF(SECOND, started, finished)) AS longest
+            FROM submission WHERE status = 2 AND projectid = $projectid
+            GROUP BY hours_ago ORDER BY hours_ago ASC LIMIT 48";
     }
+
 
     $rows = pdo_all_rows_query($sql_query);
 
-    $sep = ', ';
-
-    echo 'projectid ' . $projectid . ' wait time' . "\n";
     if (count($rows) > 0) {
-        echo 'hours_ago, time_local, num_files, avg_hours_delay, mean, shortest, longest' . "\n";
+        echo "<h2>Wait times for $project_name</h2>\n";
+        echo "<table border=1>\n";
+        echo "<tr>\n";
+        echo "<th>Hours Ago</th>\n";
+        echo "<th>Local Time</th>\n";
+        echo "<th>Files Processed Successfully</th>\n";
+        echo "<th>Avg Hours Spent Queued Before Processing</th>\n";
+        echo "<th>Avg Seconds Spent Processing a File</th>\n";
+        echo "<th>Min Seconds Spent Processing a File</th>\n";
+        echo "<th>Max Seconds Spent Processing a File</th>\n";
+        echo "</tr>\n";
         foreach ($rows as $row) {
-            echo '    ' . $row['hours_ago'] .
-                $sep . $row['time_local'] .
-                $sep . $row['num_files'] .
-                $sep . $row['avg_hours_delay'] .
-                $sep . $row['mean'] .
-                $sep . $row['shortest'] .
-                $sep . $row['longest'] .
-                "\n";
+            echo "<tr>\n";
+            echo "<td style='text-align:center'>{$row['hours_ago']}</td>\n";
+            echo "<td style='text-align:center'>{$row['time_local']}</td>\n";
+            echo "<td style='text-align:center'>{$row['num_files']}</td>\n";
+            echo "<td style='text-align:center'>{$row['avg_hours_delay']}</td>\n";
+            echo "<td style='text-align:center'>{$row['mean']}</td>\n";
+            echo "<td style='text-align:center'>{$row['shortest']}</td>\n";
+            echo "<td style='text-align:center'>{$row['longest']}</td>\n";
+            echo "</tr>\n";
         }
+        echo "</table>\n";
     } else {
-        echo 'No average wait time data for projectid ' . $projectid . "\n";
+        echo "<h2>No average wait time data for $project_name</h2>\n";
     }
-    echo "\n";
 }
 
 function echo_average_wait_times()
@@ -154,8 +164,7 @@ function echo_average_wait_times()
         'SELECT projectid, COUNT(*) AS c FROM submission ' .
         'WHERE status=2 GROUP BY projectid');
 
-    echo '<h3>Average Wait Times</h3>';
-    echo '<pre>';
+    echo '<h1>Average Wait Times per Project</h1>';
     if (count($rows) > 0) {
         foreach ($rows as $row) {
             if ($row['c'] > 0) {
@@ -165,7 +174,6 @@ function echo_average_wait_times()
     } else {
         echo 'No finished submissions for average wait time measurement...' . "\n";
     }
-    echo '</pre>';
     echo '<br/>';
 }
 
@@ -177,16 +185,25 @@ function echo_submissionprocessor_table()
         'WHERE project.id = submissionprocessor.projectid '
     );
 
-    $sep = ', ';
-
-    echo '<h3>Table `submissionprocessor` (one row per project)</h3>';
-    echo '<pre>';
-    echo 'project.name, projectid, pid, lastupdated, locked' . "\n";
-    echo "\n";
+    echo '<h1>Table `submissionprocessor` (one row per project)</h1>';
+    echo "<table border=1>\n";
+    echo "<tr>\n";
+    echo "<th>Project Name</th>\n";
+    echo "<th>Project ID</th>\n";
+    echo "<th>Process ID</th>\n";
+    echo "<th>Last Updated</th>\n";
+    echo "<th>Locked</th>\n";
+    echo "</tr>\n";
     foreach ($rows as $row) {
-        echo $row['name'] . $sep . $row['projectid'] . $sep . $row['pid'] . $sep . $row['lastupdated'] . $sep . $row['locked'] . "\n";
+        echo "<tr>\n";
+        echo "<td style='text-align:center'>{$row['name']}</td>\n";
+        echo "<td style='text-align:center'>{$row['projectid']}</td>\n";
+        echo "<td style='text-align:center'>{$row['pid']}</td>\n";
+        echo "<td style='text-align:center'>{$row['lastupdated']}</td>\n";
+        echo "<td style='text-align:center'>{$row['locked']}</td>\n";
+        echo "</tr>\n";
     }
-    echo '</pre>';
+    echo "</table>\n";
     echo '<br/>';
 }
 
@@ -199,33 +216,44 @@ function echo_submission_table()
         $limit = pdo_real_escape_numeric($limit);
     }
 
+    echo "<h1>Table `submission` (most recently queued $limit)</h1>";
+    echo "<table border=1>\n";
+    echo "<tr>\n";
+    echo "<th>id</th>\n";
+    echo "<th>filename</th>\n";
+    echo "<th>projectid</th>\n";
+    echo "<th>status</th>\n";
+    echo "<th>attempts</th>\n";
+    echo "<th>filesize</th>\n";
+    echo "<th>filemd5sum</th>\n";
+    echo "<th>lastupdated</th>\n";
+    echo "<th>created</th>\n";
+    echo "<th>started</th>\n";
+    echo "<th>finished</th>\n";
+    echo "</tr>\n";
+
     $rows = pdo_all_rows_query(
         'SELECT * FROM ' . qid('submission') . ' ORDER BY id DESC LIMIT ' . $limit
     );
 
-    $sep = ', ';
 
-    echo "<h3>Table `submission` (most recently queued $limit)</h3>";
-    echo '<pre>';
-    echo 'id, filename, projectid, status, attempts, filesize, filemd5sum, ' .
-        'lastupdated, created, started, finished' . "\n";
-    echo "\n";
     foreach ($rows as $row) {
-        echo $row['id'] .
-            $sep . $row['filename'] .
-            $sep . $row['projectid'] .
-            $sep . $row['status'] .
-            $sep . $row['attempts'] .
-            $sep . $row['filesize'] .
-            $sep . $row['filemd5sum'] .
-            $sep . $row['lastupdated'] .
-            $sep . $row['created'] .
-            $sep . $row['started'] .
-            $sep . $row['finished'] .
-            "\n";
+        echo "<tr>\n";
+        echo "<td style='text-align:center'>{$row['id']}</td>\n";
+        echo "<td style='text-align:center'>{$row['filename']}</td>\n";
+        echo "<td style='text-align:center'>{$row['projectid']}</td>\n";
+        echo "<td style='text-align:center'>{$row['status']}</td>\n";
+        echo "<td style='text-align:center'>{$row['attempts']}</td>\n";
+        echo "<td style='text-align:center'>{$row['filesize']}</td>\n";
+        echo "<td style='text-align:center'>{$row['filemd5sum']}</td>\n";
+        echo "<td style='text-align:center'>{$row['lastupdated']}</td>\n";
+        echo "<td style='text-align:center'>{$row['created']}</td>\n";
+        echo "<td style='text-align:center'>{$row['started']}</td>\n";
+        echo "<td style='text-align:center'>{$row['finished']}</td>\n";
+        echo "</tr>\n";
     }
-    echo '</pre>';
-    echo '<br/>';
+    echo "</table>\n";
+    echo "<br/>\n";
 }
 
 if (Auth::check()) {
