fda22ed0b4844a4c046327319e9f3a336d340e76
Zack Galbreath <zack.galbreath@kitware.com>
2021-12-21 14:05:37 -0500

    Improve existing 'test history' test case

--- app/cdash/tests/test_testhistory.php
+++ app/cdash/tests/test_testhistory.php
@@ -28,56 +28,70 @@ class TestHistoryTestCase extends KWWebTestCase
         }
 
         // Get the IDs for the two builds that we just created.
-        $result = pdo_query("SELECT id FROM build WHERE name='TestHistory'");
+        $result = pdo_query("SELECT id FROM build WHERE name='TestHistory' ORDER BY starttime");
         $num_builds = pdo_num_rows($result);
         if ($num_builds != 2) {
             $this->fail("Expected 2 builds, found $num_builds");
             return 1;
         }
 
-        $buildids = array();
+        $buildids = [];
         while ($row = pdo_fetch_array($result)) {
             $buildids[] = $row['id'];
         }
 
         // Verify that testing history matches what we expect.
-        $content = $this->connect($this->url . '/api/v1/viewTest.php?groupid=15&previous_builds=' . $buildids[1] . ',+' . $buildids[0] . '&projectid=5&tests%5B%5D=fails&tests%5B%5D=notrun&tests%5B%5D=flaky&tests%5B%5D=passes&time_begin=2015-11-16T01:00:00&time_end=2015-11-17T01:00:00');
-
-        $jsonobj = json_decode($content, true);
-
-        $success = true;
-        $error_message = '';
-
+        $url = "{$this->url}/api/v1/viewTest.php?buildid={$buildids[1]}&groupid=15&previous_builds={$buildids[1]},+{$buildids[0]}&projectid=5&tests%5B%5D=fails&tests%5B%5D=notrun&tests%5B%5D=flaky&tests%5B%5D=passes&time_begin=2015-11-16T01:00:00&time_end=2015-11-17T01:00:00";
+        $client = $this->getGuzzleClient();
+        $response = $client->request('GET',
+                $url,
+                ['http_errors' => false]);
+        $jsonobj = json_decode($response->getBody(), true);
         foreach ($jsonobj['tests'] as $test) {
             $history = $test['history'];
             if ($test['name'] == 'fails' && $history != 'Broken') {
-                $error_message = "Expected history for test 'fails' to be 'Broken', instead found '$history'";
-                $success = false;
+                $this->fail("Expected history for test 'fails' to be 'Broken', instead found '$history'");
             }
             if ($test['name'] == 'notrun' && $history != 'Inactive') {
-                $error_message = "Expected history for test 'notrun' to be 'Inactive', instead found '$history'";
-                $success = false;
+                $this->fail("Expected history for test 'notrun' to be 'Inactive', instead found '$history'");
             }
             if ($test['name'] == 'flaky' && $history != 'Unstable') {
-                $error_message = "Expected history for test 'flaky' to be 'Unstable', instead found '$history'";
-                $success = false;
+                $this->fail("Expected history for test 'flaky' to be 'Unstable', instead found '$history'");
             }
             if ($test['name'] == 'passes' && $history != 'Stable') {
-                $error_message = "Expected history for test 'passes' to be 'Stable', instead found '$history'";
-                $success = false;
+                $this->fail("Expected history for test 'passes' to be 'Stable', instead found '$history'");
             }
         }
+
+        // Verify test graphs for our 'flaky' test.
+        $test_result = \DB::select(
+            "SELECT id FROM test WHERE name = 'flaky' AND projectid = 5");
+        $testid = $test_result[0]->id;
+        $flaky_id_1 = \DB::table('build2test')
+            ->where('buildid', '=', $buildids[0])
+            ->where('testid', '=', $testid)
+            ->value('id');
+        $flaky_id_2 = \DB::table('build2test')
+            ->where('buildid', '=', $buildids[1])
+            ->where('testid', '=', $testid)
+            ->value('id');
+
+        $this->get("{$this->url}/api/v1/testGraph.php?testid={$testid}&buildid={$buildids[1]}&type=time");
+        $content = $this->getBrowser()->getContent();
+        $jsonobj = json_decode($content, true);
+        $this->assertEqual(0, $jsonobj[0]['data'][0]['y']);
+        $this->assertEqual(1, $jsonobj[0]['data'][1]['y']);
+
+        $this->get("{$this->url}/api/v1/testGraph.php?testid={$testid}&buildid={$buildids[1]}&type=status");
+        $content = $this->getBrowser()->getContent();
+        $jsonobj = json_decode($content, true);
+        $this->assertEqual(1, $jsonobj[0]['data'][0]['y']);
+        $this->assertEqual($flaky_id_2, $jsonobj[0]['data'][0]['buildtestid']);
+        $this->assertEqual(-1, $jsonobj[1]['data'][0]['y']);
+        $this->assertEqual($flaky_id_1, $jsonobj[1]['data'][0]['buildtestid']);
 
         // Delete the builds that we created during this test.
         remove_build($buildids[0]);
         remove_build($buildids[1]);
-
-        if ($success) {
-            $this->pass('Test passed');
-            return 0;
-        } else {
-            $this->fail($error_message);
-            return 1;
-        }
     }
 }
