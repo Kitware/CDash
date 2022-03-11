19039665c3e0b147d370623e1fcdfd321daf0171
Zack Galbreath <zack.galbreath@kitware.com>
2022-03-11 11:28:06 -0500

    Version bump for CDash v3.1.0

--- config/cdash.php
+++ config/cdash.php
@@ -22,7 +22,7 @@ return [
         'expires' => env('PASSWORD_EXPIRATION', 0),
         'unique' => env('UNIQUE_PASSWORD_COUNT', 0),
     ],
-    'version' => '3.0.1',
+    'version' => '3.1.0',
     'registration' => [
         'email' => [
             'verify' => env('REGISTRATION_EMAIL_VERIFY', true),
