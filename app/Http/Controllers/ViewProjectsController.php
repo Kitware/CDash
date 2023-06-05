a32a600b5cb814c6bd51224ebf3710f36b1dc4ad
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-06-05 15:01:14 -0400

    Redirect `viewProjects.php` to login if no public projects (#1473)

--- app/Http/Controllers/ViewProjectsController.php
+++ app/Http/Controllers/ViewProjectsController.php
@@ -2,13 +2,26 @@
 namespace App\Http\Controllers;
 
 use CDash\Database;
+use CDash\Model\Project;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class ViewProjectsController extends AbstractController
 {
-    public function viewAllProjects(): View
+    public function viewAllProjects(): View|RedirectResponse
     {
+        $num_public_projects = (int) DB::select('
+                                     SELECT COUNT(*) AS c FROM project WHERE public=?
+                                 ', [Project::ACCESS_PUBLIC])[0]->c;
+
+        // If there are no public projects to see, redirect to the login page
+        if (!Auth::check() && $num_public_projects === 0) {
+            return $this->redirectToLogin();
+        }
+
         return view("project.view-all-projects");
     }
 
