f233f4d038c7a356054a64209ef33be415f482c2
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-06-27 08:49:07 -0400

    Refactor `viewDynamicAnalysisFile.php` API endpoint

--- app/Http/Controllers/DynamicAnalysisController.php
+++ app/Http/Controllers/DynamicAnalysisController.php
@@ -3,9 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Services\PageTimer;
 use App\Services\TestingDay;
-use CDash\Model\Build;
 use CDash\Model\DynamicAnalysis;
-use CDash\Model\Project;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 
@@ -14,7 +12,6 @@ final class DynamicAnalysisController extends AbstractBuildController
     public function apiViewDynamicAnalysis(): JsonResponse
     {
         $pageTimer = new PageTimer();
-        $response = [];
 
         if (!isset($_GET['buildid']) || !is_numeric($_GET['buildid'])) {
             abort(400, 'Invalid buildid!');
@@ -23,12 +20,11 @@ final class DynamicAnalysisController extends AbstractBuildController
 
         // lookup table to make the reported defect types more understandable.
         // feel free to expand as necessary.
-        $defect_nice_names = array(
+        $defect_nice_names = [
             'FIM' => 'Freeing Invalid Memory',
             'IPR' => 'Invalid Pointer Read',
-            'IPW' => 'Invalid Pointer Write');
-
-        $response['displaylabels'] = $this->project->DisplayLabels;
+            'IPW' => 'Invalid Pointer Write',
+        ];
 
         $date = TestingDay::get($this->project, $this->build->StartTime);
 
@@ -158,10 +154,9 @@ final class DynamicAnalysisController extends AbstractBuildController
         return response()->json(cast_data_for_JSON($response));
     }
 
-    public function viewDynamicAnalysisFile(): JsonResponse
+    public function apiViewDynamicAnalysisFile(): JsonResponse
     {
         $pageTimer = new PageTimer();
-        $response = [];
 
         // Make sure a valid id was specified.
         if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
@@ -175,52 +170,33 @@ final class DynamicAnalysisController extends AbstractBuildController
         }
 
         // Get the build associated with this analysis.
-        $build = new Build();
-        $build->Id = $DA->BuildId;
-        if (!$build->Exists()) {
-            abort(404, 'This build does not exist. Maybe it has been deleted.');
-        }
-        $build->FillFromId($build->Id);
+        $this->setBuildById($DA->BuildId);
 
-        // Make sure the user has access to this project.
-        if (!can_access_project($build->ProjectId)) {
-            return;
-        }
-
-        $project = new Project();
-        $project->Id = $build->ProjectId;
-        $project->Fill();
-        if (!can_access_project($project->Id)) {
-            $response['error'] = 'You do not have permission to view this project.';
-            echo json_encode($response);
-            return;
-        }
-
-        $date = TestingDay::get($project, $build->StartTime);
+        $date = TestingDay::get($this->project, $this->build->StartTime);
         $response = begin_JSON_response();
-        get_dashboard_JSON($project->Name, $date, $response);
-        $response['title'] = "$project->Name : Dynamic Analysis";
+        get_dashboard_JSON($this->project->Name, $date, $response);
+        $response['title'] = "{$this->project->Name} : Dynamic Analysis";
 
         // Build
         $build_response = [];
-        $build_response['site'] = $build->GetSite()->name;
-        $build_response['buildname'] = $build->Name;
-        $build_response['buildid'] = $build->Id;
-        $build_response['buildtime'] = $build->StartTime;
+        $build_response['site'] = $this->build->GetSite()->name;
+        $build_response['buildname'] = $this->build->Name;
+        $build_response['buildid'] = $this->build->Id;
+        $build_response['buildtime'] = $this->build->StartTime;
         $response['build'] = $build_response;
 
         // Menu
         $menu_response = [];
-        $menu_response['back'] = "viewDynamicAnalysis.php?buildid=$build->Id";
-        $previous_id = $DA->GetPreviousId($build);
+        $menu_response['back'] = "viewDynamicAnalysis.php?buildid={$this->build->Id}";
+        $previous_id = $DA->GetPreviousId($this->build);
         if ($previous_id > 0) {
             $menu_response['previous'] = "viewDynamicAnalysisFile.php?id=$previous_id";
         } else {
             $menu_response['previous'] = false;
         }
-        $current_id = $DA->GetLastId($build);
+        $current_id = $DA->GetLastId($this->build);
         $menu_response['current'] = "viewDynamicAnalysisFile.php?id=$current_id";
-        $next_id = $DA->GetNextId($build);
+        $next_id = $DA->GetNextId($this->build);
         if ($next_id > 0) {
             $menu_response['next'] = "viewDynamicAnalysisFile.php?id=$next_id";
         } else {
@@ -234,11 +210,11 @@ final class DynamicAnalysisController extends AbstractBuildController
         $DA_response['filename'] = $DA->Name;
         // Only display the first 1MB of the log (in case it's huge)
         $DA_response['log'] = substr($DA->Log, 0, 1024 * 1024);
-        $href = "testSummary.php?project=$project->Id&name=$DA->Name&date=$date";
+        $href = "testSummary.php?project={$this->project->Id}&name=$DA->Name&date=$date";
         $DA_response['href'] = $href;
         $response['dynamicanalysis'] = $DA_response;
 
         $pageTimer->end($response);
-        echo json_encode(cast_data_for_JSON($response));
+        return response()->json(cast_data_for_JSON($response));
     }
 }
