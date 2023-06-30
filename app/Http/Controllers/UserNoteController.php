167d1901bb7e1955e0c8a360322e3106b739826b
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-06-30 13:00:18 -0400

    Refactor `addNote.php` API endpoint (#1528)
    This PR is part of our ongoing effort to migrate all of the API
    endpoints to Laravel's routing/controller system.
    
    As an aside, it is *incredibly* confusing that we have both "user notes"
    and "build notes", both of which are referred to as "notes", and tested
    in the same test file. To differentiate between the two types of notes,
    I have split the testing code into two separate files which each test
    different kinds of notes.

---
+++ app/Http/Controllers/UserNoteController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Http\Controllers;
+
+use CDash\Model\BuildUserNote;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class UserNoteController extends AbstractBuildController
+{
+    public function apiAddUserNote(): JsonResponse
+    {
+        if (!is_numeric(request()->post('buildid'))) {
+            abort(400, 'Invalid buildid!');
+        }
+        $this->setBuildById((int) request()->post('buildid'));
+
+        if (request()->post('AddNote') === null || request()->post('Status') === null ||
+            strlen(request()->post('AddNote')) < 1 || strlen(request()->post('Status')) < 1) {
+            abort(400, 'No note specified');
+        }
+
+        // Add the note.
+        $userNote = new BuildUserNote();
+        $userNote->BuildId = $this->build->Id;
+        $userNote->UserId = Auth::id();
+        $userNote->Note = request()->post('AddNote');
+        $userNote->Status = request()->post('Status');
+        $userNote->TimeStamp = gmdate(FMT_DATETIME);
+
+        if (!$userNote->Insert()) {
+            abort(400, 'Error adding note');
+        }
+
+        $response = [];
+        $response['note'] = $userNote->marshal();
+        return response()->json(cast_data_for_JSON($response));
+    }
+}
