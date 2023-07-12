30fa40a748465487b299b969c6519f6bd0b6e0a5
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-07-12 17:51:24 -0400

    Refactor `is_build_expected.php` API endpoint

--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,8 @@ Route::get('/v1/getPreviousBuilds.php', 'BuildController@apiGetPreviousBuilds');
 
 Route::get('/v1/testSummary.php', 'TestController@apiTestSummary');
 
+Route::get('/v1/is_build_expected.php', 'BuildController@apiBuildExpected');
+
 Route::middleware(['auth'])->group(function () {
     Route::post('/authtokens/create', 'AuthTokenController@createToken');
     Route::delete('/authtokens/delete/{token_hash}', 'AuthTokenController@deleteToken');
