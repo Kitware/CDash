976f7443e022fad97720713f65427b0190d72a62
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-07-17 08:48:18 -0400

    Add index to improve `/projects` performance

---
+++ database/migrations/2023_07_17_124401_build_projectid_submittime_index.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('build', function (Blueprint $table) {
+            $table->index(['projectid', 'submittime']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('build', function (Blueprint $table) {
+            $table->dropIndex(['projectid', 'submittime']);
+        });
+    }
+};
