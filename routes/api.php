e9e1473c4ad5ac0f51504d891e18a259d39eba35
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-07-19 09:19:57 -0400

    Refactor `filterdata.php` API endpoint (#1570)
    This PR is part of our ongoing effort to migrate the API to Laravel's
    routing/controller system.

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,8 @@ Route::get('/v1/user.php', 'UserController@userPageContent');
 
 Route::get('/v1/userStatistics.php', 'UserStatisticsController@api');
 
+Route::get('/v1/filterdata.php', 'FilterController@getFilterDataArray');
+
 Route::get('/v1/viewSubProjects.php', 'SubProjectController@apiViewSubProjects');
 
 Route::get('/v1/viewDynamicAnalysis.php', 'DynamicAnalysisController@apiViewDynamicAnalysis');
