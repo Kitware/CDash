df4e91780b50b8c0fa3e7fc169e95ba239950c18
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-08-09 19:11:22 -0400

    Migrate `manageBanner.php` from XSL to Blade

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $projectid
+ * @property string $text
+ *
+ * @mixin Builder<Banner>
+ */
 class Banner extends Model
 {
     public $timestamps = false;
