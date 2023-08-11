33006fb6f0935535a6c0eabd5d371d002fc55d67
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-08-11 11:25:29 -0400

    Remove unused build header view composer

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,15 +2,11 @@
 declare(strict_types=1);
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        View::composer(
-            'build.page-header', 'App\Http\View\Composers\BuildPageHeaderComposer'
-        );
     }
 }
