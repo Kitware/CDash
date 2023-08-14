17b4816218819a9e62108fd9dd8f5c78f1c6ce99
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-08-14 08:58:33 -0400

    Refactor subproject dependencies graph (#1638)
    This PR is part of an ongoing effort to migrate our existing XSL
    templates to Blade templates.
    
    As part of this refactor, I was able to eliminate a large amount of dead
    code.

--- app/Http/Controllers/SubProjectController.php
+++ app/Http/Controllers/SubProjectController.php
@@ -111,74 +111,13 @@ final class SubProjectController extends AbstractProjectController
         $pageTimer->end($response);
         return response()->json(cast_data_for_JSON($response));
     }
- 
+
     public function dependenciesGraph(): View|RedirectResponse
     {
-        if (!isset($_GET['project'])) {
-            abort(400, 'You must specify a project to access this resource.');
-        }
-        $this->setProjectByName($_GET['project']);
+        $this->setProjectByName($_GET['project'] ?? '');
 
-        @$date = $_GET['date'];
-        if ($date != null) {
-            $date = htmlspecialchars(pdo_real_escape_string($date));
-        }
-
-        $svnurl = make_cdash_url(htmlentities($this->project->CvsUrl));
-        $homeurl = make_cdash_url(htmlentities($this->project->HomeUrl));
-        $bugurl = make_cdash_url(htmlentities($this->project->BugTrackerUrl));
-        $googletracker = htmlentities($this->project->GoogleTracker);
-        $docurl = make_cdash_url(htmlentities($this->project->DocumentationUrl));
-
-        $xml = begin_XML_for_XSLT();
-
-        list($previousdate, $currentstarttime, $nextdate) = get_dates($date, $this->project->NightlyTime);
-
-        // Main dashboard section
-        $xml .=
-             '<dashboard>
-              <datetime>' . date('l, F d Y H:i:s T', time()) . '</datetime>
-              <date>' . $date . '</date>
-              <unixtimestamp>' . $currentstarttime . '</unixtimestamp>
-              <svn>' . $svnurl . '</svn>
-              <bugtracker>' . $bugurl . '</bugtracker>
-              <googletracker>' . $googletracker . '</googletracker>
-              <documentation>' . $docurl . '</documentation>
-              <projectid>' . $this->project->Id . '</projectid>
-              <projectname>' . $this->project->Name . '</projectname>
-              <projectname_encoded>' . urlencode($this->project->Name) . '</projectname_encoded>
-              <previousdate>' . $previousdate . '</previousdate>
-              <projectpublic>' . $this->project->Public . '</projectpublic>
-              <nextdate>' . $nextdate . '</nextdate>';
-
-        if (empty($this->project->HomeUrl)) {
-            $xml .= '<home>index.php?project=' . urlencode($this->project->Name) . '</home>';
-        } else {
-            $xml .= '<home>' . $homeurl . '</home>';
-        }
-
-        if ($currentstarttime > time()) {
-            $xml .= '<future>1</future>';
-        } else {
-            $xml .= '<future>0</future>';
-        }
-        $xml .= '</dashboard>';
-
-        // Menu definition
-        $xml .= '<menu>';
-        if (!isset($date) || strlen($date) < 8 || date(FMT_DATE, $currentstarttime) == date(FMT_DATE)) {
-            $xml .= add_XML_value('nonext', '1');
-        }
-        $xml .= '</menu>';
-
-        $xml .= '</cdash>';
-
-        return view('cdash', [
-            'xsl' => true,
-            'xsl_content' => generate_XSLT($xml, base_path() . '/app/cdash/public/viewSubProjectDependenciesGraph', true),
-            'project' => $this->project,
-            'title' => 'SubProject Dependencies Graph'
-        ]);
+        return view('project.subproject-dependencies')
+            ->with('project', $this->project);
     }
 
     public function apiViewSubProjects(): JsonResponse
