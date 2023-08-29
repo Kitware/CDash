7c1febcc08da99fa19b4770b56a2a73907e1d42a
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-08-29 10:07:57 -0400

    Refactor `manageMeasurements.php` API endpoint (#1665)
    This PR continues our ongoing effort to move all of the individual file
    API endpoints to Laravel's routing scheme. Additionally, I fixed a
    security vulnerability through which a project administrator could
    delete measurements in projects other than their own.

--- app/Http/Controllers/ManageMeasurementsController.php
+++ app/Http/Controllers/ManageMeasurementsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Measurement;
+use App\Services\PageTimer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Gate;
 
 final class ManageMeasurementsController extends AbstractProjectController
@@ -13,5 +16,102 @@ final class ManageMeasurementsController extends AbstractProjectController
         Gate::authorize('edit-project', $this->project);
 
         return $this->view('admin.measurements', 'Test Measurements');
+    }
+
+    public function apiGet(): JsonResponse
+    {
+        $this->setProjectById((int) request()->query('projectid'));
+        Gate::authorize('edit-project', $this->project);
+
+        $pageTimer = new PageTimer();
+        $response = begin_JSON_response();
+
+        get_dashboard_JSON($this->project->GetName(), null, $response);
+        $response['title'] = "{$this->project->Name} Test Measurements";
+
+        // Menu
+        $menu_response = [];
+        $menu_response['back'] = 'user.php';
+        $response['menu'] = $menu_response;
+        $response['hidenav'] = true;
+
+        // Get any measurements associated with this project's tests.
+        $measurements_response = [];
+        $measurements = Measurement::where('projectid', $this->project->Id)
+            ->orderBy('position', 'asc')
+            ->get();
+
+        foreach ($measurements as $measurement) {
+            $measurement_response = [];
+            $measurement_response['id'] = $measurement->id;
+            $measurement_response['name'] = $measurement->name;
+            $measurement_response['position'] = $measurement->position;
+            $measurements_response[] = $measurement_response;
+        }
+        $response['measurements'] = $measurements_response;
+        $pageTimer->end($response);
+        return response()->json(cast_data_for_JSON($response));
+    }
+
+    public function apiPost(): JsonResponse
+    {
+        $this->setProjectById((int) request()->input('projectid'));
+        Gate::authorize('edit-project', $this->project);
+
+        if (!request()->filled('measurements') || !is_array(request()->input('measurements'))) {
+            abort(400, 'A "measurements" parameter must be specified.');
+        }
+
+        $OK = true;
+        $new_ID = null;
+        foreach (request()->input('measurements') as $measurement_data) {
+            $id = (int) $measurement_data['id'];
+            if ($id > 0) {
+                // Update an existing measurement rather than creating a new one.
+                $measurement = Measurement::find($id);
+            } else {
+                $measurement = new Measurement();
+            }
+            $measurement->projectid = $this->project->Id;
+            $measurement->name = $measurement_data['name'];
+            $measurement->position = $measurement_data['position'];
+            if (!$measurement->save()) {
+                $OK = false;
+            } elseif ($id < 1) {
+                // Report the ID of the newly created measurement (if any).
+                $new_ID = $measurement->id;
+            }
+        }
+
+        if (!$OK) {
+            abort(500);
+        }
+
+        if ($new_ID) {
+            $response = ['id' => $new_ID];
+            return response()->json($response);
+        }
+        return response()->json();
+    }
+
+    public function apiDelete(): JsonResponse
+    {
+        $this->setProjectById((int) request()->input('projectid'));
+        Gate::authorize('edit-project', $this->project);
+
+        if (!request()->filled('id')) {
+            abort(400, 'Invalid measurement ID provided.');
+        }
+
+        $deleted = Measurement::where([
+            'id' => (int) request()->input('id'),
+            'projectid' => $this->project->Id,
+        ])->delete();
+
+        if ($deleted) {
+            return response()->json();
+        } else {
+            return response()->json([], 404);
+        }
     }
 }
--- app/Models/Measurement.php
+++ app/Models/Measurement.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $projectid
+ * @property string $name
+ * @property int $position
+ *
+ * @mixin Builder<Measurement>
+ */
 class Measurement extends Model
 {
     protected $table = 'measurement';
-    protected $fillable = ['projectid', 'name', 'position'];
+    protected $fillable = [
+        'projectid',
+        'name',
+        'position',
+    ];
 
     public $timestamps = false;
 }
