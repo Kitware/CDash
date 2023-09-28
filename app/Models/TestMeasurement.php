5cad3f0fd4f91ac9c9bdd4ae933177fc47be76d7
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-09-28 10:30:58 -0400

    `queryTests.php`: Optimize testmeasurement query (#1722)
    `queryTests.php` currently executes a query to check for test
    measurements for each row in the table. This contributes to the slowness
    observed on this page for bigger projects.
    
    The offensive query has been moved outside the loop and now only runs a
    larger aggregate query once.

--- app/Models/TestMeasurement.php
+++ app/Models/TestMeasurement.php
@@ -3,11 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * @property int $outputid
+ * @property string $name
+ * @property string $type
+ * @property string $value
+ *
+ * @mixin Builder<TestMeasurement>
+ */
 class TestMeasurement extends Model
 {
     protected $table = 'testmeasurement';
-    protected $fillable = ['outputid', 'name', 'type', 'value'];
+
+    protected $fillable = [
+        'outputid',
+        'name',
+        'type',
+        'value',
+    ];
+
+    protected $casts = [
+        'outputid' => 'integer',
+    ];
 
     public $timestamps = false;
 }
