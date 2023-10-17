Kitware/CDash

commit 299dbac704ef5ccf1f809b56f92896f7c11ca079

Convert build update page from AngularJS to Vue (#1561)
This PR is part of our ongoing effort to migrate from AngularJS to Vue.
There should be no functionality differences between the old and new
versions of the page, beyond a new `/build/<id>/update` route.

app/Http/Controllers/BuildController.php
@@ -15,6 +15,7 @@ use CDash\ServiceContainer;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
@@ -38,6 +39,11 @@ class BuildController extends AbstractBuildController
     public function summary($build_id = null)
     {
         return $this->renderBuildPage($build_id, 'summary', 'Build Summary');
+    }
+
+    public function update(int $build_id): View
+    {
+        return $this->renderBuildPage($build_id, 'update', 'Files Updated');
     }
 
     protected function renderBuildPage(int $build_id, string $page_name, string $page_title = '')
@@ -196,6 +202,227 @@ class BuildController extends AbstractBuildController
             'project' => $this->project,
             'title' => 'Build Overview'
         ]);
+    }
+
+    public function viewUpdatePageContent(): JsonResponse
+    {
+        $pageTimer = new PageTimer();
+
+        $this->setBuildById(intval($_GET['buildid'] ?? -1));
+
+        $db = Database::getInstance();
+
+        $date = TestingDay::get($this->project, $this->build->StartTime);
+        $response = begin_JSON_response();
+        get_dashboard_JSON($this->project->Name, $date, $response);
+
+        // Menu
+        $menu_response = [];
+        $menu_response['back'] = "index.php?project={$this->project->Name}&date=$date";
+
+        $previous_buildid = $this->build->GetPreviousBuildId();
+        $current_buildid = $this->build->GetCurrentBuildId();
+        $next_buildid = $this->build->GetNextBuildId();
+
+        if ($previous_buildid > 0) {
+            $menu_response['previous'] = url("/build/$previous_buildid/update");
+        } else {
+            $menu_response['previous'] = false;
+        }
+
+        $menu_response['current'] = url("/build/$current_buildid/update");
+
+        if ($next_buildid > 0) {
+            $menu_response['next'] = url("/build/$next_buildid/update");
+        } else {
+            $menu_response['next'] = false;
+        }
+        $response['menu'] = $menu_response;
+
+        // Build
+        $site = $this->build->GetSite();
+
+        $build_response = [];
+        $build_response['site'] = $site->name;
+        $build_response['siteid'] = $site->id;
+        $build_response['buildname'] = $this->build->Name;
+        $build_response['buildid'] = $this->build->Id;
+        $build_response['buildtime'] = date('D, d M Y H:i:s T', strtotime($this->build->StartTime . ' UTC'));
+        $response['build'] = $build_response;
+
+        // Update
+        $update = new BuildUpdate();
+        $update->BuildId = $this->build->Id;
+        $update->FillFromBuildId();
+
+        $update_response = [];
+        if (strlen($update->Status) > 0 && $update->Status != '0') {
+            $update_response['status'] = $update->Status;
+        } else {
+            $update_response['status'] = ''; // empty status
+        }
+        $update_response['revision'] = $update->Revision;
+        $update_response['priorrevision'] = $update->PriorRevision;
+        $update_response['path'] = $update->Path;
+        $update_response['revisionurl'] =
+            get_revision_url($this->project->Id, $update->Revision, $update->PriorRevision);
+        $update_response['revisiondiff'] =
+            get_revision_url($this->project->Id, $update->PriorRevision, ''); // no prior prior revision...
+        $response['update'] = $update_response;
+
+        $directoryarray = [];
+        $updatearray1 = [];
+        // Create an array so we can sort it
+        foreach ($update->GetFiles() as $update_file) {
+            $file = [];
+            $file['filename'] = $update_file->Filename;
+            $file['author'] = $update_file->Author;
+            $file['status'] = $update_file->Status;
+
+            // Only display email if the user is logged in.
+            if (Auth::check()) {
+                if ($update_file->Email == '') {
+                    // Try to find author email from repository credentials.
+                    $stmt = $db->prepare("
+                SELECT email FROM user WHERE id IN (
+                  SELECT up.userid FROM user2project AS up, user2repository AS ur
+                   WHERE ur.userid=up.userid
+                   AND up.projectid=:projectid
+                   AND ur.credential=:author
+                   AND (ur.projectid=0 OR ur.projectid=:projectid) )
+                   LIMIT 1");
+                    $stmt->bindParam(':projectid', $this->project->Id);
+                    $stmt->bindParam(':author', $file['author']);
+                    $db->execute($stmt);
+                    $file['email'] = $stmt ? $stmt->fetchColumn() : '';
+                } else {
+                    $file['email'] = $update_file->Email;
+                }
+            } else {
+                $file['email'] = '';
+            }
+
+            $file['log'] = $update_file->Log;
+            $file['revision'] = $update_file->Revision;
+            $updatearray1[] = $file;
+            $directoryarray[] = substr($update_file->Filename, 0, strrpos($update_file->Filename, '/'));
+        }
+
+        $directoryarray = array_unique($directoryarray);
+
+        usort($directoryarray, function ($a, $b) {
+            return $a > $b ? 1 : 0;
+        });
+        usort($updatearray1, function ($a, $b) {
+            // Extract directory
+            $filenamea = $a['filename'];
+            $filenameb = $b['filename'];
+            return $filenamea > $filenameb ? 1 : 0;
+        });
+
+        $updatearray = [];
+
+        foreach ($directoryarray as $directory) {
+            foreach ($updatearray1 as $update) {
+                $filename = $update['filename'];
+                if (substr($filename, 0, strrpos($filename, '/')) == $directory) {
+                    $updatearray[] = $update;
+                }
+            }
+        }
+
+        // These variables represent a list of directories that contain a list of files.
+        $updated_files = [];
+        $modified_files = [];
+        $conflicting_files = [];
+
+        $num_updated_files = 0;
+        $num_modified_files = 0;
+        $num_conflicting_files = 0;
+
+        foreach ($updatearray as $file) {
+            $filename = $file['filename'];
+            $filename = str_replace('\\', '/', $filename);
+            $directory = substr($filename, 0, strrpos($filename, '/'));
+
+            $pos = strrpos($filename, '/');
+            if ($pos !== false) {
+                $filename = substr($filename, $pos + 1);
+            }
+
+            $log = $file['log'];
+            $status = $file['status'];
+            $revision = $file['revision'];
+            $log = str_replace("\r", ' ', $log);
+            $log = str_replace("\n", ' ', $log);
+            // Do this twice so that <something> ends up as
+            // &amp;lt;something&amp;gt; because it gets sent to a
+            // javascript function not just displayed as html
+            $log = XMLStrFormat($log); // Apparently no need to do this twice anymore
+            $log = XMLStrFormat($log);
+
+            $log = trim($log);
+
+            $file['log'] = $log;
+            $file['filename'] = $filename;
+            $file['bugid'] = '0';
+            $file['bugpos'] = '0';
+            // This field is redundant because of the way our data is organized.
+            unset($file['status']);
+
+            if ($status == 'UPDATED') {
+                $diff_url = get_diff_url($this->project->Id, $this->project->CvsUrl, $directory, $filename, $revision);
+                $diff_url = XMLStrFormat($diff_url);
+                $file['diffurl'] = $diff_url;
+                $this->add_file($file, $directory, $updated_files);
+                $num_updated_files++;
+            } elseif ($status == 'MODIFIED') {
+                $diff_url = get_diff_url($this->project->Id, $this->project->CvsUrl, $directory, $filename);
+                $diff_url = XMLStrFormat($diff_url);
+                $file['diffurl'] = $diff_url;
+                $this->add_file($file, $directory, $modified_files);
+                $num_modified_files++;
+            } else {
+                //CONFLICTED
+                $diff_url = get_diff_url($this->project->Id, $this->project->CvsUrl, $directory, $filename);
+                $diff_url = XMLStrFormat($diff_url);
+                $file['diffurl'] = $diff_url;
+                $this->add_file($file, $directory, $conflicting_files);
+                $num_conflicting_files++;
+            }
+        }
+
+        $update_groups = [
+            [
+                'description' => "{$this->project->Name} Updated Files ($num_updated_files)",
+                'directories' => $updated_files
+            ],
+            [
+                'description' => "Modified Files ($num_modified_files)",
+                'directories' => $modified_files
+            ],
+            [
+                'description' => "Conflicting Files ($num_conflicting_files)",
+                'directories' => $conflicting_files
+            ]
+        ];
+        $response['updategroups'] = $update_groups;
+
+        $pageTimer->end($response);
+        return response()->json(cast_data_for_JSON($response));
+    }
+
+    private function add_file($file, string $directory, array &$list_of_files): void
+    {
+        $idx = array_search($directory, array_column($list_of_files, 'name'));
+        if ($idx === false) {
+            $d = [];
+            $d['name'] = $directory;
+            $d['files'] = [$file];
+            $list_of_files[] = $d;
+        } else {
+            $list_of_files[$idx]['files'][] = $file;
+        }
     }
 
     public function viewFiles(): View|RedirectResponse
@@ -491,7 +718,7 @@ class BuildController extends AbstractBuildController
         if (!isset($_GET['buildid']) || !is_numeric($_GET['buildid'])) {
             abort(400, 'Invalid buildid!');
         }
-        $this->setBuildById((int) $_GET['buildid']);
+        $this->setBuildById((int)$_GET['buildid']);
 
         $response = begin_JSON_response();
 

app/Http/Controllers/AdminController.php
@@ -2,20 +2,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\PageTimer;
-use App\Services\TestingDay;
 use App\Validators\Password;
 use CDash\Config;
 use CDash\Database;
 use CDash\Model\BuildUpdate;
-use CDash\Model\Project;
 use CDash\Model\SubProject;
 use Exception;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
@@ -28,244 +23,6 @@ require_once 'include/upgrade_functions.php';
 
 class AdminController extends AbstractController
 {
-    public function viewUpdate(): View
-    {
-        return view('build.update');
-    }
-
-    public function viewUpdatePageContent(): JsonResponse
-    {
-        $pageTimer = new PageTimer();
-        $build = get_request_build();
-        if (is_null($build)) {
-            return response()->json([]);
-        }
-
-        $db = Database::getInstance();
-
-        $project = new Project();
-        $project->Id = $build->ProjectId;
-        $project->Fill();
-
-        $date = TestingDay::get($project, $build->StartTime);
-        $response = begin_JSON_response();
-        get_dashboard_JSON($project->Name, $date, $response);
-        $response['title'] = "$project->Name : Update";
-
-        // Menu
-        $menu_response = [];
-        $menu_response['back'] = "index.php?project=$project->Name&date=$date";
-
-        $previous_buildid = $build->GetPreviousBuildId();
-        $current_buildid = $build->GetCurrentBuildId();
-        $next_buildid = $build->GetNextBuildId();
-
-        if ($previous_buildid > 0) {
-            $menu_response['previous'] = "viewUpdate.php?buildid=$previous_buildid";
-        } else {
-            $menu_response['previous'] = false;
-        }
-
-        $menu_response['current'] = "viewUpdate.php?buildid=$current_buildid";
-
-        if ($next_buildid > 0) {
-            $menu_response['next'] = "viewUpdate.php?buildid=$next_buildid";
-        } else {
-            $menu_response['next'] = false;
-        }
-        $response['menu'] = $menu_response;
-
-        // Build
-        $site = $build->GetSite();
-
-        $build_response = [];
-        $build_response['site'] = $site->name;
-        $build_response['siteid'] = $site->id;
-        $build_response['buildname'] = $build->Name;
-        $build_response['buildid'] = $build->Id;
-        $build_response['buildtime'] = date('D, d M Y H:i:s T', strtotime($build->StartTime . ' UTC'));
-        $response['build'] = $build_response;
-
-        // Update
-        $update = new BuildUpdate();
-        $update->BuildId = $build->Id;
-        $update->FillFromBuildId();
-
-        $update_response = [];
-        if (strlen($update->Status) > 0 && $update->Status != '0') {
-            $update_response['status'] = $update->Status;
-        } else {
-            $update_response['status'] = ''; // empty status
-        }
-        $update_response['revision'] = $update->Revision;
-        $update_response['priorrevision'] = $update->PriorRevision;
-        $update_response['path'] = $update->Path;
-        $update_response['revisionurl'] =
-            get_revision_url($project->Id, $update->Revision, $update->PriorRevision);
-        $update_response['revisiondiff'] =
-            get_revision_url($project->Id, $update->PriorRevision, ''); // no prior prior revision...
-        $response['update'] = $update_response;
-
-        $directoryarray = [];
-        $updatearray1 = [];
-        // Create an array so we can sort it
-        foreach ($update->GetFiles() as $update_file) {
-            $file = [];
-            $file['filename'] = $update_file->Filename;
-            $file['author'] = $update_file->Author;
-            $file['status'] = $update_file->Status;
-
-            // Only display email if the user is logged in.
-            if (Auth::check()) {
-                if ($update_file->Email == '') {
-                    // Try to find author email from repository credentials.
-                    $stmt = $db->prepare("
-                SELECT email FROM user WHERE id IN (
-                  SELECT up.userid FROM user2project AS up, user2repository AS ur
-                   WHERE ur.userid=up.userid
-                   AND up.projectid=:projectid
-                   AND ur.credential=:author
-                   AND (ur.projectid=0 OR ur.projectid=:projectid) )
-                   LIMIT 1");
-                    $stmt->bindParam(':projectid', $project->Id);
-                    $stmt->bindParam(':author', $file['author']);
-                    $db->execute($stmt);
-                    $file['email'] = $stmt ? $stmt->fetchColumn() : '';
-                } else {
-                    $file['email'] = $update_file->Email;
-                }
-            } else {
-                $file['email'] = '';
-            }
-
-            $file['log'] = $update_file->Log;
-            $file['revision'] = $update_file->Revision;
-            $updatearray1[] = $file;
-            $directoryarray[] = substr($update_file->Filename, 0, strrpos($update_file->Filename, '/'));
-        }
-
-        $directoryarray = array_unique($directoryarray);
-
-        usort($directoryarray, function ($a, $b) {
-            return $a > $b ? 1 : 0;
-        });
-        usort($updatearray1, function ($a, $b) {
-            // Extract directory
-            $filenamea = $a['filename'];
-            $filenameb = $b['filename'];
-            return $filenamea > $filenameb ? 1 : 0;
-        });
-
-        $updatearray = [];
-
-        foreach ($directoryarray as $directory) {
-            foreach ($updatearray1 as $update) {
-                $filename = $update['filename'];
-                if (substr($filename, 0, strrpos($filename, '/')) == $directory) {
-                    $updatearray[] = $update;
-                }
-            }
-        }
-
-        // These variables represent a list of directories that contain a list of files.
-        $updated_files = [];
-        $modified_files = [];
-        $conflicting_files = [];
-
-        $num_updated_files = 0;
-        $num_modified_files = 0;
-        $num_conflicting_files = 0;
-
-        foreach ($updatearray as $file) {
-            $filename = $file['filename'];
-            $filename = str_replace('\\', '/', $filename);
-            $directory = substr($filename, 0, strrpos($filename, '/'));
-
-            $pos = strrpos($filename, '/');
-            if ($pos !== false) {
-                $filename = substr($filename, $pos + 1);
-            }
-
-            $baseurl = $project->BugTrackerFileUrl;
-            if (empty($baseurl)) {
-                $baseurl = $project->BugTrackerUrl;
-            }
-
-            $log = $file['log'];
-            $status = $file['status'];
-            $revision = $file['revision'];
-            $log = str_replace("\r", ' ', $log);
-            $log = str_replace("\n", ' ', $log);
-            // Do this twice so that <something> ends up as
-            // &amp;lt;something&amp;gt; because it gets sent to a
-            // javascript function not just displayed as html
-            $log = XMLStrFormat($log); // Apparently no need to do this twice anymore
-            $log = XMLStrFormat($log);
-
-            $log = trim($log);
-
-            $file['log'] = $log;
-            $file['filename'] = $filename;
-            $file['bugid'] = '0';
-            $file['bugpos'] = '0';
-            // This field is redundant because of the way our data is organized.
-            unset($file['status']);
-
-            if ($status == 'UPDATED') {
-                $diff_url = get_diff_url($project->Id, $project->CvsUrl, $directory, $filename, $revision);
-                $diff_url = XMLStrFormat($diff_url);
-                $file['diffurl'] = $diff_url;
-                $this->add_file($file, $directory, $updated_files);
-                $num_updated_files++;
-            } elseif ($status == 'MODIFIED') {
-                $diff_url = get_diff_url($project->Id, $project->CvsUrl, $directory, $filename);
-                $diff_url = XMLStrFormat($diff_url);
-                $file['diffurl'] = $diff_url;
-                $this->add_file($file, $directory, $modified_files);
-                $num_modified_files++;
-            } else {
-                //CONFLICTED
-                $diff_url = get_diff_url($project->Id, $project->CvsUrl, $directory, $filename);
-                $diff_url = XMLStrFormat($diff_url);
-                $file['diffurl'] = $diff_url;
-                $this->add_file($file, $directory, $conflicting_files);
-                $num_conflicting_files++;
-            }
-        }
-
-        $update_groups = [
-            [
-                'description' => "$project->Name Updated Files ($num_updated_files)",
-                'directories' => $updated_files
-            ],
-            [
-                'description' => "Modified Files ($num_modified_files)",
-                'directories' => $modified_files
-            ],
-            [
-                'description' => "Conflicting Files ($num_conflicting_files)",
-                'directories' => $conflicting_files
-            ]
-        ];
-        $response['updategroups'] = $update_groups;
-
-        $pageTimer->end($response);
-        return response()->json(cast_data_for_JSON($response));
-    }
-
-    private function add_file($file, string $directory, array &$list_of_files): void
-    {
-        $idx = array_search($directory, array_column($list_of_files, 'name'));
-        if ($idx === false) {
-            $d = [];
-            $d['name'] = $directory;
-            $d['files'] = [$file];
-            $list_of_files[] = $d;
-        } else {
-            $list_of_files[$idx]['files'][] = $file;
-        }
-    }
-
     public function import(): View
     {
         @set_time_limit(0);

app/cdash/app/Model/BuildUpdate.php
@@ -438,6 +438,6 @@ class BuildUpdate
     public function GetUrlForSelf(): string
     {
         $config = Config::getInstance();
-        return "{$config->getBaseUrl()}/viewUpdate.php?buildid={$this->BuildId}";
+        return "{$config->getBaseUrl()}/build/{$this->BuildId}/update";
     }
 }

app/cdash/include/sendemail.php
@@ -147,7 +147,7 @@ function get_email_summary(int $buildid, array $errors, $errorkey, int $maxitems
             throw new RuntimeException('Error querying update status for build ' . $buildid);
         }
 
-        $information .= "Status: {$update['status']} ({$serverURI}/viewUpdate.php?buildid={$buildid})\n";
+        $information .= "Status: {$update['status']} ({$serverURI}/build/{$buildid}/update)\n";
         $information .= 'Command: ';
         $information .= substr($update['command'], 0, $maxchars);
         $information .= "\n";
@@ -578,7 +578,7 @@ function send_update_email(UpdateHandler $handler, int $projectid): void
 
             $body = "$sitename has encountered errors during the Update step and you have been identified as the maintainer of this site.\n\n";
             $body .= "*Update Errors*\n";
-            $body .= 'Status: ' . $update['status'] . ' (' . $serverURI . '/viewUpdate.php?buildid=' . $buildid . ")\n";
+            $body .= 'Status: ' . $update['status'] . ' (' . $serverURI . '/build/' . $buildid . "/update)\n";
             if (cdashmail($recipients, $subject, $body)) {
                 add_log('email sent to: ' . implode(', ', $recipients), 'send_update_email');
             } else {

app/cdash/public/js/controllers/viewUpdate.js
@@ -1,69 +1,0 @@
-CDash.controller('ViewUpdateController',
-  ["$scope", "$rootScope", "$http", "apiLoader", function ViewUpdateController($scope, $rootScope, $http, apiLoader) {
-    $scope.graphLoaded = false;
-    $scope.graphLoading = false;
-    $scope.showGraph = false;
-    apiLoader.loadPageData($scope, 'api/v1/viewUpdate.php');
-
-    $scope.toggleGraph = function() {
-      $scope.showGraph = !$scope.showGraph;
-      if (!$scope.graphLoaded) {
-        $scope.loadGraph();
-      }
-    };
-
-    $scope.loadGraph = function() {
-      $scope.graphLoading = true;
-      $http({
-        url: 'api/v1/buildUpdateGraph.php',
-        method: 'GET',
-        params: {
-          buildid: $scope.cdash.build.buildid
-        }
-      }).then(function success(s) {
-        $scope.initializeGraph(s.data);
-        $scope.graphLoaded = true;
-        $scope.graphLoading = false;
-      });
-    };
-
-    $scope.initializeGraph = function(input) {
-      var options = {
-        lines: {show: true},
-        points: {show: true},
-        xaxis: {mode: "time"},
-        grid: {
-          backgroundColor: "#fffaff",
-          clickable: true,
-          hoverable: true,
-          hoverFill: '#444',
-          hoverRadius: 4
-        },
-        selection: {mode: "x"},
-        colors: ["#0000FF", "#dba255", "#919733"]
-      };
-
-      $("#graph_holder").bind("selected", function (event, area) {
-        plot = $.plot($("#graph_holder"), [{
-          label: "Number of changed files",
-          data: input.data
-        }], $.extend(true, {}, options, {xaxis: {min: area.x1, max: area.x2}}));
-      });
-
-      $("#graph_holder").bind("plotclick", function (e, pos, item) {
-        if (item) {
-          plot.highlight(item.series, item.datapoint);
-          buildid = input.buildids[item.datapoint[0]];
-          window.location = "build/" + buildid;
-        }
-      });
-
-      plot = $.plot($("#graph_holder"), [{label: "Number of changed files", data: input.data}], options);
-    };
-}])
-
-.directive('updatedFiles', ["VERSION", function(VERSION) {
-  return {
-    templateUrl: 'build/views/partials/updatedfiles_' + VERSION + '.html'
-  }
-}]);

app/cdash/public/views/partials/build.html
@@ -220,7 +220,7 @@
                 build.update.warning == 1 ? 'warning' : 'normal'))">
   <div ng-if="::build.hasupdate">
     <img ng-if="::build.userupdates > 0" src="img/yellowled.png" height="10px" alt="star" title="I checked in some code for this build!"/>
-    <a ng-href="viewUpdate.php?buildid={{::build.id}}">
+    <a ng-href="build/{{::build.id}}/update">
       {{::build.update.files}}
     </a>
   </div>

app/cdash/public/views/partials/updatedfiles.html
@@ -1,50 +1,0 @@
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-12">
-            <span class="glyphicon"
-                 ng-click="group.hidden = !group.hidden"
-                 ng-class="{'glyphicon-chevron-down': !group.hidden, 'glyphicon-chevron-right': group.hidden}"></span>
-            <b>{{::group.description}}</b>
-          </div>
-        </div>
-        <div ng-show="!group.hidden"
-             ng-repeat="directory in ::group.directories"
-             class="animate-show">
-          <div class="row">
-            <div class="col-md-12 col-md-offset-1">
-              <span class="glyphicon"
-                   ng-click="directory.hidden = !directory.hidden"
-                   ng-class="{'glyphicon-chevron-down': !directory.hidden, 'glyphicon-chevron-right': directory.hidden}"></span>
-              <tt>{{::directory.name}}</tt>
-            </div>
-          </div>
-          <div ng-show="!directory.hidden"
-               ng-repeat="file in ::directory.files"
-               class="animate-show">
-            <div class="row">
-              <div class="col-md-12 col-md-offset-2">
-                <span ng-if="::file.diffurl">
-                  <a ng-href="{{::file.diffurl}}">
-                    <tt>{{::file.filename}}</tt> Revision: <tt>{{::file.revision}}</tt>
-                  </a>
-                </span>
-                <span ng-if="::!file.diffurl">
-                  <tt>{{::file.filename}}</tt> Revision: <tt>{{::file.revision}}</tt>
-                </span>
-                <span ng-if="::file.author">
-                  by
-                  <a ng-if="::file.email" ng-href="mailto:{{::file.email}}">
-                    {{::file.author}}
-                  </a>
-                  <span ng-if="::!file.email">
-                    {{::file.author}}
-                  </span>
-                </span>
-              </div>
-            </div>
-            <div class="row spacer-bottom">
-              <pre class="col-md-10 col-md-offset-2" style="white-space: pre-wrap;">{{::file.log}}</pre>
-            </div>
-          </div>
-        </div>
-      </div>

app/cdash/tests/case/CDash/Model/BuildUpdateTest.php
@@ -26,7 +26,7 @@ class BuildUpdateTest extends TestCase
 
         $sut = new BuildUpdate();
         $sut->BuildId = 1001;
-        $expected = 'https://cdash.tld/viewUpdate.php?buildid=1001';
+        $expected = 'https://cdash.tld/build/1001/update';
         $actual = $sut->GetUrlForSelf();
         $this->assertEquals($expected, $actual);
 

commit 12d4cdc9ed7a4c47986cf320b981665e8659bef3

Fix broken navigation links on `Build Files Updated` page (#1749)
Fixes https://github.com/Kitware/CDash/issues/1748

This resolves a bug that was introduced in
https://github.com/Kitware/CDash/pull/1561, and also adds a missing
check for the `CURRENT` navigation button.

Co-authored-by: Shelly Belsky <sbelsky@salaxala.khq.kitware.com>

app/Http/Controllers/BuildController.php
@@ -548,15 +548,19 @@ final class BuildController extends AbstractBuildController
         $next_buildid = $this->build->GetNextBuildId();
 
         if ($previous_buildid > 0) {
-            $menu_response['previous'] = url("/build/$previous_buildid/update");
+            $menu_response['previous'] = "/build/$previous_buildid/update";
         } else {
             $menu_response['previous'] = false;
         }
 
-        $menu_response['current'] = url("/build/$current_buildid/update");
+        if ($current_buildid > 0) {
+            $menu_response['current'] = "/build/$current_buildid/update";
+        } else {
+            $menu_response['current'] = false;
+        }
 
         if ($next_buildid > 0) {
-            $menu_response['next'] = url("/build/$next_buildid/update");
+            $menu_response['next'] = "/build/$next_buildid/update";
         } else {
             $menu_response['next'] = false;
         }