0dda1f66a7b8fc4d7d0b72c115fa447e2c5713d2
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-10-31 22:11:59 -0400

    Refactor project permissions tests
    Originally from PR #1742

--- tests/Traits/CreatesProjects.php
+++ tests/Traits/CreatesProjects.php
@@ -2,16 +2,38 @@
 
 namespace Tests\Traits;
 
-use \CDash\Model\Project;
+use CDash\Model\Project;
+use Illuminate\Support\Str;
 
 trait CreatesProjects
 {
-    public function makePublicProject(): Project
+    public function makePublicProject(?string $name = null): Project
     {
         $project = new Project();
-        $project->Name = 'PublicProject';
+        $project->Name = $name ?? 'PublicProject_' . Str::uuid()->toString();
         $project->Public = Project::ACCESS_PUBLIC;
         $project->Save();
+        $project->InitialSetup();
+        return $project;
+    }
+
+    public function makeProtectedProject(?string $name = null): Project
+    {
+        $project = new Project();
+        $project->Name = $name ?? 'ProtectedProject_' . Str::uuid()->toString();
+        $project->Public = Project::ACCESS_PROTECTED;
+        $project->Save();
+        $project->InitialSetup();
+        return $project;
+    }
+
+    public function makePrivateProject(?string $name = null): Project
+    {
+        $project = new Project();
+        $project->Name = $name ?? 'PrivateProject_' . Str::uuid()->toString();
+        $project->Public = Project::ACCESS_PRIVATE;
+        $project->Save();
+        $project->InitialSetup();
         return $project;
     }
 }
