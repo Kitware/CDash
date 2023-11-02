911644620334383f5d8e99b47a647d91e88c1020
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-11-02 08:41:37 -0400

    Delete invalid rows prior to creating FK constraint (#1768)
    The CDash 3.3 release will include foreign-key constraints for the first
    time. CDash does a poor job of cleaning up after itself when deleting
    rows, so the new constraints will fail during the migration for many
    existing CDash systems. The only way to add these constraints is delete
    any data which violates them. This should not be problematic, because
    there should be no way to access invalid data in the first place.
    
    I also simplified the existing migrations due to an egregious violation
    of the DRY principle...

--- database/migrations/2023_08_14_205552_build_foreign_keys.php
+++ database/migrations/2023_08_14_205552_build_foreign_keys.php
@@ -2,9 +2,43 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+    private const tables_to_modify = [
+        'build2configure',
+        'build2group',
+        'build2note',
+        'build2test',
+        'build2update',
+        'build2uploadfile',
+        'buildemail',
+        'builderror',
+        'builderrordiff',
+        'buildfailure',
+        'buildfile',
+        'buildinformation',
+        'buildnote',
+        'buildproperties',
+        'buildtesttime',
+        'configureerrordiff',
+        'coverage',
+        'coveragefilelog',
+        'coveragesummary',
+        'coveragesummarydiff',
+        'dynamicanalysis',
+        'dynamicanalysissummary',
+        'label2build',
+        'label2coveragefile',
+        'label2test',
+        'pending_submissions',
+        'related_builds',
+        'subproject2build',
+        'summaryemail',
+        'testdiff',
+    ];
+
     /**
      * Run the migrations.
      *
@@ -12,186 +46,15 @@ return new class extends Migration {
      */
     public function up()
     {
-        echo 'Adding buildid foreign key to build2configure table...' . PHP_EOL;
-        Schema::table('build2configure', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-
-        echo 'Adding buildid foreign key to build2group table...' . PHP_EOL;
-        Schema::table('build2group', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to build2note table...' . PHP_EOL;
-        Schema::table('build2note', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to build2test table...' . PHP_EOL;
-        Schema::table('build2test', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to build2update table...' . PHP_EOL;
-        Schema::table('build2update', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to build2uploadfile table...' . PHP_EOL;
-        Schema::table('build2uploadfile', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildemail table...' . PHP_EOL;
-        Schema::table('buildemail', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to builderror table...' . PHP_EOL;
-        Schema::table('builderror', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to builderrordiff table...' . PHP_EOL;
-        Schema::table('builderrordiff', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildfailure table...' . PHP_EOL;
-        Schema::table('buildfailure', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildfile table...' . PHP_EOL;
-        Schema::table('buildfile', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildinformation table...' . PHP_EOL;
-        Schema::table('buildinformation', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildnote table...' . PHP_EOL;
-        Schema::table('buildnote', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildproperties table...' . PHP_EOL;
-        Schema::table('buildproperties', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to buildtesttime table...' . PHP_EOL;
-        Schema::table('buildtesttime', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to configureerrordiff table...' . PHP_EOL;
-        Schema::table('configureerrordiff', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to coverage table...' . PHP_EOL;
-        Schema::table('coverage', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to coveragefilelog table...' . PHP_EOL;
-        Schema::table('coveragefilelog', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to coveragesummary table...' . PHP_EOL;
-        Schema::table('coveragesummary', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to coveragesummarydiff table...' . PHP_EOL;
-        Schema::table('coveragesummarydiff', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to dynamicanalysis table...' . PHP_EOL;
-        Schema::table('dynamicanalysis', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to dynamicanalysissummary table...' . PHP_EOL;
-        Schema::table('dynamicanalysissummary', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to label2build table...' . PHP_EOL;
-        Schema::table('label2build', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to label2coveragefile table...' . PHP_EOL;
-        Schema::table('label2coveragefile', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to label2test table...' . PHP_EOL;
-        Schema::table('label2test', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to pending_submissions table...' . PHP_EOL;
-        Schema::table('pending_submissions', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to related_builds table...' . PHP_EOL;
-        Schema::table('related_builds', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to subproject2build table...' . PHP_EOL;
-        Schema::table('subproject2build', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to summaryemail table...' . PHP_EOL;
-        Schema::table('summaryemail', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
-
-        echo 'Adding buildid foreign key to testdiff table...' . PHP_EOL;
-        Schema::table('testdiff', function (Blueprint $table) {
-            $table->integer('buildid')->nullable(false)->change();
-            $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
-        });
+        foreach (self::tables_to_modify as $table) {
+            echo "Adding buildid foreign key to $table table...";
+            $num_deleted = DB::delete("DELETE FROM $table WHERE buildid NOT IN (SELECT id FROM build)");
+            echo $num_deleted . ' invalid rows deleted' . PHP_EOL;
+            Schema::table($table, function (Blueprint $table) {
+                $table->integer('buildid')->nullable(false)->change();
+                $table->foreign('buildid')->references('id')->on('build')->cascadeOnDelete();
+            });
+        }
     }
 
     /**
@@ -201,154 +64,11 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::table('build2configure', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('build2group', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('build2note', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('build2test', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('build2update', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('build2uploadfile', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildemail', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('builderror', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('builderrordiff', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildfailure', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildfile', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildinformation', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildnote', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildproperties', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('buildtesttime', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('configureerrordiff', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('coverage', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('coveragefilelog', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('coveragesummary', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('coveragesummarydiff', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('dynamicanalysis', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('dynamicanalysissummary', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('label2build', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('label2coveragefile', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('label2test', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('pending_submissions', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('related_builds', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('subproject2build', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('summaryemail', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
-
-        Schema::table('testdiff', function (Blueprint $table) {
-            $table->integer('buildid')->change();
-            $table->dropForeign(['buildid']);
-        });
+        foreach (self::tables_to_modify as $table) {
+            Schema::table($table, function (Blueprint $table) {
+                $table->integer('buildid')->change();
+                $table->dropForeign(['buildid']);
+            });
+        }
     }
 };
