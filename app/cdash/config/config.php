b73c4f8f68007b68829ee439fb973b07103fb596
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-11-14 12:42:10 -0500

    Delete unused `CDASH_SUBMISSION_PROCESSING_TIME_LIMIT` setting

--- app/cdash/config/config.php
+++ app/cdash/config/config.php
@@ -52,8 +52,6 @@ $CDASH_GIT_COMMAND = 'git';
 $CDASH_DEFAULT_GIT_DIRECTORY = 'git';
 // Define the p4 command
 $CDASH_P4_COMMAND = 'p4';
-// Number of seconds to allow processing a single submission before resetting
-$CDASH_SUBMISSION_PROCESSING_TIME_LIMIT = '450';
 // Number of times to attempt processing a single submission before giving up
 $CDASH_SUBMISSION_PROCESSING_MAX_ATTEMPTS = '5';
 
