9b532b7f0f45fd9b35d4c44365ffd59c8a387dbe
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-11-20 11:28:55 -0500

    Specify format string for graphs with timestamps

--- resources/js/components/BuildSummary.vue
+++ resources/js/components/BuildSummary.vue
@@ -1160,7 +1160,11 @@ export default {
       var options = {
         lines: {show: true},
         points: {show: true},
-        xaxis: {mode: "time"},
+        xaxis: {
+          mode: "time",
+          timeformat: "%Y/%m/%d %H:%M",
+          timeBase: "milliseconds",
+        },
         grid: {
           backgroundColor: "#fffaff",
           clickable: true,
