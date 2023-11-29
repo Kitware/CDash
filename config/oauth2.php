155334588124504f33e6e7a2628a475df3d7cf51
Joseph Snyder <joe.snyder@kitware.com>
2023-11-29 13:19:50 -0500

    Convert GitLab login to Socialite (#1828)
    Plus some fixes to the OAuth flow:
    
     Ensure that after a user is created, they are logged in.
     Add lines for both plugins to EventServiceProvider as described in
       Socialite docs

--- config/oauth2.php
+++ config/oauth2.php
@@ -1,16 +1,8 @@
 <?php
 
-use CDash\Middleware\OAuth2\GitLab;
 use CDash\Middleware\OAuth2\Google;
 
 return [
-    'gitlab' => [
-        'clientId' => env('GITLAB_CLIENT_ID'),
-        'clientSecret' => env('GITLAB_CLIENT_SECRET'),
-        'domain' => env('GITLAB_DOMAIN', 'https://gitlab.com'),
-        'className' => GitLab::class,
-        'enable' => env('GITLAB_ENABLE', false),
-    ],
     'google' => [
         'clientId' => env('GOOGLE_CLIENT_ID'),
         'clientSecret' => env('GOOGLE_CLIENT_SECRET'),
--- config/services.php
+++ config/services.php
@@ -22,6 +22,16 @@ return [
         'oauth' => true,
         'display_name' => "GitHub",
     ],
+    'gitlab' => [
+        'client_id' => env('GITLAB_CLIENT_ID'),
+        'client_secret' => env('GITLAB_CLIENT_SECRET'),
+        'redirect' => env('GITLAB_REDIRECT_URI'),
+        'instance_uri' => env('GITLAB_DOMAIN'),
+        'enable' => env('GITLAB_ENABLE', false),
+        'autoregister' => env('GITLAB_AUTO_REGISTER_NEW_USERS', false),
+        'oauth' => true,
+        'display_name' => "GitLab",
+    ],
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
