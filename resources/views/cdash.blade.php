6d4569e968add02a46292cba059dd6846b673c18
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-11-29 09:34:10 -0500

    Delete unused AngularJS files (#1822)
    I believe the files deleted in this PR are no longer used.

--- resources/views/cdash.blade.php
+++ resources/views/cdash.blade.php
@@ -38,7 +38,6 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/cdash.css') }}"/>
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
         <script src="{{ asset("js/CDash_${js_version}.min.js") }}"></script>
-        <script src="{{ asset('js/tooltip.js') }}" type="text/javascript" charset="utf-8"></script>
         <script src="{{ asset('js/jquery.tablesorter.js') }}" type="text/javascript" charset="utf-8"></script>
         <script src="{{ asset('js/jquery.dataTables.min.js') }}" type="text/javascript" charset="utf-8"></script>
         <script src="{{ asset('js/jquery.metadata.js') }}" type="text/javascript" charset="utf-8"></script>
