a4281732e57847b20c777a97181818a8eb97b536
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-12-13 16:34:43 -0500

    Fix Vue helper files

--- resources/js/components/shared/ApiLoader.js
+++ resources/js/components/shared/ApiLoader.js
@@ -1,4 +1,4 @@
-import emitter from 'tiny-emitter/instance'
+import emitter from 'tiny-emitter/instance';
 
 export default {
   // Replacement emitters for the Vue 2 -> 3 conversion.  TODO: Remove these eventually.
@@ -12,7 +12,7 @@ export default {
       .get(endpoint_path)
       .then(response => {
         // Pre-assigment hook for components.
-        if (typeof vm.preSetup === "function") {
+        if (typeof vm.preSetup === 'function') {
           vm.preSetup(response);
         }
 
@@ -20,39 +20,39 @@ export default {
         vm.cdash.endpoint = vm.$baseURL + endpoint_path;
 
         // Post-assigment hook for components.
-        if (typeof vm.postSetup === "function") {
+        if (typeof vm.postSetup === 'function') {
           vm.postSetup(response);
         }
 
         // Add vuejs render time to page load time in footer.
-        var renderTime = +((new Date().getTime() - vm.start) / 1000);
+        const renderTime = +((new Date().getTime() - vm.start) / 1000);
         if (vm.cdash.generationtime === undefined) {
           vm.cdash.generationtime = 0;
         }
-        var generationTimeStr = (renderTime + vm.cdash.generationtime).toFixed(2);
+        let generationTimeStr = (renderTime + vm.cdash.generationtime).toFixed(2);
         generationTimeStr += `s (${vm.cdash.generationtime}s)`;
         vm.cdash.generationtime = generationTimeStr;
 
 
         // A brutal hack to populate the footer with content
         // TODO: (williamjallen) Clean this up
-        if (document.getElementById("api-endpoint")) {
-          document.getElementById("api-endpoint")?.setAttribute('href', vm.cdash.endpoint);
+        if (document.getElementById('api-endpoint')) {
+          document.getElementById('api-endpoint')?.setAttribute('href', vm.cdash.endpoint);
         }
-        if (document.getElementById("generation-time")) {
-          document.getElementById("generation-time").textContent = vm.cdash.generationtime;
+        if (document.getElementById('generation-time')) {
+          document.getElementById('generation-time').textContent = vm.cdash.generationtime;
         }
-        if (document.getElementById("testing-day") && vm.cdash.nightlytime !== undefined) {
-          document.getElementById("testing-day").textContent = `Current Testing Day ${vm.cdash.currentdate} | Started at ${vm.cdash.nightlytime}`;
+        if (document.getElementById('testing-day') && vm.cdash.nightlytime !== undefined) {
+          document.getElementById('testing-day').textContent = `Current Testing Day ${vm.cdash.currentdate} | Started at ${vm.cdash.nightlytime}`;
         }
 
         // Let other components know that data has been loaded from the API.
         this.$emit('api-loaded', vm.cdash);
       })
       .catch(error => {
-        console.log(error)
-        vm.errored = true
+        console.log(error);
+        vm.errored = true;
       })
-      .finally(() => vm.loading = false)
+      .finally(() => vm.loading = false);
   },
-}
+};
--- resources/js/components/shared/TextMutator.js
+++ resources/js/components/shared/TextMutator.js
@@ -1,14 +1,14 @@
 export default {
-	ctestNonXmlCharEscape: function (input) {
-		var pattern = /\[NON-XML-CHAR-0x1B\]/g;
-		return input.replace(pattern, '\x1B');
-	},
+  ctestNonXmlCharEscape: function (input) {
+    const pattern = /\[NON-XML-CHAR-0x1B\]/g;
+    return input.replace(pattern, '\x1B');
+  },
 
-	terminalColors: function (input, htmlEscape) {
-		var ansiUp = new AnsiUp;
-		if (htmlEscape !== undefined) {
-			ansiUp.escape_for_html = htmlEscape;
-		}
-		return ansiUp.ansi_to_html(input);
-	},
-}
+  terminalColors: function (input, htmlEscape) {
+    const ansiUp = new AnsiUp;
+    if (htmlEscape !== undefined) {
+      ansiUp.escape_for_html = htmlEscape;
+    }
+    return ansiUp.ansi_to_html(input);
+  },
+};
