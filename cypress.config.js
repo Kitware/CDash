5871f3dda005f2b2c8a07ecb0fe2dfdf78d8823e
William Allen <16820599+williamjallen@users.noreply.github.com>
2023-12-13 10:52:53 -0500

    Set up Cypress e2e testing in CI (#1677)
    [Cypress](https://www.cypress.io) can now be used for e2e testing. We
    should begin converting our Selenium and Protractor tests to Cypress
    since Selenium is becoming outdated, and Protractor has been EOL for
    some time.
    
    This PR only includes a single example "spec" file. Future PRs will add
    real test cases and eliminate the example spec file.
    
    I also performed a light refactor of our CTest test structure. It would
    be a good idea to take a closer look at our CTest setup in the future.

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  fixturesFolder: 'tests/cypress/fixtures',
+  screenshotsFolder: 'tests/cypress/screenshots',
+  videosFolder: 'tests/cypress/videos',
+  downloadsFolder: 'tests/cypress/downloads',
+  pageLoadTimeout: 300000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/cypress/support/e2e.js',
+    experimentalStudio: true,
+  },
+});
