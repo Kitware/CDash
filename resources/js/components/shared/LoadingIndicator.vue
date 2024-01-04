235275f35b67275a17c0006bc6c88190dcd06649
William Allen <16820599+williamjallen@users.noreply.github.com>
2024-01-04 11:50:19 -0500

    Add loading indicator Vue template (#1906)
    We currently provide no information about the state of the page for
    pages with API endpoints which take a long time to return. This PR adds
    a Vue template which accepts a slot for content to be rendered when the
    page is loaded, and a boolean prop which indicates whether the loading
    indicator should be shown. I have initially used this template to add
    loading indicators to two pages as an example for future use. I plan to
    switch the loading indicator icon to a Font Awesome icon instead of a
    grainy GIF once #1895 is merged.

---
+++ resources/js/components/shared/LoadingIndicator.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div v-if="isLoading">
+      <img
+        v-if="initialDelayComplete"
+        :src="$baseURL + '/img/loading.gif'"
+        class="loading-indicator"
+        alt="The page is loading."
+      >
+    </div>
+    <slot v-else />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  data() {
+    return {
+      initialDelayComplete: false,
+    };
+  },
+
+  mounted() {
+    // Prevent a flicker if the page loads quickly
+    setTimeout(() => {
+      this.initialDelayComplete = true;
+    }, 500);
+  },
+};
+</script>
+
+<style scoped>
+.loading-indicator {
+  margin: 0 auto;
+  display: block;
+}
+</style>
