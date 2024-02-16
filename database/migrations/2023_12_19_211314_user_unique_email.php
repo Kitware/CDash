ee83d308d1861bfa3851b8f0ebf51cac60dc820b
Zack Galbreath <zack.galbreath@kitware.com>
2024-02-16 15:53:48 -0500

    Delete duplicate emails in unique email migration

--- database/migrations/2023_12_19_211314_user_unique_email.php
+++ database/migrations/2023_12_19_211314_user_unique_email.php
@@ -4,12 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+require_once 'include/common.php';
+
 return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        // Delete any duplicate email addresses, keeping the oldest userid for each one.
+        $user_table = qid('user');
+        $num_deleted = DB::delete(
+            "DELETE FROM $user_table WHERE id NOT IN (SELECT * FROM (SELECT MIN(id) FROM $user_table GROUP BY email) AS A)");
+        echo $num_deleted . ' duplicate accounts deleted' . PHP_EOL;
+
+        // Add the unique constraint.
         Schema::table('user', function (Blueprint $table) {
             $table->unique('email');
         });
@@ -21,7 +30,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('user', function (Blueprint $table) {
-            $table->dropUnique('email');
+            $table->dropUnique(['email']);
         });
     }
 };
