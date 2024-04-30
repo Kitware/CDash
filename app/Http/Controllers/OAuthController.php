3f13c757cd94d416daac5bf1f3508a2ff2812c5b
Joseph Snyder <joe.snyder@kitware.com>
2024-04-30 16:32:52 -0400

    Add try/catch block to account for permission denials
    Add a try/catch block to the OAuth controller which will catch an
    exception thrown when trying to get a user from an auth call that was
    rejected.

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -27,7 +27,13 @@ final class OAuthController extends AbstractController
 
     public function callback(string $service): RedirectResponse|Redirector
     {
-        $authUser = Socialite::driver($service)->user();
+        // Try/Catch to prevent 500  error when access is denied at provider
+        try {
+            $authUser = Socialite::driver($service)->user();
+        } catch (\Exception $e) {
+            return redirect("/login");
+        };
+
         $email =  $authUser->getEmail();
         [$fname, $lname] = explode(" ", $authUser->getName() ?? '');
 
