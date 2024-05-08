e3f7d2944215c4e7a2b828943d7c604e46ef8747
William Allen <16820599+williamjallen@users.noreply.github.com>
2024-05-08 08:55:38 -0400

    Add DaisyUI component library (#2114)
    We currently hope to include some UI enhancements in CDash 3.5, but our
    current styling is messy and quite difficult to work with.
    [DaisyUI](https://daisyui.com) is built on
    [TailwindCSS](https://tailwindcss.com), which is currently the most
    modern web UI library in widespread use. The large selection of reusable
    components DaisyUI provides should allow us to build more complicated UI
    features, and reduce our own handwritten CSS substantially.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
+
