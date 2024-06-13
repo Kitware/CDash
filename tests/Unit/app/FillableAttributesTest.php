30cf66aca62b89b34bc0004466f6b5bf558a4001
William Allen <16820599+williamjallen@users.noreply.github.com>
2024-06-13 11:16:27 -0400

    Fail when unfillable Eloquent attributes provided (#2261)
    Eloquent discards attributes which are not explicitly listed as
    "fillable" by default. In most cases, discarded attributes are probably
    discarded due to inadvertent errors, such as the test typo fixed in this
    PR. This PR enables one of Eloquent's "strict" modes in the development
    environment, in the hope that doing so will prevent some types of
    Eloquent errors. In the future, it may be desirable to enable this
    feature in production as well, but I haven't done so yet due to the
    possibility of errors being exposed in untested code.

---
+++ tests/Unit/app/FillableAttributesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\app;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+/**
+ * TODO: Figure out how to switch the APP_ENV to "production" in the tests.
+ */
+class FillableAttributesTest extends TestCase
+{
+    public function testCreatesModelWhenValidFillableAttributesProvided(): void
+    {
+        self::expectNotToPerformAssertions();
+        new User([
+            'firstname' => Str::uuid()->toString(),
+            'lastname' => Str::uuid()->toString(),
+            'email' => Str::uuid()->toString() . Str::uuid()->toString(),
+        ]);
+    }
+
+    public function testFailsToCreateModelWhenInvalidFillableAttribute(): void
+    {
+        self::expectException(\Illuminate\Database\Eloquent\MassAssignmentException::class);
+        new User([
+            'firstname' => Str::uuid()->toString(),
+            'lastname' => Str::uuid()->toString(),
+            'email' => Str::uuid()->toString() . Str::uuid()->toString(),
+            'admin' => true,
+        ]);
+    }
+}
