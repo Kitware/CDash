55b031333625ab47b2597258287f3a2f4b21a11e
William Allen <16820599+williamjallen@users.noreply.github.com>
2024-06-26 14:09:35 -0400

    Remove install page (#2279)
    The functionality of `/install` has slowly diminished in favor of
    individual Artisan commands. The page is now redundant. This PR removes
    `/install` entirely, and cleans up a bunch of associated test DB logic.

--- app/cdash/tests/test_install.php
+++ app/cdash/tests/test_install.php
@@ -1,16 +1,50 @@
 <?php
-require_once dirname(__FILE__) . '/cdash_test_case.php';
-require_once 'tests/install_test.php';
 
-class InstallTestCase extends BaseInstallTestCase
+use Illuminate\Support\Facades\Artisan;
+
+require_once dirname(__FILE__) . '/cdash_test_case.php';
+
+class InstallTestCase extends KWWebTestCase
 {
+    protected string $databaseName;
+
     public function __construct()
     {
         parent::__construct();
+
+        $db_type = config('database.default');
+        $db_config = config("database.connections.{$db_type}");
+        $this->databaseName = $db_config['database'];
     }
 
     public function testInstall()
     {
-        $this->install();
+        //double check that it's the testing database before doing anything hasty...
+        if ($this->databaseName !== 'cdash4simpletest') {
+            $this->fail("can only test on a database named 'cdash4simpletest'");
+            return 1;
+        }
+
+        //drop any old testing database before testing install
+        $success = $this->db->drop($this->databaseName);
+        if (!$success) {
+            $this->fail('Error dropping database');
+            return 1;
+        }
+
+        $this->db->create($this->databaseName);
+
+        Artisan::call('migrate', ['--force' => true]);
+
+        Artisan::call('user:save', [
+            '--email' => 'simpletest@localhost',
+            '--firstname' => 'administrator',
+            '--lastname' => '',
+            '--password' => 'simpletest',
+            '--institution' => 'Kitware Inc.',
+            '--admin' => 1,
+        ]);
+
+        $this->pass('Passed');
     }
 }
