991f6d13d8364aecbdbbc62e801a31f5dfc72712
William Allen <16820599+williamjallen@users.noreply.github.com>
2024-09-17 19:15:29 -0400

    Bump version to 3.5.1 (#2444)

--- config/cdash.php
+++ config/cdash.php
@@ -22,7 +22,7 @@ return [
         'expires' => env('PASSWORD_EXPIRATION', 0),
         'unique' => env('UNIQUE_PASSWORD_COUNT', 0),
     ],
-    'version' => '3.5.0',
+    'version' => '3.5.1',
     'registration' => [
         'email' => [
             'verify' => env('REGISTRATION_EMAIL_VERIFY', true),
