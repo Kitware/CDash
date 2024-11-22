3ca4b7c3552d7ad9d09895bece4504e4b0561387
William Allen <16820599+williamjallen@users.noreply.github.com>
2024-11-22 10:04:57 -0500

    Expose site maintainers via the GraphQL API

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Carbon;
@@ -61,6 +63,14 @@ class Site extends Model
             ->ofMany(['timestamp' => 'max'], function (Builder $query) use ($date) {
                 $query->where('timestamp', '<=', $date ?? Carbon::maxValue());
             });
+    }
+
+    /**
+     * @return BelongsToMany<User>
+     */
+    public function maintainers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'site2user', 'siteid', 'userid');
     }
 
     public function save(array $options = []): bool
