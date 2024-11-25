Kitware/CDash

commit 6f21352945a2c25ea5ab5fac50ffb66140d5ff8e

Overhaul project sites page (#2583)
The "site map" page is currently very slow on large production
instances, and is also partially broken. This PR completely overhauls
the page, using Vue and GraphQL behind the scenes to render content
faster. I plan to make a series of UI improvements to this page in the
near future via separate PRs.


![image](https://github.com/user-attachments/assets/1560f9d8-2c26-41f6-a520-2639b41d8a7a)

resources/js/vue/components/ProjectSitesPage.vue
@@ -1,0 +1,167 @@
+<template>
+  <div>
+    <loading-indicator :is-loading="!allSites">
+      <data-table
+        :column-groups="[
+          {
+            displayName: 'All Sites',
+            width: 100,
+          }
+        ]"
+        :columns="[
+          {
+            name: 'name',
+            displayName: 'Name',
+          },
+          {
+            name: 'processors',
+            displayName: 'Processors',
+          },
+          {
+            name: 'memory',
+            displayName: 'Memory',
+          },
+          {
+            name: 'maintainers',
+            displayName: 'Maintainers',
+          },
+          {
+            name: 'description',
+            displayName: 'Description',
+            expand: true,
+          },
+        ]"
+        :rows="formattedSiteRows"
+        :full-width="true"
+      />
+    </loading-indicator>
+  </div>
+</template>
+
+<script>
+
+import DataTable from './shared/DataTable.vue';
+import gql from 'graphql-tag';
+import FilterBuilder from './shared/FilterBuilder.vue';
+import LoadingIndicator from './shared/LoadingIndicator.vue';
+
+export default {
+  components: {
+    LoadingIndicator,
+    DataTable,
+  },
+
+  props: {
+    projectId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  apollo: {
+    allSites: {
+      query: gql`
+        query($projectid: ID, $after: String) {
+          allSites: project(id: $projectid) {
+            sites(after: $after, first: 100) {
+              edges {
+                node {
+                  id
+                  name
+                  mostRecentInformation {
+                    description
+                    numberPhysicalCpus
+                    totalPhysicalMemory
+                  }
+                  maintainers {
+                    edges {
+                      node {
+                        id
+                        firstname
+                        lastname
+                      }
+                    }
+                  }
+                }
+              }
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+              }
+            }
+          }
+        }
+      `,
+      variables() {
+        return {
+          projectid: this.projectId,
+        };
+      },
+      result({data}) {
+        if (data && data.allSites.sites.pageInfo.hasNextPage) {
+          this.$apollo.queries.allSites.fetchMore({
+            variables: {
+              after: data.allSites.sites.pageInfo.endCursor,
+            },
+          });
+        }
+      },
+    },
+  },
+
+  computed: {
+    formattedSiteRows() {
+      return this.allSites.sites.edges?.map(edge => {
+        return {
+          name: {
+            value: edge.node.name,
+            text: edge.node.name,
+            href: `${this.$baseURL}/sites/${edge.node.id}`,
+          },
+          processors: {
+            value: edge.node.mostRecentInformation.numberPhysicalCpus,
+            text: edge.node.mostRecentInformation.numberPhysicalCpus,
+          },
+          memory: {
+            value: edge.node.mostRecentInformation.totalPhysicalMemory,
+            text: this.humanReadableMemory(edge.node.mostRecentInformation.totalPhysicalMemory),
+          },
+          maintainers: edge.node.maintainers.edges.map(maintainer => {
+            let maintainerName = '';
+            if (maintainer.node.firstname) {
+              maintainerName += maintainer.node.firstname;
+            }
+            if (maintainer.node.firstname && maintainer.node.lastname) {
+              maintainerName += ' ';
+            }
+            if (maintainer.node.lastname) {
+              maintainerName += maintainer.node.lastname;
+            }
+            return maintainerName;
+          }).join(', '),
+          description: {
+            value: edge.node.mostRecentInformation.description,
+            text: edge.node.description,
+          },
+        };
+      });
+    },
+  },
+
+  methods: {
+    humanReadableMemory(inputInMiB) {
+      if (inputInMiB < 1024) {
+        return `${inputInMiB} MiB`;
+      }
+      else if (inputInMiB < 1024 * 1024) {
+        return `${(inputInMiB / 1024).toFixed(2)} GiB`;
+      }
+      else {
+        return `${(inputInMiB / (1024 * 1024)).toFixed(2)} TiB`;
+      }
+    },
+  },
+};
+</script>

app/Http/Controllers/MapController.php
@@ -1,160 +1,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use CDash\Database;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\View\View;
-
-final class MapController extends AbstractProjectController
-{
-    public function viewMap(): View|RedirectResponse
-    {
-        $this->setProjectByName(htmlspecialchars($_GET['project'] ?? ''));
-
-        @$date = $_GET['date'];
-        if ($date != null) {
-            $date = htmlspecialchars($date);
-        }
-
-        $xml = begin_XML_for_XSLT();
-        $xml .= '<backurl>index.php?project=' . urlencode($this->project->Name) . "&#38;date=$date</backurl>";
-        $xml .= '<menutitle>CDash</menutitle>';
-        $xml .= '<menusubtitle>Build location</menusubtitle>';
-
-        $xml .= '<dashboard>';
-        $xml .= '<title>CDash</title>';
-        $xml .= '<date>' . $date . '</date>';
-
-        $apikey = config('cdash.google_map_api_key');
-
-        $xml .= add_XML_value('googlemapkey', $apikey);
-        $xml .= add_XML_value('projectname', $this->project->Name);
-        $xml .= add_XML_value('projectname_encoded', urlencode($this->project->Name));
-        $xml .= '</dashboard>';
-
-        $db = Database::getInstance();
-
-        [$previousdate, $currenttime, $nextdate] = get_dates($date, $this->project->NightlyTime);
-
-        $nightlytime = strtotime($this->project->NightlyTime);
-
-        $nightlyhour = gmdate('H', $nightlytime);
-        $nightlyminute = gmdate('i', $nightlytime);
-        $nightlysecond = gmdate('s', $nightlytime);
-
-        $end_timestamp = $currenttime - 1; // minus 1 second when the nightly start time is midnight exactly
-
-        $beginning_timestamp = gmmktime($nightlyhour, $nightlyminute, $nightlysecond, gmdate('m', $end_timestamp), gmdate('d', $end_timestamp), gmdate('Y', $end_timestamp));
-        if ($end_timestamp < $beginning_timestamp) {
-            $beginning_timestamp = gmmktime($nightlyhour, $nightlyminute, $nightlysecond, gmdate('m', $end_timestamp - 24 * 3600), gmdate('d', $end_timestamp - 24 * 3600), gmdate('Y', $end_timestamp - 24 * 3600));
-        }
-
-        $beginning_UTCDate = gmdate(FMT_DATETIME, $beginning_timestamp);
-        $end_UTCDate = gmdate(FMT_DATETIME, $end_timestamp);
-
-        if (config('database.default') == 'pgsql') {
-            $site = $db->executePrepared('
-                        SELECT
-                            s.id,
-                            s.name,
-                            si.processorclockfrequency,
-                            si.description,
-                            si.numberphysicalcpus,
-                            s.ip,
-                            s.latitude,
-                            s.longitude,
-                            u.firstname,
-                            u.lastname,
-                            u.id AS userid
-                        FROM
-                            build AS b,
-                            siteinformation AS si,
-                            site as s
-                        LEFT JOIN site2user ON (site2user.siteid=s.id)
-                        LEFT JOIN ' . qid('user') . ' AS u ON (site2user.userid=u.id)
-                        WHERE
-                            s.id=b.siteid
-                            AND b.starttime<?
-                            AND b.starttime>?
-                            AND si.siteid=s.id
-                            AND b.projectid=?
-                        GROUP BY
-                            s.id,
-                            s.name,
-                            si.processorclockfrequency,
-                            si.description,
-                            si.numberphysicalcpus,
-                            s.ip,
-                            s.latitude,
-                            s.longitude,
-                            u.firstname,
-                            u.lastname,
-                            u.id
-                    ', [$end_UTCDate, $beginning_UTCDate, $this->project->Id]);
-        } else {
-            $site = $db->executePrepared('
-                        SELECT
-                            s.id,
-                            s.name,
-                            si.processorclockfrequency,
-                            si.description,
-                            si.numberphysicalcpus,
-                            s.ip,
-                            s.latitude,
-                            s.longitude,
-                            u.firstname,
-                            u.lastname,
-                            u.id AS userid
-                        FROM
-                            build AS b,
-                            siteinformation AS si,
-                            site as s
-                        LEFT JOIN site2user ON (site2user.siteid=s.id)
-                        LEFT JOIN ' . qid('user') . ' AS u ON (site2user.userid=u.id)
-                        WHERE
-                            s.id=b.siteid
-                            AND b.starttime<?
-                            AND b.starttime>?
-                            AND si.siteid=s.id
-                            AND b.projectid=?
-                        GROUP BY s.id
-                    ', [$end_UTCDate, $beginning_UTCDate, $this->project->Id]);
-        }
-
-        echo pdo_error();
-
-        foreach ($site as $site_array) {
-            if ($site_array['processorclockfrequency'] > 0) {
-                $processor_speed = getByteValueWithExtension($site_array['processorclockfrequency'] * 1024 * 1024) . 'Hz';
-            } else {
-                $processor_speed = '';
-            }
-
-            if ($site_array['numberphysicalcpus'] > 0) {
-                $num_physical_cpus = $site_array['numberphysicalcpus'];
-            } else {
-                $num_physical_cpus = '';
-            }
-
-            $xml .= '<site>';
-            $xml .= add_XML_value('name', $site_array['name']);
-            $xml .= add_XML_value('id', $site_array['id']);
-            $xml .= add_XML_value('description', $site_array['description']);
-            $xml .= add_XML_value('processor_speed', $processor_speed);
-            $xml .= add_XML_value('numberphysicalcpus', $num_physical_cpus);
-            $xml .= add_XML_value('latitude', $site_array['latitude']);
-            $xml .= add_XML_value('longitude', $site_array['longitude']);
-            $xml .= add_XML_value('longitude', $site_array['longitude']);
-            $xml .= add_XML_value('maintainer_name', $site_array['firstname'] . ' ' . $site_array['lastname']);
-            $xml .= add_XML_value('maintainer_id', $site_array['userid']);
-            $xml .= '</site>';
-        }
-
-        $xml .= '</cdash>';
-
-        return $this->view('cdash', 'Sites Map')
-            ->with('xsl', true)
-            ->with('xsl_content', generate_XSLT($xml, base_path() . '/app/cdash/public/viewMap', true));
-    }
-}

app/Http/Controllers/ProjectController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
 
 final class ProjectController extends AbstractProjectController
 {
@@ -192,5 +193,12 @@ final class ProjectController extends AbstractProjectController
         $stmt->execute();
         $projects = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         return is_array($projects) ? $projects : [];
+    }
+
+    public function sites(int $project_id): View
+    {
+        $this->setProjectById($project_id);
+
+        return $this->view('project.sites', 'Sites');
     }
 }

app/Models/Site.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\QueryException;
@@ -61,6 +62,14 @@ class Site extends Model
             ->ofMany(['timestamp' => 'max'], function (Builder $query) use ($date) {
                 $query->where('timestamp', '<=', $date ?? Carbon::maxValue());
             });
+    }
+
+    /**
+     * @return BelongsToMany<User>
+     */
+    public function maintainers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'site2user', 'siteid', 'userid');
     }
 
     public function save(array $options = []): bool

app/cdash/public/viewMap.xsl
@@ -1,90 +1,0 @@
-<xsl:stylesheet
-    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version='1.0'>
-
- <!-- HEADER -->
-   <xsl:output method="xml" indent="yes"  doctype-public="-//W3C//DTD XHTML 1.0 Transitional//EN"
-   doctype-system="http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" />
-    <xsl:template match="/">
-
-<xsl:if test="string-length(cdash/googlemapkey)>0">
- <script type="text/javascript">
-      <xsl:attribute name="src">http://maps.google.com/maps?file=api&amp;v=2&amp;key=<xsl:value-of select="cdash/dashboard/googlemapkey"/></xsl:attribute>
-   </script>
-  <xsl:text disable-output-escaping="yes">
-    &lt;script type="text/javascript"&gt;
-      // Creates a marker whose info window displays the letter corresponding
-      // to the given index.
-      function createMarker(point,title)
-        {
-        var marker = new GMarker(point);
-        GEvent.addListener(marker, "click", function()
-          {
-          marker.openInfoWindowHtml(title);
-          });
-        return marker;
-      }
-
-    function load() {
-      if (GBrowserIsCompatible()) {
-        var map = new GMap2(document.getElementById("map"));
-        map.setCenter(new GLatLng(37.4419, -30.00), 2);
-        map.addControl(new GLargeMapControl());
-  </xsl:text>
-        <xsl:for-each select="cdash/site">
-        <xsl:if test="string-length(latitude)>0">
-        var point = new GLatLng(<xsl:value-of select="latitude"/>,<xsl:value-of select="longitude"/>);
-        map.addOverlay(createMarker(point,'<xsl:value-of select="name"/>'));
-        </xsl:if>
-        </xsl:for-each><xsl:text disable-output-escaping="yes">
-      }
-    }
-    &lt;/script&gt;
-    </xsl:text>
-</xsl:if>
-
-<div style="position:relative;">
-<div style="float:left; padding-right:20px;">
-<!-- Display the table of maintainers -->
-<table id="maintainerTable" width="100%" cellspacing="0" class="tabb">
-<thead>
-  <tr class="table-heading1">
-    <th id="sort_0">Site Name</th>
-    <th id="sort_1" >Maintainer</th>
-    <th id="sort_2" >Processor Speed</th>
-    <th id="sort_3" class="nob"># Processors</th>
-  </tr>
-</thead>
-<xsl:for-each select="cdash/site">
-<tr>
-<td><center>
-<a>
-<xsl:attribute name="href">sites/<xsl:value-of select="id"/></xsl:attribute>
-<xsl:value-of select="name"/></a></center></td>
-<td><center>
-<xsl:if test="string-length(maintainer_name)>1">
-<xsl:value-of select="maintainer_name"/>
-</xsl:if>
-<xsl:if test="string-length(maintainer_name)=1">
-<a>
-<xsl:attribute name="href">editSite.php?siteid=<xsl:value-of select="id"/></xsl:attribute>
-[claim this site]
-</a>
-</xsl:if>
-</center></td>
-<td><center><xsl:value-of select="processor_speed"/></center></td>
-<td><center><xsl:value-of select="numberphysicalcpus"/></center></td>
-</tr>
-</xsl:for-each>
-</table>
-</div>
-
-<xsl:if test="string-length(cdash/googlemapkey)>0">
-  <!-- Display the map -->
-  <div id="map" style="width: 700px; height: 400px; float:left;"></div>
-</xsl:if>
-</div>
-
-<script language="javascript" type="text/javascript" src="assets/js/angular/cdashSiteSorter.js"></script>
-
-    </xsl:template>
-</xsl:stylesheet>

app/cdash/tests/CMakeLists.txt
@@ -329,11 +329,8 @@ set_tests_properties(viewdynamicanalysis PROPERTIES DEPENDS viewconfigure)
 add_php_test(viewdynamicanalysisfile)
 set_tests_properties(viewdynamicanalysisfile PROPERTIES DEPENDS viewdynamicanalysis)
 
-add_php_test(viewmap)
-set_tests_properties(viewmap PROPERTIES DEPENDS viewdynamicanalysisfile)
-
 add_php_test(buildmodel)
-set_tests_properties(buildmodel PROPERTIES DEPENDS viewmap)
+set_tests_properties(buildmodel PROPERTIES DEPENDS viewdynamicanalysisfile)
 
 add_unit_test(/CDash/Model/PendingSubmissions)
 set_tests_properties(/CDash/Model/PendingSubmissions PROPERTIES DEPENDS buildmodel)

app/cdash/tests/test_viewmap.php
@@ -1,24 +1,0 @@
-<?php
-//
-// After including cdash_test_case.php, subsequent require_once calls are
-// relative to the top of the CDash source tree
-//
-require_once dirname(__FILE__) . '/cdash_test_case.php';
-
-class ViewMapTestCase extends KWWebTestCase
-{
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function testViewMap()
-    {
-        $this->get($this->url . '/viewMap.php?project=InsightExample');
-        if (strpos($this->getBrowser()->getContentAsText(), 'Maintainer') === false) {
-            $this->fail("'Maintainer' not found when expected");
-            return 1;
-        }
-        $this->pass('Passed');
-    }
-}

graphql/schema.graphql
@@ -421,6 +421,9 @@ type Site {
   "The most recent information we have about this site."
   # TODO: Figure out how to support the date parameter. Perhaps it would be better to use a scope instead.
   mostRecentInformation: SiteInformation @hasOne
+
+  "The users who have signed up to maintain this site."
+  maintainers: [User!]! @belongsToMany(type: CONNECTION) @orderBy(column: "id")
 }
 
 input SiteFilterInput {

commit 0eb07888524764c186823e485ab68240900110a7

Improve handling of sites with no information (#2586)
This PR fixes an issue introduced in #2583 where sites without any
attached site information prevent the entire page from rendering
properly. I plan to add thorough e2e tests for this page once I finish
getting Laravel Dusk set up.

resources/js/vue/components/ProjectSitesPage.vue
@@ -121,12 +121,12 @@ export default {
             href: `${this.$baseURL}/sites/${edge.node.id}`,
           },
           processors: {
-            value: edge.node.mostRecentInformation.numberPhysicalCpus,
-            text: edge.node.mostRecentInformation.numberPhysicalCpus,
+            value: edge.node.mostRecentInformation?.numberPhysicalCpus,
+            text: edge.node.mostRecentInformation?.numberPhysicalCpus,
           },
           memory: {
-            value: edge.node.mostRecentInformation.totalPhysicalMemory,
-            text: this.humanReadableMemory(edge.node.mostRecentInformation.totalPhysicalMemory),
+            value: edge.node.mostRecentInformation?.totalPhysicalMemory,
+            text: this.humanReadableMemory(edge.node.mostRecentInformation?.totalPhysicalMemory),
           },
           maintainers: edge.node.maintainers.edges.map(maintainer => {
             let maintainerName = '';
@@ -142,7 +142,7 @@ export default {
             return maintainerName;
           }).join(', '),
           description: {
-            value: edge.node.mostRecentInformation.description,
+            value: edge.node.mostRecentInformation?.description,
             text: edge.node.description,
           },
         };
@@ -152,6 +152,10 @@ export default {
 
   methods: {
     humanReadableMemory(inputInMiB) {
+      if (!inputInMiB) {
+        return '';
+      }
+
       if (inputInMiB < 1024) {
         return `${inputInMiB} MiB`;
       }