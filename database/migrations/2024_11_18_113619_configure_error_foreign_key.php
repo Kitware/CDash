bfd5bbc2825efe0a41d572abc1efac20ceae7e31
Zack Galbreath <zack.galbreath@kitware.com>
2024-12-02 15:13:46 -0500

    Perform batch deletion of invalid records before applying new foreign keys (#2588)
    Perform batch deletion of invalid records before applying new foreign
    keys

--- database/migrations/2024_11_18_113619_configure_error_foreign_key.php
+++ database/migrations/2024_11_18_113619_configure_error_foreign_key.php
@@ -16,9 +16,63 @@ return new class extends Migration {
             $table->integer('configureid')->change();
         });
 
+        echo 'Deleting invalid rows for configureerror' . PHP_EOL;
+
+        $done = false;
+        $error_msg = "Could not determine min & max configureid for configureerror table.\n";
+        $error_msg .= "Manually set \$start and \$max and rerun the migration\n";
+
+        $start = DB::table('configureerror')->min('configureid');
+        $max = DB::table('configureerror')->max('configureid');
+        if (!is_numeric($start) || !is_numeric($max)) {
+            if (DB::table('configureerror')->count() > 0) {
+                throw new Exception($error_msg);
+            } else {
+                $done = true;
+            }
+        }
+
+        if (!$done) {
+            $start = intval($start);
+            $max = intval($max);
+            $total = $max - $start + 1;
+            if ($total < 1) {
+                if (DB::table('configureerror')->count() > 0) {
+                    throw new Exception($error_msg);
+                } else {
+                    $done = true;
+                }
+            }
+        }
+
+        $num_done = 0;
+        $num_deleted = 0;
+        $next_report = 10;
+        while (!$done) {
+            $end = $start + 49999;
+            $num_deleted += DB::delete("
+                DELETE FROM configureerror
+                WHERE configureid BETWEEN {$start} AND {$end}
+                      AND NOT EXISTS
+                      (SELECT 1 FROM configure WHERE configure.id = configureerror.configureid)");
+            $num_done += 50000;
+            if ($end >= $max) {
+                $done = true;
+            } else {
+                usleep(1);
+                $start += 50000;
+                // Calculate percentage of work completed so far.
+                $percent = round(($num_done / $total) * 100, -1);
+                if ($percent > $next_report) {
+                    echo "Cleaning `configureerror`: {$next_report}%" . PHP_EOL;
+                    $next_report = $next_report + 10;
+                }
+            }
+        }
+
+        echo "{$num_deleted} rows deleted from `configureerror`" . PHP_EOL;
         echo "Adding foreign key constraint configureerror(configureid)->configure(id)...";
-        $num_deleted = DB::delete("DELETE FROM configureerror WHERE configureid NOT IN (SELECT id FROM configure)");
-        echo $num_deleted . ' invalid rows deleted' . PHP_EOL;
+
         Schema::table('configureerror', function (Blueprint $table) {
             $table->foreign('configureid')->references('id')->on('configure')->cascadeOnDelete();
         });
