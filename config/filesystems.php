5f505736610c1ceb1db000ee6d4b62993f029674
Zack Galbreath <zack.galbreath@kitware.com>
2025-01-15 14:54:28 -0500

    Update s3 filesystem config to match stock Laravel 10

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,8 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
         'cdash' => [
