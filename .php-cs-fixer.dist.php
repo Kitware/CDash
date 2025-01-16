dc2ca39ac651a203e2da23d78fdb396b35b3a499
William Allen <16820599+williamjallen@users.noreply.github.com>
2025-01-16 08:36:06 -0500

    Reformat fixer config file

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,9 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
     ->exclude('app/cdash/config')
     ->exclude('bootstrap/cache')
     ->exclude('node_modules')
@@ -10,22 +13,19 @@ $finder = PhpCsFixer\Finder::create()
     ->exclude('resources')
     ->exclude('public')
     ->notPath('app/cdash/tests/config.test.local.php')
-    ->in(__DIR__)
-;
+    ->in(__DIR__);
 
-$config = new PhpCsFixer\Config();
+$config = new Config();
 return $config->setRules([
-        '@PSR12' => true,
-        '@PHP82Migration' => true,
-        '@Symfony' => true,
-        'yoda_style' => false,
-        'blank_line_before_statement' => false,
-        'phpdoc_summary' => false,
-        'concat_space' => ['spacing' => 'one'],
-        'increment_style' => ['style' => 'post'],
-        'fully_qualified_strict_types' => ['import_symbols' => true],
-        'global_namespace_import' => ['import_classes' => true, 'import_constants' => null, 'import_functions' => null],
-        'phpdoc_align' => ['align' => 'left'],
-    ])
-    ->setFinder($finder)
-;
+    '@PSR12' => true,
+    '@PHP82Migration' => true,
+    '@Symfony' => true,
+    'yoda_style' => false,
+    'blank_line_before_statement' => false,
+    'phpdoc_summary' => false,
+    'concat_space' => ['spacing' => 'one'],
+    'increment_style' => ['style' => 'post'],
+    'fully_qualified_strict_types' => ['import_symbols' => true],
+    'global_namespace_import' => ['import_classes' => true, 'import_constants' => null, 'import_functions' => null],
+    'phpdoc_align' => ['align' => 'left'],
+])->setFinder($finder);
