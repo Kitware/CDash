64e37a8032fa6933db8d5d00847bd0e213854a90
Joseph Snyder <joe.snyder@kitware.com>
2025-01-24 15:30:52 -0500

    Start using autoregister as the default for OAuth (#2677)
    Remove the configuration options for the OAuth providers which would
    allow for the auto-registration of a user to be disabled.
    
    Remove the code paths that would allow either the user to fill the
    registration form for themselves or the error that would be thrown
    
    This is a step to take before being able to disable registration forms
    but still allow OAuth to register users.

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Auth\RegisterController;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\RedirectResponse;
@@ -47,29 +46,19 @@ final class OAuthController extends AbstractController
         // TODO: What if, for whatever reason, there is more than one user found?
         $user = User::firstWhere('email', $email);
         if ($user === null) {
-            if (config("services.{$service}.autoregister") === true) {
-                $registerData = [
-                    'email' => $email,
-                    'fname' => $fname,
-                    'lname' => $lname,
-                    'institution' => '',
-                    'password' => Hash::make(Str::random(40)),
-                ];
-                $registerController = new RegisterController();
-                $user = $registerController->create($registerData);
-                if ($user === null) {
-                    Log::error("Error registering new user via $service: $email");
-                    abort(500, 'Error registering new user');
-                }
-                Auth::login($user, true);
-            } else {
-                $parameters = compact('fname', 'lname', 'email');
-                $to = route('register', $parameters);
-                return redirect($to);
+            $user = User::create([
+                'firstname' => $fname,
+                'lastname' => $lname,
+                'email' => $email,
+                'password' => Hash::make(Str::random(40)),
+                'institution' => '',
+            ]);
+            if ($user === null) {
+                Log::error("Error registering new user via $service: $email");
+                abort(500, 'Error registering new user');
             }
-        } else {
-            Auth::login($user, true);
         }
-        return redirect(session('url.intended') ?? '/');
+        Auth::login($user, true);
+        return redirect(session('url.intended') ?? '/profile');
     }
 }
