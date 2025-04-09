abb5f2745ddd1143f7f0565f60de01451f028e2b
William Allen <16820599+williamjallen@users.noreply.github.com>
2025-04-09 15:14:08 -0400

    Change column types to match related tables for MySQL

--- database/migrations/2025_02_24_132230_create_user_invitations_table.php
+++ database/migrations/2025_02_24_132230_create_user_invitations_table.php
@@ -10,8 +10,10 @@ return new class extends Migration {
         Schema::create('user_invitations', function (Blueprint $table) {
             $table->id();
             $table->string('email', 255)->index()->nullable(false)->index();
-            $table->foreignId('invited_by_id')->index()->nullable(false)->references('id')->on('users')->cascadeOnDelete();
-            $table->foreignId('project_id')->index()->nullable(false)->references('id')->on('project')->cascadeOnDelete();
+            $table->integer('invited_by_id')->index()->nullable(false);
+            $table->foreign('invited_by_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->integer('project_id')->index()->nullable(false);
+            $table->foreign('project_id')->references('id')->on('project')->cascadeOnDelete();
             $table->enum('role', ['USER', 'ADMINISTRATOR'])->nullable(false);
             $table->timestampTz('invitation_timestamp')->index()->nullable(false)->useCurrent();
 
