79400739aef51d32dc23be526990e90cfa868b1f
Zack Galbreath <zack.galbreath@kitware.com>
2025-09-10 16:55:56 -0400

    Restart worker containers after one hour
    This matches the configuration we use for our production systems hosted
    on cdash.org.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -43,7 +43,7 @@ if [ "$1" = "start-website" ] ; then
 # If the start-worker argument was provided, start a worker process instead
 elif [ "$1" = "start-worker" ] ; then
   php artisan storage:mkdirs
-  php -d memory_limit=-1 artisan queue:work
+  php -d memory_limit=-1 artisan queue:work --max-time=3600
 
 # Otherwise, throw an error...
 else
