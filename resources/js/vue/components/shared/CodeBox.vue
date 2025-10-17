d4d8d6b30c4e986123597ea97d860e43a391ee40
William Allen <16820599+williamjallen@users.noreply.github.com>
2025-10-17 15:14:42 -0400

    Add reusable code box UI component

---
+++ resources/js/vue/components/shared/CodeBox.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    ref="editor"
+    class="code-box"
+    :class="{ 'tw-border': bordered }"
+  />
+</template>
+
+<script>
+import { EditorView, lineNumbers, drawSelection } from '@codemirror/view';
+import { EditorState } from '@codemirror/state';
+
+export default {
+  name: 'CodeBox',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+
+    bordered: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      editor: null,
+    };
+  },
+  watch: {
+    text(newText) {
+      if (this.editor) {
+        this.editor.dispatch({
+          changes: { from: 0, to: this.editor.state.doc.length, insert: newText },
+        });
+      }
+    },
+  },
+  mounted() {
+    const state = EditorState.create({
+      doc: this.text,
+      extensions: [
+        lineNumbers(),
+        drawSelection(),
+        EditorState.readOnly.of(true),
+        EditorView.theme({
+          '.cm-scroller': {
+            overflow: 'auto',
+          },
+        }),
+      ],
+    });
+
+    this.editor = new EditorView({
+      state,
+      parent: this.$refs.editor,
+    });
+  },
+  beforeUnmount() {
+    if (this.editor) {
+      this.editor.destroy();
+    }
+  },
+};
+</script>
