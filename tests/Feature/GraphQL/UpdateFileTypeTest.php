be2c44b3926a9eea8b90810acb9f38b734ce3d73
William Allen <16820599+williamjallen@users.noreply.github.com>
2026-01-13 09:04:45 -0500

    Expose build updates via graphql (#3362)
    Incremental progress towards exposing the full database via GraphQL.

---
+++ tests/Feature/GraphQL/UpdateFileTypeTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Tests\Feature\GraphQL;
+
+use App\Models\Build;
+use App\Models\BuildUpdate;
+use App\Models\BuildUpdateFile;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+use Tests\Traits\CreatesProjects;
+use Tests\Traits\CreatesUsers;
+
+class UpdateFileTypeTest extends TestCase
+{
+    use CreatesProjects;
+    use CreatesUsers;
+
+    private Project $project;
+
+    /** @var BuildUpdate[] */
+    private array $updates = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->project = $this->makePublicProject();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->project->delete();
+
+        foreach ($this->updates as $update) {
+            $update->delete();
+        }
+
+        parent::tearDown();
+    }
+
+    /**
+     * A basic test to ensure that each of the fields works
+     */
+    public function testBasicFieldAccess(): void
+    {
+        /** @var Build $build */
+        $build = $this->project->builds()->create([
+            'name' => 'build1',
+            'uuid' => Str::uuid()->toString(),
+        ]);
+
+        /** @var BuildUpdate $update */
+        $update = $build->updates()->create([
+            'command' => Str::uuid()->toString(),
+            'type' => 'GIT',
+            'status' => Str::uuid()->toString(),
+            'revision' => Str::uuid()->toString(),
+            'priorrevision' => Str::uuid()->toString(),
+            'path' => Str::uuid()->toString(),
+        ]);
+
+        /** @var BuildUpdateFile $updateFile */
+        $updateFile = $update->updateFiles()->create([
+            'filename' => Str::uuid()->toString(),
+            'author' => Str::uuid()->toString(),
+            'email' => Str::uuid()->toString(),
+            'committer' => Str::uuid()->toString(),
+            'committeremail' => Str::uuid()->toString(),
+            'log' => Str::uuid()->toString(),
+            'revision' => Str::uuid()->toString(),
+            'priorrevision' => Str::uuid()->toString(),
+            'status' => 'UPDATED',
+        ]);
+
+        $this->graphQL('
+            query build($id: ID) {
+                build(id: $id) {
+                    updates {
+                        edges {
+                            node {
+                                updateFiles {
+                                    edges {
+                                        node {
+                                            fileName
+                                            authorName
+                                            authorEmail
+                                            committerName
+                                            committerEmail
+                                            log
+                                            revision
+                                            priorRevision
+                                            status
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ', [
+            'id' => $build->id,
+        ])->assertExactJson([
+            'data' => [
+                'build' => [
+                    'updates' => [
+                        'edges' => [
+                            [
+                                'node' => [
+                                    'updateFiles' => [
+                                        'edges' => [
+                                            [
+                                                'node' => [
+                                                    'fileName' => $updateFile->filename,
+                                                    'authorName' => $updateFile->author,
+                                                    'authorEmail' => $updateFile->email,
+                                                    'committerName' => $updateFile->committer,
+                                                    'committerEmail' => $updateFile->committeremail,
+                                                    'log' => $updateFile->log,
+                                                    'revision' => $updateFile->revision,
+                                                    'priorRevision' => $updateFile->priorrevision,
+                                                    'status' => $updateFile->status,
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+}
---
+++ tests/Feature/GraphQL/UpdateTypeTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Tests\Feature\GraphQL;
+
+use App\Models\Build;
+use App\Models\BuildUpdate;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+use Tests\Traits\CreatesProjects;
+use Tests\Traits\CreatesUsers;
+
+class UpdateTypeTest extends TestCase
+{
+    use CreatesProjects;
+    use CreatesUsers;
+
+    private Project $project;
+
+    /** @var BuildUpdate[] */
+    private array $updates = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->project = $this->makePublicProject();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->project->delete();
+
+        foreach ($this->updates as $update) {
+            $update->delete();
+        }
+
+        parent::tearDown();
+    }
+
+    /**
+     * A basic test to ensure that each of the fields works
+     */
+    public function testBasicFieldAccess(): void
+    {
+        /** @var Build $build */
+        $build = $this->project->builds()->create([
+            'name' => 'build1',
+            'uuid' => Str::uuid()->toString(),
+        ]);
+
+        /** @var BuildUpdate $update */
+        $update = $build->updates()->create([
+            'command' => Str::uuid()->toString(),
+            'type' => 'GIT',
+            'status' => Str::uuid()->toString(),
+            'revision' => Str::uuid()->toString(),
+            'priorrevision' => Str::uuid()->toString(),
+            'path' => Str::uuid()->toString(),
+        ]);
+
+        $this->graphQL('
+            query build($id: ID) {
+                build(id: $id) {
+                    updates {
+                        edges {
+                            node {
+                                id
+                                command
+                                type
+                                status
+                                revision
+                                priorRevision
+                                path
+                            }
+                        }
+                    }
+                }
+            }
+        ', [
+            'id' => $build->id,
+        ])->assertExactJson([
+            'data' => [
+                'build' => [
+                    'updates' => [
+                        'edges' => [
+                            [
+                                'node' => [
+                                    'id' => (string) $update->id,
+                                    'command' => $update->command,
+                                    'type' => $update->type,
+                                    'status' => $update->status,
+                                    'revision' => $update->revision,
+                                    'priorRevision' => $update->priorrevision,
+                                    'path' => $update->path,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+}
