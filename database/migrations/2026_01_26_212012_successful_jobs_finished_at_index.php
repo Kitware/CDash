7dca78fa3ea193a5debd26511ad0e2043076b3bb
William Allen <william.allen@kitware.com>
2026-01-26 16:23:19 -0500

    Create index on successful_jobs(finished_at)
    This index directly improves the performance of the PruneJobs job.

---
+++ database/migrations/2026_01_26_212012_successful_jobs_finished_at_index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        DB::statement('CREATE INDEX ON successful_jobs (finished_at)');
+    }
+
+    public function down(): void
+    {
+    }
+};
