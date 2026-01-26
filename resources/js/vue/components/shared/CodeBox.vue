93581ef60fe35f9a06c2a928399367441e069f05
William Allen <16820599+williamjallen@users.noreply.github.com>
2026-01-26 09:08:12 -0500

    Re-work CodeBox UI component (#3387)
    https://github.com/Kitware/CDash/pull/3203 added a UI component built on
    top of CodeMirror which added line number and other code formatting to
    each output cell across the site. CodeMirror boxes are not searchable
    because they use an infinite-scroll technique to minimize memory usage.
    We want all pages to be searchable via native browser search tools, and
    line numbers didn't turn out to be as useful as imagined. As such, this
    PR switches back to a purely tailwind-based approach for styling code
    boxes.

--- resources/js/vue/components/shared/CodeBox.vue
+++ resources/js/vue/components/shared/CodeBox.vue
@@ -1,15 +1,13 @@
 <template>
   <div
-    ref="editor"
-    class="code-box"
-    :class="{ 'tw-border': bordered }"
-  />
+    class="tw-font-mono tw-bg-gray-100 tw-p-2 tw-whitespace-pre-wrap tw-break-all tw-overflow-hidden"
+    :class="{ 'tw-border': bordered, 'tw-rounded': bordered }"
+  >
+    {{ text }}
+  </div>
 </template>
 
 <script>
-import { EditorView, lineNumbers, drawSelection } from '@codemirror/view';
-import { EditorState } from '@codemirror/state';
-
 export default {
   name: 'CodeBox',
   props: {
@@ -22,46 +20,6 @@ export default {
       type: Boolean,
       default: true,
     },
-  },
-  data() {
-    return {
-      editor: null,
-    };
-  },
-  watch: {
-    text(newText) {
-      if (this.editor) {
-        this.editor.dispatch({
-          changes: { from: 0, to: this.editor.state.doc.length, insert: newText },
-        });
-      }
-    },
-  },
-  mounted() {
-    const state = EditorState.create({
-      doc: this.text,
-      extensions: [
-        lineNumbers(),
-        drawSelection(),
-        EditorState.readOnly.of(true),
-        EditorView.theme({
-          '.cm-scroller': {
-            overflow: 'auto',
-          },
-        }),
-        EditorView.lineWrapping,
-      ],
-    });
-
-    this.editor = new EditorView({
-      state,
-      parent: this.$refs.editor,
-    });
-  },
-  beforeUnmount() {
-    if (this.editor) {
-      this.editor.destroy();
-    }
   },
 };
 </script>
