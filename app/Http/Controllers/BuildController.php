Kitware/CDash

commit 247ac5f60dcd87f670ec2ac5fc4489827839715c

Remove unnecessary build2update pivot table (#3370)
Builds are associated with at most one update. The `build2update` pivot
table is thus unnecessary, and causes confusion when interacting with
the `BuildUpdate` model. This PR moves the `updateid` column to the
`build` table and removes the pivot table.

app/Http/Controllers/BuildController.php
@@ -319,10 +319,10 @@ final class BuildController extends AbstractBuildController
             SELECT *
             FROM
                 buildupdate AS u,
-                build2update AS b2u
+                build AS b
             WHERE
-                b2u.updateid = u.id
-                AND b2u.buildid = ?
+                b.updateid = u.id
+                AND b.id = ?
         ', [$this->build->Id])[0] ?? [];
 
         // TODO: (williamjallen) Determine what $buildupdate was supposed to be.  It is currently undefined.
@@ -334,10 +334,10 @@ final class BuildController extends AbstractBuildController
                 SELECT count(*) AS c
                 FROM
                     updatefile,
-                    build2update AS b2u
+                    build
                 WHERE
-                    updatefile.updateid=b2u.updateid
-                    AND b2u.buildid = ?
+                    updatefile.updateid=build.updateid
+                    AND build.id = ?
                     AND author = 'Local User'
             ", [$this->build->Id])[0]->c;
 
@@ -352,8 +352,8 @@ final class BuildController extends AbstractBuildController
 
             $nupdates = (int) DB::select('
                 SELECT count(*) AS c
-                FROM updatefile, build2update AS b2u
-                WHERE updatefile.updateid=b2u.updateid AND b2u.buildid=?
+                FROM updatefile, build
+                WHERE updatefile.updateid=build.updateid AND build.id=?
             ', [$this->build->Id])[0]->c;
             $update_response['nupdates'] = $nupdates;
 
@@ -436,7 +436,7 @@ final class BuildController extends AbstractBuildController
 
         // Previous build
         if ($previous_buildid > 0 && isset($previous_build)) {
-            $previous_build_update = EloquentBuild::findOrFail($previous_buildid)->updates()->first();
+            $previous_build_update = EloquentBuild::findOrFail($previous_buildid)->updateStep;
 
             $response['previousbuild'] = [
                 'buildid' => $previous_buildid,
@@ -460,7 +460,7 @@ final class BuildController extends AbstractBuildController
             $next_build = new Build();
             $next_build->Id = $next_buildid;
             $next_build->FillFromId($next_build->Id);
-            $next_build_update = EloquentBuild::findOrFail($next_buildid)->updates()->first();
+            $next_build_update = EloquentBuild::findOrFail($next_buildid)->updateStep;
 
             $response['nextbuild'] = [
                 'buildid' => $next_buildid,
@@ -642,7 +642,7 @@ final class BuildController extends AbstractBuildController
         $response['build'] = $build_response;
 
         // Update
-        $update = EloquentBuild::findOrFail((int) $this->build->Id)->updates()->first();
+        $update = EloquentBuild::findOrFail((int) $this->build->Id)->updateStep;
 
         $update_response = [];
         if ($update !== null) {
@@ -896,8 +896,8 @@ final class BuildController extends AbstractBuildController
         ];
 
         // Update
-        if ($eloquentBuild->updates()->exists()) {
-            $revision = $eloquentBuild->updates()->firstOrFail()->revision;
+        if ($eloquentBuild->updateStep !== null) {
+            $revision = $eloquentBuild->updateStep->revision;
             $extra_build_fields['revision'] = $revision;
         } else {
             $revision = null;
@@ -1103,8 +1103,7 @@ final class BuildController extends AbstractBuildController
                 b.starttime,
                 bu.nfiles
             FROM build as b
-            JOIN build2update AS b2u ON b2u.buildid = b.id
-            JOIN buildupdate AS bu ON bu.id = b2u.updateid
+            JOIN buildupdate AS bu ON bu.id = b.updateid
             WHERE
                 b.siteid = ?
                 AND b.type = ?
@@ -1161,8 +1160,7 @@ final class BuildController extends AbstractBuildController
                                 b.starttime,
                                 b.endtime
                             FROM build AS b
-                            LEFT JOIN build2update AS b2u ON (b2u.buildid=b.id)
-                            LEFT JOIN buildupdate AS bu ON (b2u.updateid=bu.id)
+                            LEFT JOIN buildupdate AS bu ON (bu.updateid=b.id)
                             WHERE
                                 siteid = ?
                                 AND b.type = ?

app/Http/Controllers/SiteController.php
@@ -26,14 +26,12 @@ final class SiteController extends AbstractController
                     AVG(submittime - buildupdate.starttime) AS elapsed
                 FROM
                     build,
-                    build2update,
                     buildupdate,
                     project,
                     site
                 WHERE
                     submittime > NOW() - interval '168 hours'
-                    AND build2update.buildid = build.id
-                    AND buildupdate.id = build2update.updateid
+                    AND buildupdate.id = build.updateid
                     AND site.id = build.siteid
                     AND build.projectid = project.id
                 GROUP BY

app/Http/Controllers/TestController.php
@@ -302,17 +302,17 @@ final class TestController extends AbstractProjectController
             $prepared_array = Database::getInstance()->createPreparedArray(count($buildids));
             $query = DB::select("
                 SELECT
-                    b2u.buildid as buildid,
+                    build.id as buildid,
                     status,
                     revision,
                     priorrevision,
                     path
                 FROM
                     buildupdate,
-                    build2update AS b2u
+                    build
                 WHERE
-                    b2u.updateid = buildupdate.id
-                    AND b2u.buildid IN $prepared_array
+                    build.updateid = buildupdate.id
+                    AND build.id IN $prepared_array
             ", $buildids);
             foreach ($query as $row) {
                 $status_by_buildid[(int) $row->buildid] = $row;

app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BuildUpdate;
 use App\Models\Site;
 use App\Models\User;
 use App\Utils\AuthTokenUtil;
@@ -293,7 +294,8 @@ final class UserController extends AbstractController
             }
 
             // Update
-            $BuildUpdate = $build->updates()->first();
+            /** @var ?BuildUpdate $BuildUpdate */
+            $BuildUpdate = $build->updateStep;
             $response['update'] = $BuildUpdate->nfiles ?? 0;
             $response['updateclass'] = $BuildUpdate !== null && $BuildUpdate->warnings > 0 ? 'error' : 'normal';
 

app/Http/Submission/Handlers/DoneHandler.php
@@ -66,7 +66,7 @@ class DoneHandler extends AbstractXmlHandler
             // Should we re-run any checks that were previously marked
             // as pending?
             if ($pendingSubmissionsModel !== null && $pendingSubmissionsModel->recheck) {
-                $revision = \App\Models\Build::findOrFail((int) $this->Build->Id)->updates()->first()->revision ?? '';
+                $revision = \App\Models\Build::findOrFail((int) $this->Build->Id)->updateStep->revision ?? '';
                 Repository::createOrUpdateCheck($revision);
             }
 

app/Jobs/ProcessSubmission.php
@@ -629,7 +629,7 @@ class ProcessSubmission implements ShouldQueue
 
                 $body = "$sitename has encountered errors during the Update step and you have been identified as the maintainer of this site.\n\n";
                 $body .= "*Update Errors*\n";
-                $body .= 'Status: ' . $eloquentBuild->updates()->firstOrFail()->status . ' (' . url('/builds/' . $buildid . '/update') . ")\n";
+                $body .= 'Status: ' . $eloquentBuild->updateStep?->status . ' (' . url('/builds/' . $buildid . '/update') . ")\n";
 
                 Mail::raw($body, function ($message) use ($subject, $recipients): void {
                     $message->subject($subject)
@@ -642,7 +642,7 @@ class ProcessSubmission implements ShouldQueue
     /** Check for update errors for a given build. */
     private static function check_email_update_errors(int $buildid): array
     {
-        $num_errors = \App\Models\Build::findOrFail($buildid)->updates()->first()->errors ?? 0;
+        $num_errors = \App\Models\Build::findOrFail($buildid)->updateStep->errors ?? 0;
 
         return [
             'hasfixes' => false,

app/Models/Build.php
@@ -310,13 +310,11 @@ class Build extends Model
     }
 
     /**
-     * TODO: Perhaps rename this function in the future to make it less similar to Laravel's update()?
-     *
-     * @return BelongsToMany<BuildUpdate, $this>
+     * @return BelongsTo<BuildUpdate, $this>
      */
-    public function updates(): BelongsToMany
+    public function updateStep(): BelongsTo
     {
-        return $this->belongsToMany(BuildUpdate::class, 'build2update', 'buildid', 'updateid');
+        return $this->belongsTo(BuildUpdate::class, 'updateid');
     }
 
     /**

app/Models/BuildUpdate.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
@@ -63,11 +62,11 @@ class BuildUpdate extends Model
     }
 
     /**
-     * @return BelongsToMany<Build, $this>
+     * @return HasMany<Build, $this>
      */
-    public function builds(): BelongsToMany
+    public function builds(): HasMany
     {
-        return $this->belongsToMany(Build::class, 'build2update', 'updateid', 'buildid');
+        return $this->hasMany(Build::class, 'updateid');
     }
 
     /**

commit 49837db4b6de75eb05ee2f4fe8d8627f9aaf45a7

Fix build history plots
The graphs under `/builds/<id>` were broken in https://github.com/Kitware/CDash/pull/3370.

app/Http/Controllers/BuildController.php
@@ -1160,7 +1160,7 @@ final class BuildController extends AbstractBuildController
                                 b.starttime,
                                 b.endtime
                             FROM build AS b
-                            LEFT JOIN buildupdate AS bu ON (bu.updateid=b.id)
+                            LEFT JOIN buildupdate AS bu ON (b.updateid=bu.id)
                             WHERE
                                 siteid = ?
                                 AND b.type = ?