Kitware/CDash

commit 6ee7e8d3c3b262f9d52d05d655c470683576d9ab

Revamp project settings page (#3540)
This PR completely replaces the project settings page, fixing numerous
bugs, cleaning up the UI, and allowing us to deprecate two legacy API
routes. Along the way, I fixed an issue where the fileUploadLimit
returned a value in bytes rather than the documented GiB.

| Before | After |
| --- | --- |
| <img width="2832" height="1832" alt="image"
src="https://github.com/user-attachments/assets/3f36c058-6d5a-425c-9089-a0644b278b45"
/> | <img width="2828" height="1918" alt="image"
src="https://github.com/user-attachments/assets/16888056-9bc7-41ee-8f51-2a24b927f13b"
/> |

resources/views/components/header.blade.php
@@ -215,7 +215,7 @@ $showHeaderNav = isset($build);
                             <a href="#">Settings</a>
                             <ul>
                                 <li>
-                                    <a href="{{ url('/project') }}/{{ $project->Id }}/edit">
+                                    <a href="{{ url('/project') }}/{{ $project->Id }}/settings">
                                         Project
                                     </a>
                                 </li>

app/Http/Controllers/EditProjectController.php
@@ -1,26 +1,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Support\Facades\Gate;
-use Illuminate\View\View;
-
-final class EditProjectController extends AbstractProjectController
-{
-    // Render the create project form.
-    public function create(): View
-    {
-        Gate::authorize('create-project');
-
-        return $this->vue('edit-project', 'Create Project', ['projectid' => 0], false);
-    }
-
-    // Render the edit project form.
-    public function edit(int $project_id): View
-    {
-        $this->setProjectById($project_id);
-        Gate::authorize('edit-project', $this->project);
-
-        return $this->vue('edit-project', 'Edit Project', ['projectid' => $this->project->Id], false);
-    }
-}

app/Http/Controllers/ProjectController.php
@@ -159,6 +159,8 @@ final class ProjectController extends AbstractProjectController
 
         $response['ldap_enabled'] = config('cdash.ldap_enabled');
 
+        $response['deprecated'] = 'This endpoint will be removed in the next major version of CDash.';
+
         $pageTimer->end($response);
         return response()->json(cast_data_for_JSON($response));
     }

app/Http/Controllers/ProjectSettingsController.php
@@ -1,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
+
+final class ProjectSettingsController extends AbstractProjectController
+{
+    public function __invoke(Request $request, int $project_id): View
+    {
+        $this->setProjectById($project_id);
+
+        $project = Project::find($project_id);
+        Gate::authorize('update', $project);
+
+        return $this->vue('project-settings-page', 'Project Settings', [
+            'project-id' => $project_id,
+            'ldap-enabled' => (bool) config('cdash.ldap_enabled'),
+        ]);
+    }
+}

app/Models/Project.php
@@ -41,6 +41,7 @@ use Illuminate\Support\Facades\Auth;
  * @property int $autoremovetimeframe
  * @property int $autoremovemaxbuilds
  * @property int $uploadquota Maximum sum of uploaded file sizes (in bytes)
+ * @property int $uploadquotagb Maximum sum of uploaded file sizes (in GiB)
  * @property bool $showcoveragecode
  * @property bool $sharelabelfilters
  * @property bool $authenticatesubmissions
@@ -89,6 +90,7 @@ class Project extends Model
         'autoremovetimeframe',
         'autoremovemaxbuilds',
         'uploadquota',
+        'uploadquotagb',
         'showcoveragecode',
         'sharelabelfilters',
         'authenticatesubmissions',
@@ -134,6 +136,19 @@ class Project extends Model
 
                 return url('/image/' . $attributes['imageid']);
             },
+        );
+    }
+
+    /**
+     * @return Attribute<int,int>
+     */
+    protected function uploadquotagb(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes): int => (int) ((int) $attributes['uploadquota'] / (2 ** 30)),
+            set: fn (int $value): array => [
+                'uploadquota' => $value * (2 ** 30),
+            ]
         );
     }
 

app/cdash/public/subscribeProject.xsl
@@ -61,7 +61,7 @@
             <td><font color="#900000">*This project has not been configured to send emails.
              <xsl:choose>
                <xsl:when test="/cdash/role>1"><a>
-               <xsl:attribute name="href">project/<xsl:value-of select="/cdash/project/id"/>/edit#Email</xsl:attribute>Change the project settings.
+               <xsl:attribute name="href">project/<xsl:value-of select="/cdash/project/id"/>/settings#Email</xsl:attribute>Change the project settings.
                </a></xsl:when>
                <xsl:otherwise> Contact the project administrator.</xsl:otherwise>
              </xsl:choose>
@@ -135,7 +135,7 @@
             <td><font color="#900000">*This project has not been configured to send emails.
              <xsl:choose>
                <xsl:when test="/cdash/role>1"><a>
-               <xsl:attribute name="href">project/<xsl:value-of select="/cdash/project/id"/>/edit#Email</xsl:attribute>Change the project settings.
+               <xsl:attribute name="href">project/<xsl:value-of select="/cdash/project/id"/>/settings#Email</xsl:attribute>Change the project settings.
                </a></xsl:when>
                <xsl:otherwise> Contact the project administrator.</xsl:otherwise>
              </xsl:choose>

app/cdash/tests/CMakeLists.txt
@@ -417,6 +417,8 @@ add_browser_test(/Browser/Pages/BuildSummaryPageTest)
 
 add_browser_test(/Browser/Pages/BuildSidebarComponentTest)
 
+add_browser_test(/Browser/Pages/ProjectSettingsPageTest)
+
 add_php_test(image)
 set_tests_properties(image PROPERTIES DEPENDS /CDash/XmlHandler/UpdateHandler)
 

graphql/schema.graphql
@@ -307,7 +307,7 @@ type Project {
   """
   Uploaded files exceeding this limit (in GB) will be deleted from oldest to newest.
   """
-  fileUploadLimit: Int! @rename(attribute: "uploadquota")
+  fileUploadLimit: Int! @rename(attribute: "uploadquotagb")
 
   """
   Enable/Disable the display of source code for the project. Only administrators can see the source
@@ -478,7 +478,7 @@ input UpdateProjectInput @validator {
 
   autoRemoveMaxBuilds: Int @rename(attribute: "autoremovemaxbuilds")
 
-  fileUploadLimit: Int @rename(attribute: "uploadquota")
+  fileUploadLimit: Int @rename(attribute: "uploadquotagb")
 
   showCoverageCode: Boolean @rename(attribute: "showcoveragecode")
 

commit ecac607728e3652b20c8dc8f6822a5b728266697

Fix header links
The "Project" link was broken by https://github.com/Kitware/CDash/pull/3540, and the "Measurements" link follows a redirect.  This fixes both issues.

resources/views/components/header.blade.php
@@ -215,7 +215,7 @@ $showHeaderNav = isset($build);
                             <a href="#">Settings</a>
                             <ul>
                                 <li>
-                                    <a href="{{ url('/project') }}/{{ $project->Id }}/settings">
+                                    <a href="{{ url("/projects/{$project->Id}/settings") }}">
                                         Project
                                     </a>
                                 </li>
@@ -225,7 +225,7 @@ $showHeaderNav = isset($build);
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="{{ url('/project') }}/{{ $project->Id }}/testmeasurements">
+                                    <a href="{{ url("/projects/{$project->Id}/testmeasurements") }}">
                                         Measurements
                                     </a>
                                 </li>