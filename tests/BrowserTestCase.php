7801599df405d09288fa132b8f83f7fef833cc4e
William Allen <william.allen@kitware.com>
2026-04-01 09:10:55 -0400

    Parallelize browser tests

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -7,7 +7,6 @@ use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Config;
 use Laravel\Dusk\Browser;
 use Laravel\Dusk\Dusk;
 use Laravel\Dusk\TestCase as BaseTestCase;
@@ -21,33 +20,12 @@ abstract class BrowserTestCase extends BaseTestCase
      */
     public function setUp(): void
     {
-        // The APP_URL must be set prior to config initialization so we can correctly generate test URLs.
-        $env_contents = file_get_contents('/cdash/.env');
-        if ($env_contents === false) {
-            throw new Exception('Unable to read .env file.');
-        }
-        $env_after_substitution = str_replace('APP_URL=http://localhost:8080', 'APP_URL=http://website:8080', $env_contents);
-        file_put_contents('/cdash/.env', $env_after_substitution);
-
         parent::setUp();
 
         Dusk::selectorHtmlAttribute('data-test');
 
         Browser::$baseUrl = 'http://website:8080';
-        Browser::$waitSeconds = 10;
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-
-        $env_contents = file_get_contents(base_path('.env'));
-        if ($env_contents === false) {
-            throw new Exception('Unable to read .env file.');
-        }
-
-        $env_after_substitution = str_replace('APP_URL=http://website:8080', 'APP_URL=http://localhost:8080', $env_contents);
-        file_put_contents(base_path('.env'), $env_after_substitution);
+        Browser::$waitSeconds = 20;
     }
 
     /**
