Kitware/CDash

commit daf82caeabaf81a92d256fdbcb35431aeb7c8601

Convert test details page backend to GraphQL (#3662)
This PR adds all the missing pieces required to render the test details
page based on data obtained via the GraphQL API, and switches the page
to use them. While this page also desperately needs some UI
improvements, the goal of this PR is simply to move away from the legacy
API endpoint. I'll follow up with a separate PR containing UI
improvements.

resources/js/vue/components/TestDetailsPage.vue
@@ -1,24 +1,21 @@
 <template>
-  <section v-if="errored">
-    <p>{{ cdash.error }}</p>
-  </section>
   <BuildSidebar
     :build-id="buildId"
     active-tab="tests"
   >
     <build-summary-card :build-id="buildId" />
 
-    <loading-indicator :is-loading="loading">
+    <loading-indicator :is-loading="!build || !test">
       <div
         id="executiontime"
         class="tw-flex tw-flex-row tw-gap-1"
       >
         <img
           :src="$baseURL + '/img/clock.png'"
-          :title="'Average: ' + cdash.test.timemean + ', SD: ' + cdash.test.timestd"
+          :title="'Average: ' + test.meanRunningTime + ', SD: ' + test.stdDevRunningTime"
         >
         <span class="builddateelapsed">
-          {{ cdash.test.time }}
+          {{ runningTime }}
         </span>
       </div>
       <br>
@@ -27,64 +24,69 @@
       <a
         id="summary_link"
         class="tw-link tw-link-hover"
-        :href="$baseURL + '/' + cdash.test.summaryLink"
+        :href="`${$baseURL}/queryTests.php?project=${build.project.name}&filtercount=1&showfilters=1&field1=testname&compare1=61&value1=${test.name}&date=${testingDay}`"
       >
-        {{ cdash.test.test }}
+        {{ test.name }}
       </a>
-      <b :class="cdash.test.statusColor">
-        ({{ cdash.test.status }})
+      <b :class="testStatusColorClass">
+        ({{ testStatus }})
       </b>
       <br>
 
-      <div v-if="cdash.test.update.revision">
+      <div v-if="build.updateStep?.revision">
         <b>Repository revision: </b>
         <a
           id="revision_link"
           class="tw-link tw-link-hover"
-          :href="cdash.test.update.revisionurl"
+          :href="revisionUrl"
         >
-          {{ cdash.test.update.revision }}
+          {{ build.updateStep.revision }}
         </a>
         <br>
       </div>
 
-      <div v-if="cdash.test.details != ''">
+      <div v-if="test.details">
         <b>Test Details: </b>
-        {{ cdash.test.details }}
+        {{ test.details }}
         <br>
       </div>
 
-      <div v-if="cdash.project.showtesttime == 1">
+      <div v-if="build.project.enableTestTiming">
         <br>
         <b>Test Timing: </b>
-        <b :class="cdash.test.timeStatusColor">
-          {{ cdash.test.timestatus }}
+        <b :class="testTimeStatusColorClass">
+          {{ testTimeStatus }}
         </b>
-        <div v-if="cdash.test.timestatus != 'Passed'">
-          This test took longer to complete ({{ cdash.test.time }}) than the threshold allows ({{ cdash.test.threshold }}).
+        <div v-if="test.timeStatusCategory !== 'PASSED'">
+          This test took longer to complete ({{ runningTime }}) than the threshold allows ({{ runningTimeThreshold }}).
         </div>
       </div>
 
-      <div v-if="cdash.test.labels != ''">
-        <b>Labels: </b>
-        {{ cdash.test.labels }}
-        <br>
+      <div v-if="test.labels.edges.length > 0">
+        <div class="tw-flex tw-flex-row tw-flex-wrap tw-gap-2">
+          <b>Labels:</b>
+          <span
+            v-for="{ node: label } in test.labels.edges"
+            class="tw-badge tw-badge-outline tw-text-xs tw-text-neutral-500"
+          >
+            {{ label.text }}
+          </span>
+        </div>
       </div>
 
       <br>
 
       <!-- Display the measurements -->
       <table id="test_measurement_table">
-        <tr v-if="cdash.test.compareimages">
+        <tr v-if="compareImages.length > 0">
           <th class="measurement">
             Interactive Image
           </th>
           <td>
             <div class="je_compare">
               <img
-                v-for="(image, index) in cdash.test.compareimages"
-                :key="image.imgid"
-                :src="$baseURL + '/image/' + image.imgid"
+                v-for="(image, index) in compareImages"
+                :src="image.url"
                 :alt="image.role"
                 @load="index === 0 && initializeJeCompare()"
               >
@@ -92,13 +94,13 @@
           </td>
         </tr>
 
-        <tr v-for="image in cdash.test.images">
+        <tr v-for="{ node: image } in test.testImages.edges">
           <th class="measurement">
             {{ image.role }}
           </th>
           <td>
             <img
-              :src="$baseURL + '/image/' + image.imgid"
+              :src="image.url"
               :alt="image.role"
             >
           </td>
@@ -156,14 +158,14 @@
       <div v-if="showcommandline">
         <code-box
           id="commandline"
-          :text="cdash.test.command"
+          :text="test.command"
         />
         <br>
       </div>
 
       <!-- Show environment variables -->
       <div
-        v-if="hasenvironment"
+        v-if="environment"
         class="tw-flex tw-flex-row tw-gap-1"
       >
         <img
@@ -184,7 +186,7 @@
       <div v-if="showenvironment">
         <code-box
           id="environment"
-          :text="cdash.test.environment"
+          :text="environment"
         />
         <br>
       </div>
@@ -236,23 +238,23 @@
       <test-history-plot
         v-if="graphSelection === 'status'"
         :base-url="$baseURL"
-        :project-id="cdash.projectid"
-        :project-name="cdash.projectname"
-        :test-name="cdash.test.test"
-        :build-name="cdash.test.build"
+        :project-id="build.project.id"
+        :project-name="build.project.name"
+        :test-name="test.name"
+        :build-name="build.name"
       />
 
       <br>
       <b>Test Output</b>
       <code-box
         id="test_output"
-        :text="cdash.test.output"
+        :text="test.output"
       />
       <br>
 
-      <div v-for="preformatted_measurement in cdash.test.preformatted_measurements">
-        <b>{{ preformatted_measurement.name }}</b>
-        <code-box :text="preformatted_measurement.value" />
+      <div v-for="preformattedMeasurement in preformattedMeasurements">
+        <b>{{ preformattedMeasurement.name }}</b>
+        <code-box :text="preformattedMeasurement.value" />
         <br>
       </div>
     </loading-indicator>
@@ -262,13 +264,15 @@
 <script>
 import $ from 'jquery';
 import ApiLoader from './shared/ApiLoader';
-import QueryParams from './shared/QueryParams';
 import {DateTime} from 'luxon';
 import TestHistoryPlot from './shared/TestHistoryPlot.vue';
 import CodeBox from './shared/CodeBox.vue';
 import BuildSummaryCard from './shared/BuildSummaryCard.vue';
 import LoadingIndicator from './shared/LoadingIndicator.vue';
 import BuildSidebar from './shared/BuildSidebar.vue';
+import gql from 'graphql-tag';
+import {getRepository} from './shared/RepositoryIntegrations';
+import Utils from './shared/Utils';
 
 export default {
   name: 'TestDetails',
@@ -291,18 +295,99 @@ export default {
       type: Number,
       required: true,
     },
+
+    testingDay: {
+      type: String,
+      required: true,
+    },
+  },
+
+  apollo: {
+    build: {
+      query: gql`
+        query($id: ID) {
+          build(id: $id) {
+            id
+            name
+            updateStep {
+              id
+              revision
+              priorRevision
+            }
+            project {
+              id
+              name
+              enableTestTiming
+              testTimeStdMultiplier
+              vcsViewer
+              vcsUrl
+              cmakeProjectRoot
+            }
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.buildId,
+        };
+      },
+    },
+
+    test: {
+      query: gql`
+        query($id: ID) {
+          test(id: $id) {
+            id
+            name
+            status
+            details
+            command
+            output
+            runningTime
+            meanRunningTime
+            stdDevRunningTime
+            timeStatusCategory
+            testMeasurements {
+              id
+              name
+              type
+              value
+            }
+            labels {
+              edges {
+                node {
+                  id
+                  text
+                }
+              }
+            }
+            testImages {
+              edges {
+                node {
+                  id
+                  role
+                  url
+                }
+              }
+            }
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.testId,
+        };
+      },
+    },
   },
 
   data () {
     return {
       // API results.
       cdash: {},
-      loading: true,
-      errored: false,
 
       showcommandline: false,
       showenvironment: false,
-      hasenvironment: false,
       showgraph: false,
       graphSelection: '',
       rawdatalink: '',
@@ -311,25 +396,137 @@ export default {
   },
 
   computed: {
-    files: function () {
-      return this.cdash.test.measurements.filter((measurement) => {
+    files() {
+      return this.test.testMeasurements.filter((measurement) => {
         return measurement.type === 'file';
       });
     },
-    links: function () {
-      return this.cdash.test.measurements.filter((measurement) => {
+
+    links() {
+      return this.test.testMeasurements.filter((measurement) => {
         return measurement.type === 'text/link';
       });
     },
-    measurements: function () {
-      return this.cdash.test.measurements.filter((measurement) => {
-        return measurement.type !== 'file' && measurement.type !== 'text/link';
+
+    measurements() {
+      return this.test.testMeasurements.filter((measurement) => {
+        return measurement.type !== 'file'
+          && measurement.type !== 'text/link'
+          && measurement.type !== 'text/preformatted'
+          && !(measurement.type === 'text/string' && measurement.name === 'Environment');
       });
     },
-    numericMeasurements: function () {
-      return this.cdash.test.measurements.filter((measurement) => {
+
+    numericMeasurements() {
+      return this.test.testMeasurements.filter((measurement) => {
         return measurement.type.lastIndexOf('numeric/', 0) === 0;
       });
+    },
+
+    preformattedMeasurements() {
+      return this.test.testMeasurements.filter((measurement) => {
+        return measurement.type === 'text/preformatted';
+      });
+    },
+
+    environment() {
+      return this.test.testMeasurements.filter((measurement) => {
+        return measurement.type === 'text/string' && measurement.name === 'Environment';
+      })[0]?.value ?? null;
+    },
+
+    revisionUrl() {
+      return getRepository(this.build.project.vcsViewer, this.build.project.vcsUrl, this.build.project.cmakeProjectRoot)
+        ?.getCommitUrl(this.build.updateStep?.revision);
+    },
+
+    runningTime() {
+      return Utils.formatDuration(this.test.runningTime * 1000);
+    },
+
+    runningTimeThreshold() {
+      if (!this.test.runningTime || !this.test.meanRunningTime || !this.test.stdDevRunningTime) {
+        return '';
+      }
+
+      const thresholdInSeconds = this.test.meanRunningTime + (this.build.project.testTimeStdMultiplier * this.test.stdDevRunningTime);
+      return Utils.formatDuration(thresholdInSeconds * 1000);
+    },
+
+    testStatus() {
+      switch (this.test.status) {
+      case 'PASSED':
+        return 'Passed';
+      case 'FAILED':
+        return 'Failed';
+      case 'NOT_RUN':
+        return 'Not Run';
+      default:
+        return this.test.status;
+      }
+    },
+
+    /**
+     * TODO: Convert these to Tailwind colors
+     */
+    testStatusColorClass() {
+      switch (this.test.status) {
+      case 'PASSED':
+        return 'normal-text';
+      case 'FAILED':
+        return 'error-text';
+      case 'NOT_RUN':
+        return 'warning-text';
+      default:
+        return '';
+      }
+    },
+
+    testTimeStatus() {
+      switch (this.test.timeStatusCategory) {
+      case 'PASSED':
+        return 'Passed';
+      case 'FAILED':
+        return 'Warning';
+      default:
+        return this.test.timeStatusCategory;
+      }
+    },
+
+    /**
+     * TODO: Convert these to Tailwind colors
+     */
+    testTimeStatusColorClass() {
+      switch (this.test.timeStatusCategory) {
+      case 'PASSED':
+        return 'normal-text';
+      case 'FAILED':
+        return 'warning-text';
+      default:
+        return '';
+      }
+    },
+
+    compareImages() {
+      if (!this.test || !this.test.testImages) {
+        return [];
+      }
+
+      const images = [];
+
+      this.test.testImages.edges.forEach(({ node: image }) => {
+        if (image.role === 'ValidImage') {
+          images.push(image);
+        }
+      });
+
+      this.test.testImages.edges.forEach(({ node: image }) => {
+        if (image.role === 'TestImage') {
+          images.push(image);
+        }
+      });
+
+      return images;
     },
   },
 
@@ -338,14 +535,6 @@ export default {
     window.jQuery = $;
     await import('flot/dist/es5/jquery.flot');
     await import('../../angular/je_compare.js');
-
-    let endpoint_path = `/api/v1/testDetails.php?buildtestid=${this.testId}`;
-    this.queryParams = QueryParams.get();
-    if ('graph' in this.queryParams) {
-      this.graphSelection = this.queryParams.graph;
-      endpoint_path += `&graph=${this.graphSelection}`;
-    }
-    ApiLoader.loadPageData(this, endpoint_path);
   },
 
   methods: {
@@ -358,14 +547,8 @@ export default {
     },
 
     postSetup: function() {
-      this.queryParams = QueryParams.get();
       if (this.graphSelection) {
         this.displayGraph();
-      }
-
-      // eslint-disable-next-line eqeqeq
-      if (this.cdash.test.environment != '') {
-        this.hasenvironment = true;
       }
     },
 
@@ -377,14 +560,6 @@ export default {
           const newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}${graph_query}`;
           window.history.pushState({path:newurl},'',newurl);
 
-          // Update menu links.
-          this.cdash.menu.current = this.cdash.menu.current.split('?')[0] + graph_query;
-          if (this.cdash.menu.previous) {
-            this.cdash.menu.previous = this.cdash.menu.previous.split('?')[0] + graph_query;
-          }
-          if (this.cdash.menu.next) {
-            this.cdash.menu.next = this.cdash.menu.next.split('?')[0] + graph_query;
-          }
           ApiLoader.$emit('api-loaded', this.cdash);
         }
       }
@@ -394,7 +569,6 @@ export default {
         return;
       }
 
-      const testname = this.cdash.test.test;
       const measurementname = this.graphSelection;
       if (this.graphSelection === '') {
         this.showgraph = false;
@@ -405,7 +579,7 @@ export default {
       this.showgraph = true;
 
       let graph_type = '';
-      let endpoint_path = `/api/v1/testGraph.php?testname=${testname}&buildid=${this.buildId}`;
+      let endpoint_path = `/api/v1/testGraph.php?testname=${this.test.name}&buildid=${this.buildId}`;
       switch (this.graphSelection) {
       case 'status':
         graph_type = 'status';

app/Http/Controllers/TestDetailsController.php
@@ -30,6 +30,7 @@ final class TestDetailsController extends AbstractBuildController
         return $this->vue('test-details-page', 'Test Results', [
             'build-id' => $this->build->Id,
             'test-id' => $test->id,
+            'testing-day' => $this->date,
         ])
             ->with('previousUrl', $previousTest?->GetUrlForSelf())
             ->with('latestUrl', $latestTest?->GetUrlForSelf())

app/Models/TestImage.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -46,5 +47,21 @@ class TestImage extends Model
     public function image(): BelongsTo
     {
         return $this->belongsTo(Image::class, 'imgid');
+    }
+
+    /**
+     * @return Attribute<?string,void>
+     */
+    protected function url(): Attribute
+    {
+        return Attribute::make(
+            get: function (mixed $value, array $attributes): ?string {
+                if ($attributes['imgid'] === null || (int) $attributes['imgid'] === 0) {
+                    return null;
+                }
+
+                return url('/image/' . $attributes['imgid']);
+            },
+        );
     }
 }

app/Policies/TestPolicy.php
@@ -1,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Test;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
+class TestPolicy
+{
+    public function view(?User $user, Test $test): bool
+    {
+        return Gate::check('view', $test->build);
+    }
+}

app/cdash/tests/CMakeLists.txt
@@ -208,6 +208,8 @@ add_feature_test_in_transaction(/Feature/GraphQL/ConfigureTypeTest)
 
 add_feature_test_in_transaction(/Feature/GraphQL/TestTypeTest)
 
+add_feature_test_in_transaction(/Feature/GraphQL/TestImageTypeTest)
+
 add_feature_test_in_transaction(/Feature/GraphQL/TestMeasurementTypeTest)
 
 add_feature_test_in_transaction(/Feature/GraphQL/NoteTypeTest)

graphql/schema.graphql
@@ -51,6 +51,12 @@ type Query {
     id: ID @eq
   ): Build @find @canResolved(ability: "view")
 
+  "Find a single test by ID."
+  test(
+    "Search by primary key."
+    id: ID @eq
+  ): Test @find @canResolved(ability: "view", action: RETURN_VALUE, returnValue: null)
+
   """
   Find a single site by ID.  All sites are currently public (although not necessarily
   the projects they contribute to).  In the future, the concept of "private sites"
@@ -930,6 +936,12 @@ type Test {
 
   runningTime: NonNegativeSeconds! @rename(attribute: "time") @filterable
 
+  "The average of recent running times for this test"
+  meanRunningTime: NonNegativeSeconds! @rename(attribute: "timemean") @filterable
+
+  "The standard deviation of recent running times for this test"
+  stdDevRunningTime: Float! @rename(attribute: "timestd") @filterable
+
   startTime: DateTimeTz @rename(attribute: "starttime") @filterable
 
   details: String! @filterable
@@ -943,6 +955,10 @@ type Test {
   testMeasurements(
     filters: _ @filter
   ): [TestMeasurement!]! @hasMany @orderBy(column: "id", direction: DESC)
+
+  testImages(
+    filters: _ @filter
+  ): [TestImage!]! @hasMany(type: CONNECTION) @orderBy(column: "id", direction: DESC)
 
   labels(
     filters: _ @filter
@@ -981,6 +997,17 @@ type TestMeasurement {
   Example: "5"
   """
   value: String!
+}
+
+
+"Test Measurement."
+type TestImage {
+  "Unique primary key."
+  id: ID!
+
+  role: String! @filterable
+
+  url: String
 }
 
 

phpstan-baseline.neon
@@ -5044,6 +5044,18 @@ parameters:
 			path: app/Models/Test.php
 
 		-
+			rawMessage: Binary operation "." between '/image/' and mixed results in an error.
+			identifier: binaryOp.invalid
+			count: 1
+			path: app/Models/TestImage.php
+
+		-
+			rawMessage: Cannot cast mixed to int.
+			identifier: cast.int
+			count: 1
+			path: app/Models/TestImage.php
+
+		-
 			rawMessage: '''
 				Call to deprecated method forceRootUrl() of class Illuminate\Routing\UrlGenerator:
 				Use useOrigin

resources/js/vue/components/shared/QueryParams.js
@@ -1,11 +1,0 @@
-export default {
-  get: function () {
-    const params = {};
-    const kv_pairs = window.location.search.substring(1).split('&');
-    for (let i = 0; i < kv_pairs.length; i++) {
-      const kv = kv_pairs[i].split('=');
-      params[kv[0]] = kv[1];
-    }
-    return params;
-  },
-};

commit dcecc57586901489383f646be4b2aede2d90c035

Fix test file download link
Fixes an issue introduced in #3662 which broke test file download links.

resources/js/vue/components/TestDetailsPage.vue
@@ -114,14 +114,17 @@
             {{ measurement.value }}
           </td>
         </tr>
-        <tr v-for="file in files">
+        <tr
+          v-for="(file, index) in files"
+          :key="index"
+        >
           <th class="measurement">
             {{ file.name }}
           </th>
           <td>
             <a
               class="cdash-link"
-              :href="$baseURL + '/api/v1/testDetails.php?buildtestid=' + testId + '&fileid=' + file.fileid"
+              :href="$baseURL + '/api/v1/testDetails.php?buildtestid=' + testId + '&fileid=' + (index + 1)"
             >
               <img :src="$baseURL + '/img/package.png'">
             </a>