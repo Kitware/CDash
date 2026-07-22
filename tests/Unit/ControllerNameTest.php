3f0edc71371236fc4ead4a419fc65cd79354a4f9
William Allen <william.allen@kitware.com>
2026-07-22 09:10:59 -0400

    Remove `IteratesControllers` trait
    While `IteratesControllers` was designed with several use cases in mind, it's only used in a single test class for the foreseeable future.  This PR moves the `getControllers` method from the trait to the single class where it's used to reduce clutter.

--- tests/Unit/ControllerNameTest.php
+++ tests/Unit/ControllerNameTest.php
@@ -2,8 +2,13 @@
 
 namespace Tests\Unit;
 
+use Exception;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use ReflectionClass;
+use ReflectionException;
+use RegexIterator;
 use Tests\TestCase;
-use Tests\Traits\IteratesControllers;
 
 /**
  * CDash has a policy that all abstract controllers should start with "Abstract".  All non-abstract
@@ -11,7 +16,36 @@ use Tests\Traits\IteratesControllers;
  */
 class ControllerNameTest extends TestCase
 {
-    use IteratesControllers;
+    /**
+     * Returns an array of reflection objects corresponding to each controller.
+     *
+     * @return array<ReflectionClass<object>>
+     *
+     * @throws Exception
+     */
+    private static function getControllers(): array
+    {
+        $dir_iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(base_path('app/Http/Controllers')));
+        $file_iterator = new RegexIterator($dir_iterator, '/.*\.php/', RegexIterator::GET_MATCH);
+        $files = [];
+        foreach ($file_iterator as $file) {
+            $files = array_merge($files, $file);
+        }
+
+        $controllers = [];
+        foreach ($files as $file) {
+            $file = str_replace('.php', '', $file);
+            $file = str_replace(base_path('app'), 'App', $file);
+            $file = str_replace('/', '\\', $file);
+            try {
+                $controllers[] = new ReflectionClass($file);
+            } catch (ReflectionException) {
+                throw new Exception('Class name must match file name for all classes.');
+            }
+        }
+
+        return $controllers;
+    }
 
     public function testAllControllersAbstractOrFinal(): void
     {
