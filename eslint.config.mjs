dab9894248889722dd332658d96244f55af31bce
William Allen <william.allen@kitware.com>
2026-07-23 19:02:58 -0400

    Add `indent-binary-ops` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -59,6 +59,7 @@ export default defineConfig([
       '@stylistic/generator-star-spacing': 'error',
       '@stylistic/implicit-arrow-linebreak': 'error',
       '@stylistic/indent': ['error', 2],
+      '@stylistic/indent-binary-ops': ['error', 2],
       '@stylistic/keyword-spacing': ['error', {'before': true, 'after': true}],
       '@stylistic/space-before-blocks': ['error', 'always'],
       '@stylistic/linebreak-style': ['error', 'unix'],
