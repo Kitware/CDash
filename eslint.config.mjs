a82764fb40878c003064778fbe22a9905b58c408
William Allen <william.allen@kitware.com>
2026-07-23 19:05:54 -0400

    Drop redundant defaults for `keyword-spacing`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -61,7 +61,7 @@ export default defineConfig([
       '@stylistic/indent': ['error', 2],
       '@stylistic/indent-binary-ops': ['error', 2],
       '@stylistic/key-spacing': 'error',
-      '@stylistic/keyword-spacing': ['error', {'before': true, 'after': true}],
+      '@stylistic/keyword-spacing': 'error',
       '@stylistic/space-before-blocks': ['error', 'always'],
       '@stylistic/linebreak-style': ['error', 'unix'],
       '@stylistic/no-trailing-spaces': 'error',
