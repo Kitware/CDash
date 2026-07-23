59e160c4019614e6320046644507dc6e915884f4
William Allen <william.allen@kitware.com>
2026-07-23 19:42:33 -0400

    Add `no-tabs` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -71,6 +71,7 @@ export default defineConfig([
       '@stylistic/no-mixed-operators': 'error',
       '@stylistic/no-multi-spaces': 'error',
       '@stylistic/no-multiple-empty-lines': 'error',
+      '@stylistic/no-tabs': 'error',
       '@stylistic/space-before-blocks': ['error', 'always'],
       '@stylistic/linebreak-style': ['error', 'unix'],
       '@stylistic/no-trailing-spaces': 'error',
