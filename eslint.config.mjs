08b603e5e5d2384770a1d69f9df62fa9294f2ab0
William Allen <william.allen@kitware.com>
2026-07-23 18:55:02 -0400

    Add `function-call-spacing` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,6 +54,7 @@ export default defineConfig([
       '@stylistic/dot-location': ['error', 'property'],
       '@stylistic/eol-last': 'error',
       '@stylistic/function-call-argument-newline': ['error', 'consistent'],
+      '@stylistic/function-call-spacing': 'error',
       '@stylistic/keyword-spacing': ['error', {'before': true, 'after': true}],
       '@stylistic/space-before-blocks': ['error', 'always'],
       '@stylistic/linebreak-style': ['error', 'unix'],
