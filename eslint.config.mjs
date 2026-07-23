e447e15f55689286a6348ce31dbb949580d4edb6
William Allen <william.allen@kitware.com>
2026-07-23 18:27:17 -0400

    Add `array-element-newline` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,6 +41,7 @@ export default defineConfig([
 
       '@stylistic/array-bracket-newline': ['error', 'consistent'],
       '@stylistic/array-bracket-spacing': ['error', 'never'],
+      '@stylistic/array-element-newline': ['error', 'consistent'],
       '@stylistic/indent': ['error', 2],
       '@stylistic/arrow-spacing': 'error',
       '@stylistic/block-spacing': 'error',
