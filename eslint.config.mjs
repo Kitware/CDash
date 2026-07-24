ac4128b3e56577978358c6dee26ff58ab5ae64cc
William Allen <william.allen@kitware.com>
2026-07-23 20:00:24 -0400

    Add `padded-blocks` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -79,6 +79,7 @@ export default defineConfig([
       '@stylistic/object-property-newline': ['error', { 'allowAllPropertiesOnSameLine': true }],
       '@stylistic/one-var-declaration-per-line': 'error',
       '@stylistic/operator-linebreak': ['error', 'before'],
+      '@stylistic/padded-blocks': ['error', 'never'],
       '@stylistic/space-before-blocks': ['error', 'always'],
       '@stylistic/linebreak-style': ['error', 'unix'],
       '@stylistic/quotes': ['error', 'single', {'avoidEscape': true}],
