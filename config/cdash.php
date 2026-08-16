d81f7a45d7ae28124440b7ac56738def1b276592
William Allen <william.allen@kitware.com>
2026-08-16 19:46:27 -0400

    Remove dead code from ProcessSubmission
    I've noticed this dead code several times, but thought it was too trivial to justify a dedicated PR.  With major changes to this file upcoming, this is a good time to remove the dead code so it doesn't cause confusion.

--- config/cdash.php
+++ config/cdash.php
@@ -46,7 +46,6 @@ return [
     'login_field' => env('LOGIN_FIELD', 'Email'),
     'max_upload_quota' => env('MAX_UPLOAD_QUOTA', 10),
     'notify_pull_request' => env('NOTIFY_PULL_REQUEST', false),
-    'queue_timeout' => env('QUEUE_TIMEOUT', 2000),
     'retry_base' => env('QUEUE_RETRY_BASE', 5),
     'show_last_submission' => env('SHOW_LAST_SUBMISSION', true),
     'slow_page_time' => env('SLOW_PAGE_TIME', 10),
